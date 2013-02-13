c27af08ed6defe28e69f84ef5fe6d2449287a6ca
mmalohlava <michal.malohlava@gmail.com>
2013-02-13 23:06:31 +0100

    A simple RF test.

---
+++ py/testdir_ec2/test_rf_static_hosts.py
@@ -0,0 +1,50 @@
+import os, json, unittest, time, shutil, sys
+sys.path.extend(['.','..','py'])
+
+import h2o, h2o_cmd, h2o_hosts
+
+def pp(rf):
+    return """
+ Leaves: {0} / {1} / {2}
+  Depth: {3} / {4} / {5}
+   mtry: {6}
+    err: {7} %
+""".format(rf['trees']['leaves']['min'],rf['trees']['leaves']['mean'],rf['trees']['leaves']['max'],rf['trees']['depth']['min'],rf['trees']['depth']['mean'],rf['trees']['depth']['max'],-1, 0)
+class Basic(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        h2o_hosts.build_cloud_with_hosts()
+        
+    @classmethod
+    def tearDownClass(cls):
+        h2o.tear_down_cloud()
+        
+
+    def s3_default_bucket(self):
+        return 'h2o_datasets'
+
+    def test_RF_1000trees(self):
+        # NAs cause CM to zero..don't run for now
+        ### csvPathnamegz = h2o.find_file('smalldata/hhp_9_17_12.predict.100rows.data.gz')
+        s3bucket  = self.s3_default_bucket()  
+        s3dataset = 'covtype20x.data.gz'
+        s3dataset = 'covtype.data'
+        s3dataset = 'covtype200x.data.gz'
+        s3dataset = 'covtype50x.data'
+        s3dataset = 'covtype100x.data'
+        s3dataset = 'covtype.20k.data'
+
+        s3dataset = 'covtype.data'
+       
+        start = time.time()
+        parseKey = h2o_cmd.parseS3File(bucket=s3bucket, filename=s3dataset,timeoutSecs=14800)
+        print "Parsing took {0}".format(time.time()-start)
+        
+        start = time.time()
+	rf = h2o_cmd.runRFOnly(parseKey=parseKey, ntree=100, timeoutSecs=14800, bin_limit=20000, out_of_bag_error_estimate=1,gini=0,depth=100,exclusive_split_limit=0)
+        print "Computation took {0} sec".format(time.time()-start)
+        print "Result:",rf
+        #print pp(rf)
+
+if __name__ == '__main__':
+    h2o.unit_main()
