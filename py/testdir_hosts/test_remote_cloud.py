bec6f95517805cfb6355f37f6b44e3020ee7008f
Kevin Normoyle <kevin@0xdata.com>
2013-02-15 18:20:32 -0800

    tweaking

--- py/testdir_hosts/test_remote_cloud.py
+++ py/testdir_hosts/test_remote_cloud.py
@@ -4,7 +4,8 @@ sys.path.extend(['.','..','py'])
 import h2o_cmd, h2o, h2o_hosts
 import time
 
-nodes_per_host = 1
+node_count = 1
+base_port = 54321
 class Basic(unittest.TestCase):
     def tearDown(self):
         h2o.check_sandbox_for_errors()
@@ -12,7 +13,10 @@ class Basic(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # do the first one to build up hosts
-        h2o_hosts.build_cloud_with_hosts(nodes_per_host, use_flatfile=True)
+        # so we don't repeatedly copy the jar
+        # have to make sure base_port is the same on both!
+        h2o.write_flatfile(node_count=node_count, base_port=base_port)
+        h2o_hosts.build_cloud_with_hosts(node_count, base_port=base_port, use_flatfile=True)
         h2o.verify_cloud_size()
         h2o.check_sandbox_for_errors()
 
@@ -24,11 +28,11 @@ class Basic(unittest.TestCase):
         trySize = 0
         # FIX! we should increment this from 1 to N? 
         for i in range(1,10):
-            # timeout wants to be larger for large numbers of hosts * nodes_per_host
+            # timeout wants to be larger for large numbers of hosts * node_count
             # don't want to reload jar/flatfile, so use build_cloud
-            timeoutSecs = max(60, 2*(len(h2o_hosts.hosts) * nodes_per_host))
-            h2o.build_cloud(nodes_per_host, hosts=h2o_hosts.hosts, use_flatfile=True, 
-                timeoutSecs=timeoutSecs, retryDelaySecs=1)
+            timeoutSecs = max(60, 2*(len(h2o_hosts.hosts) * node_count))
+            h2o.build_cloud(node_count, hosts=h2o_hosts.hosts, use_flatfile=True, 
+                timeoutSecs=timeoutSecs, retryDelaySecs=0.5)
             h2o.verify_cloud_size()
             h2o.check_sandbox_for_errors()
 
@@ -38,10 +42,10 @@ class Basic(unittest.TestCase):
             # wait to make sure no sticky ports or anything os-related
             # so let's expand the delay if larger number of jvms
             # 1 second per node seems good
-            h2o.verboseprint("Waiting", nodes_per_host, "seconds to avoid OS sticky port problem")
+            h2o.verboseprint("Waiting", node_count, "seconds to avoid OS sticky port problem")
             sys.stdout.write('.')
             sys.stdout.flush()
-            time.sleep(nodes_per_host)
+            time.sleep(node_count)
 
 
 if __name__ == '__main__':
