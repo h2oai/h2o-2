5aff4c4db32fca994d2ec8bbdae8231cfd217cde
mmalohlava <michal.malohlava@gmail.com>
2013-02-20 20:21:12 +0100

    minor edit

--- py/testdir_ec2/test_rf_static_hosts.py
+++ py/testdir_ec2/test_rf_static_hosts.py
@@ -1,15 +1,8 @@
 import os, json, unittest, time, shutil, sys
 sys.path.extend(['.','..','py'])
 
-import h2o, h2o_cmd, h2o_hosts
+import h2o, h2o_cmd, h2o_hosts, h2o_pp
 
-def pp(rf):
-    return """
- Leaves: {0} / {1} / {2}
-  Depth: {3} / {4} / {5}
-   mtry: {6}
-    err: {7} %
-""".format(rf['trees']['leaves']['min'],rf['trees']['leaves']['mean'],rf['trees']['leaves']['max'],rf['trees']['depth']['min'],rf['trees']['depth']['mean'],rf['trees']['depth']['max'],-1, 0)
 class Basic(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -41,10 +34,9 @@ class Basic(unittest.TestCase):
         print "Parsing took {0}".format(time.time()-start)
         
         start = time.time()
-	rf = h2o_cmd.runRFOnly(parseKey=parseKey, ntree=100, timeoutSecs=14800, bin_limit=20000, out_of_bag_error_estimate=1,gini=0,depth=100,exclusive_split_limit=0)
+        rf_train = h2o_cmd.runRFOnly(parseKey=parseKey, ntree=100, timeoutSecs=14800, bin_limit=20000, out_of_bag_error_estimate=1,gini=0,depth=100,exclusive_split_limit=0)
         print "Computation took {0} sec".format(time.time()-start)
-        print "Result:",rf
-        #print pp(rf)
+        print pp(rf_train)
 
 if __name__ == '__main__':
     h2o.unit_main()
