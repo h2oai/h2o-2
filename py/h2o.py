e2178418ef964c1321a33560b6609ad8617e27c5
Kevin Normoyle <kevin@0xdata.com>
2013-02-23 10:49:23 -0800

    print the random_forest_view params when used for RF scoring, but not when it's part of a full RF

--- py/h2o.py
+++ py/h2o.py
@@ -847,7 +847,7 @@ class H2O(object):
         verboseprint("\nrandom_forest result:", dump_json(a))
         return a
 
-    def random_forest_view(self, data_key, model_key, timeoutSecs=300, **kwargs):
+    def random_forest_view(self, data_key, model_key, timeoutSecs=300, print_params=False, **kwargs):
         # UPDATE: only pass the minimal set of params to RFView. It should get the 
         # rest from the model. what about classWt? It can be different between RF and RFView?
         params_dict = {
@@ -857,7 +857,6 @@ class H2O(object):
             'class_weights': None,
             'response_variable': None, # FIX! apparently this is needed now?
             }
-
         browseAlso = kwargs.pop('browseAlso',False)
 
         # only update params_dict..don't add
@@ -866,15 +865,17 @@ class H2O(object):
             if k in params_dict:
                 params_dict[k] = kwargs[k]
 
+        if print_params:
+            print "\nrandom_forest_view parameters:", params_dict
+
         a = self.__check_request(requests.get(
             self.__url('RFView.json'),
             timeout=timeoutSecs,
             params=params_dict))
-
         verboseprint("\nrandom_forest_view result:", dump_json(a))
+
         if (browseAlso | browse_json):
             h2b.browseJsonHistoryAsUrlLastMatch("RFView")
-
         return a
 
     def random_forest_treeview(self, tree_number, data_key, model_key, 
