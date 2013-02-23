d59995b84df81ff400cc9e0b9d807b9d67f99c1e
Kevin Normoyle <kevin@0xdata.com>
2013-02-23 15:39:13 -0800

    tweaking max cloud size tested

--- py/testdir_multi_jvm/test_cloud.py
+++ py/testdir_multi_jvm/test_cloud.py
@@ -18,7 +18,7 @@ class Basic(unittest.TestCase):
         # don't know if we care
         base_port = 54300
         ports_per_node = 3
-        for tryNodes in range(2,10):
+        for tryNodes in range(2,17):
             h2o.verboseprint("Trying cloud of", tryNodes)
             sys.stdout.write('.')
             sys.stdout.flush()
