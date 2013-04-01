46a3955a5e74a85dfe12bdd10e9b15fdb5b130b2
Kevin Normoyle <kevin@0xdata.com>
2013-04-01 14:08:28 -0700

    changed ip for default ec2 test login

--- py/ec2_stuff/ec2_login.sh
+++ py/ec2_stuff/ec2_login.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ssh -l hduser -i /home/hduser/.ec2/keys/mrjenkins_test.pem hduser@ec2-184-73-55-110.compute-1.amazonaws.com
+# ssh -i ~/.ec2/keys/mrjenkins_test.pem root@ec2-184-73-55-110.compute-1.amazonaws.com
+ssh -i ~/.ec2/keys/mrjenkins_test.pem hduser@23.21.237.69
