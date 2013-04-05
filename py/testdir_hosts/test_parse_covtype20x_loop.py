13cd6303835b7211e6ff4717721630037f550e6c
Kevin Normoyle <kevin@0xdata.com>
2013-04-05 15:46:11 -0700

    tweaked test

--- py/testdir_hosts/test_parse_covtype20x_loop.py
+++ py/testdir_hosts/test_parse_covtype20x_loop.py
@@ -15,38 +15,39 @@ class Basic(unittest.TestCase):
     def tearDownClass(cls):
         h2o.tear_down_cloud()
 
-    def test_import_covtype20x_parse_loop(self):
+    def test_parse_covtype20x_loop(self):
         csvFilename = "covtype20x.data"
         importFolderPath = "/home/0xdiag/datasets"
-        trialMax = 3
-        for tryHeap in [4,12]:
-            print "\n", tryHeap,"GB heap, 1 jvm per host, import folder,", \
-                "then loop parsing 'covtype20x.data' to unique keys"
-            h2o_hosts.build_cloud_with_hosts(node_count=1, java_heap_GB=tryHeap)
-            timeoutSecs=300
-            for trial in range(trialMax):
-                # since we delete the key, we have to re-import every iteration, to get it again
-                h2i.setupImportFolder(None, importFolderPath)
+        trialMax = 2
+        for tryJvms in [1,2,3,4]:
+            for tryHeap in [1,3]:
+                print "\n", tryHeap,"GB heap,", tryJvms, "jvm per host, import folder,", \
+                    "then loop parsing 'covtype20x.data' to unique keys"
+                h2o_hosts.build_cloud_with_hosts(node_count=tryJvms, java_heap_GB=tryHeap)
+                timeoutSecs=300
+                for trial in range(trialMax):
+                    # since we delete the key, we have to re-import every iteration, to get it again
+                    h2i.setupImportFolder(None, importFolderPath)
 
-                key2 = csvFilename + "_" + str(trial) + ".hex"
-                start = time.time()
-                parseKey = h2i.parseImportFolderFile(None, csvFilename, importFolderPath, key2=key2, 
-                    timeoutSecs=timeoutSecs, retryDelaySecs=4, pollTimeoutSecs=60)
-                elapsed = time.time() - start
-                print "Trial #", trial, "completed in", elapsed, "seconds.", \
-                    "%d pct. of timeout" % ((elapsed*100)/timeoutSecs)
+                    key2 = csvFilename + "_" + str(trial) + ".hex"
+                    start = time.time()
+                    parseKey = h2i.parseImportFolderFile(None, csvFilename, importFolderPath, key2=key2, 
+                        timeoutSecs=timeoutSecs, retryDelaySecs=4, pollTimeoutSecs=60)
+                    elapsed = time.time() - start
+                    print "Trial #", trial, "completed in", elapsed, "seconds.", \
+                        "%d pct. of timeout" % ((elapsed*100)/timeoutSecs)
 
-                print "Deleting key in H2O so we get it from S3 (if ec2) or nfs again.", \
-                      "Otherwise it would just parse the cached key."
-                storeView = h2o.nodes[0].store_view()
-                ### print "storeView:", h2o.dump_json(storeView)
-                print "Removing", parseKey['source_key']
-                removeKeyResult = h2o.nodes[0].remove_key(key=parseKey['source_key'])
-                ### print "removeKeyResult:", h2o.dump_json(removeKeyResult)
+                    print "Deleting key in H2O so we get it from S3 (if ec2) or nfs again.", \
+                          "Otherwise it would just parse the cached key."
+                    storeView = h2o.nodes[0].store_view()
+                    ### print "storeView:", h2o.dump_json(storeView)
+                    print "Removing", parseKey['source_key']
+                    removeKeyResult = h2o.nodes[0].remove_key(key=parseKey['source_key'])
+                    ### print "removeKeyResult:", h2o.dump_json(removeKeyResult)
 
-            # sticky ports?
-            h2o.tear_down_cloud()
-            time.sleep(5)
+                # sticky ports?
+                h2o.tear_down_cloud()
+                time.sleep(tryJvms * 5)
 
 if __name__ == '__main__':
     h2o.unit_main()
