c6db8d7d97eb6fb5e5d48663528614ca2c0b3045
Kevin Normoyle <kevin@0xdata.com>
2013-04-05 18:10:05 -0700

    test kddcup_1999.data

---
+++ py/testdir_hosts/test_rf_kddcup_1999.py
@@ -0,0 +1,67 @@
+import unittest, sys, time
+sys.path.extend(['.','..','py'])
+
+import h2o_cmd, h2o, h2o_hosts
+import h2o_browse as h2b, h2o_import as h2i
+
+# Uses your username specific json: pytest_config-<username>.json
+# copy pytest_config-simple.json and modify to your needs.
+class Basic(unittest.TestCase):
+    def tearDown(self):
+        h2o.check_sandbox_for_errors()
+
+    @classmethod
+    def setUpClass(cls):
+        h2o_hosts.build_cloud_with_hosts(1)
+
+    @classmethod
+    def tearDownClass(cls):
+        ### time.sleep(3600)
+        h2o.tear_down_cloud()
+
+    def test_rf_kddcup_1999(self):
+        # since we'll be waiting, pop a browser
+        h2b.browseTheCloud()
+
+        importFolderPath = '/home/0xdiag/datasets'
+        h2i.setupImportFolder(None, importFolderPath)
+        csvFilename = 'kddcup_1999.data'
+
+        for trials in range(2):
+            parseKey = h2i.parseImportFolderFile(None, csvFilename, importFolderPath, timeoutSecs=300)
+            print csvFilename, 'parse time:', parseKey['response']['time']
+            print "Parse result['destination_key']:", parseKey['destination_key']
+            inspect = h2o_cmd.runInspect(None,parseKey['destination_key'])
+
+            print "\n" + csvFilename
+            start = time.time()
+
+            kwargs = {
+                'response_variable': 'classifier',
+                'ntree': 50,
+                'gini': 0,
+                'class_weights': None,
+                'stratify': 0,
+                'features': None,
+                'ignore': None,
+                'sample': 67,
+                'bin_limit': 1024,
+                'depth': 2147483647,
+                'seed': 784834182943470027,
+                'parallel': 1,
+                'exclusive_split_limit': None,
+                }
+            
+            if trials == 2:
+                kwargs['out_of_bag_error_estimate'] = None
+            else:
+                kwargs['out_of_bag_error_estimate'] = trials
+
+            RFview = h2o_cmd.runRFOnly(trees=50,parseKey=parseKey, 
+                timeoutSecs=300, retryDelaySecs=1.0, **kwargs)
+            print "RF end on ", csvFilename, 'took', time.time() - start, 'seconds'
+
+if __name__ == '__main__':
+    h2o.unit_main()
+
+
