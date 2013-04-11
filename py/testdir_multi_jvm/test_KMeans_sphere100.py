766c187a43c480b1bce770f20a68e00bfde5851b
Kevin Normoyle <kevin@0xdata.com>
2013-04-10 17:14:19 -0700

    some updates for changing params in the test

--- py/testdir_multi_jvm/test_KMeans_sphere100.py
+++ py/testdir_multi_jvm/test_KMeans_sphere100.py
@@ -10,25 +10,28 @@ import math
 # he offers the exact solution: http://stackoverflow.com/questions/918736/random-number-generator-that-produces-a-power-law-distribution/918782#918782
 # In spherical coordinates, taking advantage of the sampling rule:
 # http://stackoverflow.com/questions/2106503/pseudorandom-number-generator-exponential-distribution/2106568#2106568
-global CLUSTERS, SPHERE_PTS
 CLUSTERS = 3
-SPHERE_PTS = 10000
-ALLOWED_DELTA = 1
+SPHERE_PTS = 1000
+RANDOMIZE_SPHERE_PTS = False
+CENTER_DELTA = 1
+DIMENSIONS = 2 # or 3
+JUMP_RANDOM_ALL_DIRS = False
 
 def get_xyz_sphere(R):
-    phi = random.uniform(0, 2 * math.pi)
-    costheta = random.uniform(-1,1)
     u = random.random() # 0 to 1
-    theta = math.acos(costheta)
     r = R * (u ** (1.0/3))
 
+    costheta = random.uniform(-1,1)
+    theta = math.acos(costheta)
+
+    phi = random.uniform(0, 2 * math.pi)
     # now you have a (r, theta, phi) group which can be transformed to (x, y, z) 
-    # in the usual way
     x = r * math.sin(theta) * math.cos(phi)
     y = r * math.sin(theta) * math.sin(phi)
-    z =  r * math.cos(theta) 
-    ### print [z,y,z]
-    return [x,y,z]
+    z = r * math.cos(theta) 
+    xyz = [x, y, z]
+    print xyz[:DIMENSIONS]
+    return xyz[:DIMENSIONS]
 
 def write_spheres_dataset(csvPathname, CLUSTERS, n):
     dsf = open(csvPathname, "w+")
@@ -47,24 +50,29 @@ def write_spheres_dataset(csvPathname, CLUSTERS, n):
     totalRows = 0
     for sphereCnt in range(CLUSTERS):
         R = 10 * (sphereCnt+1)
-        if (1==0):
-            jump = random.randint(3*R,(3*R)+9)
-            xyzChoice = random.randint(0,2)
+        if JUMP_RANDOM_ALL_DIRS:
+            jump = random.randint(10*R,(10*R)+10)
+            xyzChoice = random.randint(0,DIMENSIONS-1)
         else:
-            print "FIX! temporarily limiting the center jumps to z dimension for easy understanding"
-            jump = 3*R
-            xyzChoice = 2
+            jump = 10*R
+            if DIMENSIONS==3:
+                # limit jumps to z
+                xyzChoice = 2
+            else:
+                # limit jumps to x
+                xyzChoice = 0
 
-        newOffset = [0,0,0]
+        zeroes = [0] * DIMENSIONS
+        newOffset = zeroes
         newOffset[xyzChoice] = jump
 
         # figure out the next center
         if currentCenter is None:
-            currentCenter = [0,0,0]
+            currentCenter = zeroes
         else:
             lastCenter = currentCenter
             currentCenter  = [a+b for a,b in zip(currentCenter, newOffset)] 
-            if (sum(currentCenter) - sum(lastCenter) < (len(currentCenter)* ALLOWED_DELTA)):
+            if (sum(currentCenter) - sum(lastCenter) < (len(currentCenter)* CENTER_DELTA)):
                 print "ERROR: adjacent centers are too close for our sort algorithm"
                 print "currentCenter:", currentCenter, "lastCenter:", lastCenter
                 raise Exception
@@ -72,8 +80,13 @@ def write_spheres_dataset(csvPathname, CLUSTERS, n):
         centersList.append(currentCenter)
 
         # build a sphere at that center
-        # pick a random # of points, from .5n to 1.5n
-        numPts = random.randint(int(.5*n), int(1.5*n))
+        # fixed number of pts?
+        if RANDOMIZE_SPHERE_PTS:
+            # pick a random # of points, from .5n to 1.5n
+            numPts = random.randint(int(.5*n), int(1.5*n))
+        else:
+            numPts = n
+
         print "currentCenter:", currentCenter, "R:", R, "numPts", numPts
         for i in range(numPts):
             xyz = get_xyz_sphere(R)
@@ -122,7 +135,12 @@ class Basic(unittest.TestCase):
 
         # try 5 times, to see if all inits by h2o are good
         for trial in range(3):
-            kwargs = {'k': CLUSTERS, 'epsilon': 1e-6, 'cols': None, 'destination_key': 'syn_spheres100.hex'}
+            kwargs = {
+                'k': CLUSTERS, 
+                'epsilon': 1e-6, 
+                'cols': None, 
+                'destination_key': 'syn_spheres100.hex'
+            }
             timeoutSecs = 100
             start = time.time()
             kmeans = h2o_cmd.runKMeansOnly(parseKey=parseKey, timeoutSecs=timeoutSecs, **kwargs)
@@ -131,6 +149,7 @@ class Basic(unittest.TestCase):
                 "%d pct. of timeout" % ((elapsed/timeoutSecs) * 100)
 
             kmeansResult = h2o_cmd.runInspect(key='syn_spheres100.hex')
+            print h2o.dump_json(kmeansResult)
 
             ### print h2o.dump_json(kmeans)
             ### print h2o.dump_json(kmeansResult)
@@ -138,6 +157,7 @@ class Basic(unittest.TestCase):
 
             # cluster centers can return in any order
             clusters = kmeansResult['KMeansModel']['clusters']
+
             # the way we create the centers above, if we sort on the sum of xyz
             # we should get the order the same as when they were created.
             # to be safe, we'll sort the centers that were generated too, the same way
@@ -146,22 +166,23 @@ class Basic(unittest.TestCase):
             ### print clustersSorted
 
             print "\nh2o result, centers (sorted by key=sum)"
-            s = '{0:6.2f} {1:6.2f} {2:6.2f}'
+            cf = '{0:6.2f}'
             for c in clustersSorted:
-                print s.format(*c)
+                print ' '.join(map(cf.format,c))
+
             print "\ngenerated centers (sorted by key=sum)"
             for c in centersSorted:
-                print s.format(*c)
+                print ' '.join(map(cf.format,c))
             
             for i,center in enumerate(centersSorted):
                 # Doing the compare of gen'ed/actual centers is kind of a hamming distance problem.
                 # Assuming that the difference between adjacent sums of all center values, 
                 # is greater than 2x the sum of all max allowed variance on each value, 
                 # Then the sums will be unique and non-overlapping with allowed variance.
-                # So a sort of the centers, keyed on sum of all values for a center
+                # So a sort of the centers, keyed on sum of all values for a center.
                 # will create an ordering that can be compared. 
                 # sort gen'ed and actual separately.
-                # A check on the adjacent center constraint (with allowed variance) is done while gen'ing)
+                # Adjacent center hamming distance check is done during gen above.
                 a = center
                 b = clustersSorted[i]
                 print "\nexpected:", a
@@ -171,11 +192,11 @@ class Basic(unittest.TestCase):
                 iStr = str(i)
 
                 emsg = aStr+" != "+bStr+". Sorted cluster center "+iStr+" x not correct."
-                self.assertAlmostEqual(a[0], b[0], delta=ALLOWED_DELTA, msg=emsg)
+                self.assertAlmostEqual(a[0], b[0], delta=CENTER_DELTA, msg=emsg)
                 emsg = aStr+" != "+bStr+". Sorted cluster center "+iStr+" y not correct."
-                self.assertAlmostEqual(a[1], b[1], delta=ALLOWED_DELTA, msg=emsg)
+                self.assertAlmostEqual(a[1], b[1], delta=CENTER_DELTA, msg=emsg)
                 emsg = aStr+" != "+bStr+". Sorted cluster center "+iStr+" z not correct."
-                self.assertAlmostEqual(a[2], b[2], delta=ALLOWED_DELTA, msg=emsg)
+                self.assertAlmostEqual(a[2], b[2], delta=CENTER_DELTA, msg=emsg)
 
             print "Trial #", trial, "completed"
 
