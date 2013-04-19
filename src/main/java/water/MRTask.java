122a6d28a8273ff0ed6a3107be12497dcfd0ede1
cliffclick <cliffc@acm.org>
2013-04-19 14:57:06 -0700

    MRTask now forks once (instead of twice) and does the child work directly.

--- src/main/java/water/MRTask.java
+++ src/main/java/water/MRTask.java
@@ -1,4 +1,5 @@
 package water;
+
 import jsr166y.CountedCompleter;
 import jsr166y.ForkJoinPool.ManagedBlocker;
 
@@ -42,24 +43,26 @@ public abstract class MRTask extends DRemoteTask {
       _rite = r; r._reservedMem = 0;
       _left._hi = mid;          // Reset mid-point
       _rite._lo = mid;          // Also set self mid-point
-      setPendingCount(2);
-      // compute min. memory required to run the right branch in parallel
-      // min memory equals to the max memory used if the right branch will be executed single threaded (but in parallel with our left branch)
-      // assuming all memory is kept in the tasks and it is halved by reduce operation, the min memory is proportional to the depth of the right subtree.
+      setPendingCount(1);
+      // Compute min. memory required to run the right branch in parallel.  Min
+      // memory equals to the max memory used if the right branch will be
+      // executed single threaded (but in parallel with our left branch).
+      // Assuming all memory is kept in the tasks and it is halved by reduce
+      // operation, the min memory is proportional to the depth of the right
+      // subtree.
       long reqMem = (log2(_hi - mid)+3)*memOverheadPerChunk();
       if(MemoryManager.tryReserveTaskMem(reqMem)){
         _reservedMem += reqMem;   // Remember the amount of reserved memory to free it later.
         _left.fork();             // Runs in another thread/FJ instance
-        _rite.fork();             // Runs in another thread/FJ instance
       } else {
         _left.compute2();
-        _rite.compute2();
       }
+      _rite.compute2();         // Runs in THIS F/J thread
     } else {
       if( _hi > _lo )           // Single key?
         map(_keys[_lo]);        // Get it, run it locally
+      tryComplete();            // And this task is complete
     }
-    tryComplete();              // And this task is complete
   }
 
   private final void returnReservedMemory(){
@@ -73,7 +76,6 @@ public abstract class MRTask extends DRemoteTask {
     if( _left != null ) reduceAlsoBlock(_left); _left = null;
     if( _rite != null ) reduceAlsoBlock(_rite); _rite = null;
     returnReservedMemory();
-
   }
   @Override public final boolean onExceptionalCompletion(Throwable ex, CountedCompleter caller ) {
     _left = null;
