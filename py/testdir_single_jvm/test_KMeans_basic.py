6bb7494b36dc7f71372463a4131bc5fbab501c79
Kevin Normoyle <kevin@0xdata.com>
2013-05-16 12:30:00 -0700

    adding some kmeans score and apply use, and printing of results

--- py/testdir_single_jvm/test_KMeans_basic.py
+++ py/testdir_single_jvm/test_KMeans_basic.py
@@ -2,10 +2,28 @@ import os, json, unittest, time, shutil, sys
 sys.path.extend(['.','..','py'])
 import h2o, h2o_cmd, h2o_kmeans, h2o_hosts
 
+def show_results(csvPathname, parseKey, model_key, centers, destination_key):
+    kmeansApplyResult = h2o.nodes[0].kmeans_apply(
+        data_key=parseKey['destination_key'], model_key=model_key, 
+        destination_key=destination_key)
+    # print h2o.dump_json(kmeansApplyResult)
+    inspect = h2o_cmd.runInspect(None, destination_key)
+    h2o_cmd.infoFromInspect(inspect, csvPathname)
+
+    kmeansScoreResult = h2o.nodes[0].kmeans_score(
+        key=parseKey['destination_key'], model_key=model_key)
+    score = kmeansScoreResult['score']
+    rows_per_cluster = score['rows_per_cluster']
+    sqr_error_per_cluster = score['sqr_error_per_cluster']
+
+    for i,c in enumerate(centers):
+        print "\ncenters["+str(i)+"]: ", centers[i]
+        print "rows_per_cluster["+str(i)+"]: ", rows_per_cluster[i]
+        print "sqr_error_per_cluster["+str(i)+"]: ", sqr_error_per_cluster[i]
+
 class Basic(unittest.TestCase):
     def tearDown(self):
         h2o.check_sandbox_for_errors()
-
 
     @classmethod
     def setUpClass(cls):
@@ -26,12 +44,19 @@ class Basic(unittest.TestCase):
         csvPathname = h2o.find_file('smalldata/logreg' + '/' + csvFilename)
         parseKey = h2o_cmd.parseFile(csvPathname=csvPathname, key2=csvFilename + ".hex")
 
-        kwargs = {'k': 1, 'epsilon': 1e-6, 'cols': None, 'destination_key': 'benign_k.hex'}
-        kmeans = h2o_cmd.runKMeansOnly(parseKey=parseKey, timeoutSecs=5, **kwargs)
-        kmeansResult = h2o_cmd.runInspect(key='benign_k.hex')
-        print h2o.dump_json(kmeans)
-        print h2o.dump_json(kmeansResult)
-        h2o_kmeans.simpleCheckKMeans(self, kmeans, **kwargs)
+        # loop, to see if we get same centers
+        for i in range(2):
+            kwargs = {'k': 3, 'epsilon': 1e-6, 'cols': None, 'destination_key': 'benign_k.hex'}
+            kmeans = h2o_cmd.runKMeansOnly(parseKey=parseKey, timeoutSecs=5, **kwargs)
+            model_key = kmeans['destination_key']
+            kmeansResult = h2o_cmd.runInspect(key=model_key)
+            centers = kmeansResult['KMeansModel']['clusters']
+
+            ## print h2o.dump_json(kmeans)
+            ## print h2o.dump_json(kmeansResult)
+            h2o_kmeans.simpleCheckKMeans(self, kmeans, **kwargs)
+            show_results(csvPathname, parseKey, model_key, centers, 'd')
+
 
     def test_C_kmeans_prostate(self):
         csvFilename = "prostate.csv"
@@ -39,12 +64,18 @@ class Basic(unittest.TestCase):
         csvPathname = h2o.find_file('smalldata/logreg' + '/' + csvFilename)
         parseKey = h2o_cmd.parseFile(csvPathname=csvPathname, key2=csvFilename + ".hex")
 
-        kwargs = {'k': 1, 'epsilon': 1e-6, 'cols': None, 'destination_key': 'prostate_k.hex'}
-        kmeans = h2o_cmd.runKMeansOnly(parseKey=parseKey, timeoutSecs=5, **kwargs)
-        kmeansResult = h2o_cmd.runInspect(key='prostate_k.hex')
-        print h2o.dump_json(kmeans)
-        print h2o.dump_json(kmeansResult)
-        h2o_kmeans.simpleCheckKMeans(self, kmeans, **kwargs)
+        # loop, to see if we get same centers
+        for i in range(2):
+            kwargs = {'k': 3, 'epsilon': 1e-6, 'cols': None, 'destination_key': 'prostate_k.hex'}
+            kmeans = h2o_cmd.runKMeansOnly(parseKey=parseKey, timeoutSecs=5, **kwargs)
+            model_key = kmeans['destination_key']
+            kmeansResult = h2o_cmd.runInspect(key=model_key)
+            centers = kmeansResult['KMeansModel']['clusters']
+
+            ## print h2o.dump_json(kmeans)
+            ## print h2o.dump_json(kmeansResult)
+            h2o_kmeans.simpleCheckKMeans(self, kmeans, **kwargs)
+            show_results(csvPathname, parseKey, model_key, centers, 'd')
 
 if __name__ == '__main__':
     h2o.unit_main()
