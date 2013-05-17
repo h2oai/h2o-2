e9b0fb20e578467fc37fd8cbccc8fe19490d2111
Tomas Nykodym <tomas.nykodym@gmail.com>
2013-05-17 11:09:33 -0700

    Updated Summary html.

--- src/main/java/water/api/SummaryPage.java
+++ src/main/java/water/api/SummaryPage.java
@@ -28,36 +28,39 @@ public class SummaryPage extends Request {
           String cname = o.get("name").getAsString();
           sb.append("<div class='table' id='col_" + cname + "' style='border-top-style:solid;'><h4>Column: " + cname + "</h4>\n");
           if(o.has("min") && o.has("max")){
-            sb.append("<h5>Base Stats</h5><table>");
-            StringBuilder minRow = new StringBuilder("<tr><th>min[5]</th>");
-            StringBuilder maxRow = new StringBuilder("<tr><th>max[5]</th>");
+            StringBuilder minRow = new StringBuilder("<tr><th>&mu;</th><td>" + Utils.p2d(o.get("mean").getAsDouble())+"</td><th style='border-left-style:solid; borde-left:1px;border-left-color:#ddd;'>min[5]</th>");
+            StringBuilder maxRow = new StringBuilder("<tr><th>&sigma;</th><td>" + Utils.p2d(o.get("sigma").getAsDouble()) + "</td><th style='border-left-style:solid; borde-left:1px;border-left-color:#ddd;'>max[5]</th>");
             Iterator<JsonElement> iter = o.get("min").getAsJsonArray().iterator();
-            while(iter.hasNext())minRow.append("<td>" + Utils.p2d(iter.next().getAsDouble()) + "</td>");
-            minRow.append("</tr>");
+            int nCols = 3;
+            while(iter.hasNext()){
+              ++nCols;
+              minRow.append("<td>" + Utils.p2d(iter.next().getAsDouble()) + "</td>");
+            }
             iter = o.get("max").getAsJsonArray().iterator();
             while(iter.hasNext())maxRow.append("<td>" + Utils.p2d(iter.next().getAsDouble()) + "</td>");
+            StringBuilder firstRow = new StringBuilder("<tr><th colspan='" + nCols + "' style='text-align:center;'>Base Stats</th>");
+            if(o.has("percentiles")){
+              firstRow.append("<th colspan='12' style='text-align:center;border-left-style:solid; borde-left:1px;border-left-color:#ddd;'>Percentiles</th>");
+              JsonObject percentiles = o.get("percentiles").getAsJsonObject();
+              JsonArray thresholds = percentiles.get("thresholds").getAsJsonArray();
+              JsonArray values = percentiles.get("values").getAsJsonArray();
+              Iterator<JsonElement> tIter = thresholds.iterator();
+              Iterator<JsonElement> vIter = values.iterator();
+              minRow.append("<th style='border-left-style:solid; borde-left:1px;border-left-color:#ddd;'>Threshold</th>");
+              maxRow.append("<th style='border-left-style:solid; borde-left:1px;border-left-color:#ddd;'>Value</th>");
+              while(tIter.hasNext() && vIter.hasNext()){
+                minRow.append("<td>" + tIter.next().getAsString() + "</td>");
+                maxRow.append("<td>" + Utils.p2d(vIter.next().getAsDouble()) + "</td>");
+              }
+            }
+            firstRow.append("</tr>");
+            minRow.append("</tr>");
             maxRow.append("</tr>");
             sb.append("<table>");
+            sb.append(firstRow.toString());
             sb.append(minRow.toString());
             sb.append(maxRow.toString());
-            sb.append("<tr><th>&mu;</th><td>" + Utils.p2d(o.get("mean").getAsDouble()) + "</td><td></td><td></td><td></td><td></td>" + "</tr><tr><th>&sigma;</th><td>" + Utils.p2d(o.get("sigma").getAsDouble()) + "</td><td></td><td></td><td></td><td></td></tr>");
-          }
-          sb.append("</table>");
-          if(o.has("percentiles")){
-            JsonObject percentiles = o.get("percentiles").getAsJsonObject();
-            JsonArray thresholds = percentiles.get("thresholds").getAsJsonArray();
-            JsonArray values = percentiles.get("values").getAsJsonArray();
-            Iterator<JsonElement> tIter = thresholds.iterator();
-            Iterator<JsonElement> vIter = values.iterator();
-            StringBuilder t = new StringBuilder("<tr><th>Threshold</th>");
-            StringBuilder v = new StringBuilder("<tr><th>Value</th>");
-            while(tIter.hasNext() && vIter.hasNext()){
-              t.append("<td>" + tIter.next().getAsString() + "</td>");
-              v.append("<td>" + Utils.p2d(vIter.next().getAsDouble()) + "</td>");
-            }
-            t.append("</tr>");
-            v.append("</tr>");
-            sb.append("<h5>Percentiles</h5><table>" + t.toString() + v.toString() + "</table>");
+            sb.append("</table>");
           }
           sb.append("<h5>Histogram</h5>");
           JsonObject histo = o.get("histogram").getAsJsonObject();
