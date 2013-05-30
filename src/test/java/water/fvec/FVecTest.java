139ac8de6aeb0466683de66091b5c83bfc8c8854
cliffclick <cliffc@acm.org>
2013-05-29 17:45:00 -0700

    Starting on a WordCount for fluid vectors

--- src/test/java/water/fvec/FVecTest.java
+++ src/test/java/water/fvec/FVecTest.java
@@ -2,9 +2,12 @@ package water.fvec;
 
 import static org.junit.Assert.*;
 import java.io.File;
+import java.util.Arrays;
 import org.junit.*;
 import water.*;
+import water.nbhm.NonBlockingHashMap;
 import water.util.Log;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FVecTest extends TestUtil {
 
@@ -12,8 +15,7 @@ public class FVecTest extends TestUtil {
 
   @Test public void testBasicCRUD() {
     // Make and insert a FileVec to the global store
-    //File file = TestUtil.find_test_file("./smalldata/cars.csv");
-    File file = TestUtil.find_test_file("./target/h2o.jar");
+    File file = TestUtil.find_test_file("./smalldata/cars.csv");
     Key key = NFSFileVec.make(file);
     NFSFileVec nfs=DKV.get(key).get();
 
@@ -26,7 +28,7 @@ public class FVecTest extends TestUtil {
     UKV.remove(key);
   }
 
-  public static class ByteHisto extends MRTask2<ByteHisto> {
+  private static class ByteHisto extends MRTask2<ByteHisto> {
     public int[] _x;
     // Count occurrences of bytes
     @Override public void map( long start, int len, BigVector bv ) {
@@ -40,4 +42,88 @@ public class FVecTest extends TestUtil {
         _x[i] += bh._x[i];
     }
   }
+
+
+  @Test public void testWordCount() {
+    File file = TestUtil.find_test_file("../wiki/enwiki-latest-pages-articles.xml");
+    Key key = NFSFileVec.make(file);
+    NFSFileVec nfs=DKV.get(key).get();
+    NonBlockingHashMap<VStr,VStr> words = new WordCount().invoke(nfs)._words;
+    Log.unwrap(System.err,words.toString());
+    UKV.remove(key);
+  }
+
+  private static class WordCount extends MRTask2<WordCount> {
+    public static NonBlockingHashMap<VStr,VStr> WORDS;
+    public NonBlockingHashMap<VStr,VStr> _words;
+    @Override public void init() { WORDS = new NonBlockingHashMap(); }
+    @Override public void map( long start, int len, BigVector bv ) {
+      _words = WORDS;
+      long i = start;
+      if( start > 0 )           // Not on the 1st chunk...
+        while( Character.isLetter((char)bv.at(i)) ) i++; // skip any partial word from prior
+      VStr vs = new VStr();
+      while( i<start+len ) {    // Till we run dry
+        int c = (int)bv.at(i);  // Load a char
+        if( Character.isLetter(c) ) { // In a word?
+          vs.append(c);         // Append char
+        } else {                // Out of a word?
+          if( vs._len > 0 ) {   // Have a word?
+            VStr vs2 = WORDS.putIfAbsent(vs,vs);
+            if( vs2 == null ) {
+              vs = new VStr();  // If actually inserted, need new VStr
+            } else {
+              vs2.inc();        // Inc count on added word, 
+              vs._len = 0;      // and re-use VStr
+            }
+          }
+        }
+        i++;
+      }
+    }
+
+    @Override public void reduce( WordCount wc ) {
+      if( _words != wc._words )
+        throw H2O.unimpl();
+    }
+
+    @Override public AutoBuffer write(AutoBuffer bb) { 
+      throw H2O.unimpl();
+    }
+    @Override public WordCount read(AutoBuffer bb) { 
+      throw H2O.unimpl();
+    }
+    @Override public void copyOver(DTask that) { 
+      throw H2O.unimpl();
+    }    
+  }
+
+  // A word, and a count of occurences
+  private static class VStr {
+    byte[] _mem = new byte[1];
+    int _len = 0;
+    final AtomicInteger _cnt = new AtomicInteger(1);
+    public void append( int c ) {
+      if( _len == _mem.length ) 
+        _mem = Arrays.copyOf(_mem,_len<<1);
+      _mem[_len++] = (byte)c;
+    }
+    public String toString() { return new String(_mem,0,_len)+"="+_cnt.get(); }
+    void inc() { _cnt.addAndGet(1); }
+    @Override public boolean equals(Object o){
+      if(!(o instanceof VStr)) return false;
+      VStr vs = (VStr)o;
+      if( vs._len != _len)return false;
+      for(int i = 0; i < _len; ++i)
+        if(_mem[i] != vs._mem[i]) return false;
+      return true;
+    }
+    @Override public int hashCode() {
+     int hash = 0;
+     for(int i = 0; i < _len; ++i)
+       hash = 31 * hash + _mem[i];
+     return hash;
+    }
+  }
+
 }
