7cd8132461d8ea44978d820a11459191c94fa2aa
Tom Kraljevic <tomk@0xdata.com>
2013-06-03 23:22:39 -0700

    More windows fixes.

--- installer/windows/h2o.bat
+++ installer/windows/h2o.bat
@@ -1,11 +1,10 @@
-# Set up variables and environment variables.
+REM Set up variables and environment variables.
 for %%F in ("%0") do set dirname=%%~dpF
-echo %dirname%
 set jre_version=jre1.7.0_21-windows-x64
-set JAVA_HOME=%dirname%windows\%jre_version%\java-windows
-set H2O_DIR=%dirname%
+set JAVA_HOME=%dirname%..\windows\%jre_version%\java-windows
+set H2O_DIR=%dirname%..
 set jar_file=H2OLauncher.jar
 
-# Run H2OLauncher in the background. 
-chdir $H2O_DIR
+REM Run H2OLauncher in the background.
+chdir "%H2O_DIR%"
 start "H2OLauncher" /B "%JAVA_HOME%\bin\java.exe" -ea -Xmx128m -jar %jar_file%
