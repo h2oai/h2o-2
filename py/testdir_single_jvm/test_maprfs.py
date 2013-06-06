94ef33c62f38f842a669b6096f6f9813632de635
Kevin Normoyle <kevin@0xdata.com>
2013-06-06 11:49:23 -0700

    remove the -cp experiment

--- py/testdir_single_jvm/test_maprfs.py
+++ py/testdir_single_jvm/test_maprfs.py
@@ -19,13 +19,13 @@ class Basic(unittest.TestCase):
         # hdfs_config='/opt/mapr/conf/mapr-clusters.conf', 
         if (localhost):
             h2o.build_cloud(1, 
-                java_extra_args = "-cp /opt/mapr/hadoop/hadoop-0.20.2/conf:/opt/mapr/hadoop/hadoop-0.20.2/lib/hadoop-0.20.2-dev-core.jar:/opt/mapr/hadoop/hadoop-0.20.2/lib/maprfs-0.1.jar:.:/opt/mapr/hadoop/hadoop-0.20.2/lib/commons-logging-1.0.4.jar:/opt/mapr/hadoop/hadoop-0.20.2/lib/zookeeper-3.3.6.jar:/opt/mapr/hadoop/hadoop-0.20.2/lib/log4j-1.2.15.jar",
+                # java_extra_args = "-cp /opt/mapr/hadoop/hadoop-0.20.2/conf:/opt/mapr/hadoop/hadoop-0.20.2/lib/hadoop-0.20.2-dev-core.jar:/opt/mapr/hadoop/hadoop-0.20.2/lib/maprfs-0.1.jar:.:/opt/mapr/hadoop/hadoop-0.20.2/lib/commons-logging-1.0.4.jar:/opt/mapr/hadoop/hadoop-0.20.2/lib/zookeeper-3.3.6.jar:/opt/mapr/hadoop/hadoop-0.20.2/lib/log4j-1.2.15.jar",
                 use_maprfs=True, 
                 hdfs_version='0.20.2mapr', 
                 hdfs_name_node='mr-0x1.0xdata.loc')
         else:
             h2o_hosts.build_cloud_with_hosts(1, 
-                java_extra_args = "-cp /opt/mapr/hadoop/hadoop-0.20.2/conf:/opt/mapr/hadoop/hadoop-0.20.2/lib/hadoop-0.20.2-dev-core.jar:/opt/mapr/hadoop/hadoop-0.20.2/lib/maprfs-0.1.jar:.:/opt/mapr/hadoop/hadoop-0.20.2/lib/commons-logging-1.0.4.jar:/opt/mapr/hadoop/hadoop-0.20.2/lib/zookeeper-3.3.6.jar:/opt/mapr/hadoop/hadoop-0.20.2/lib/log4j-1.2.15.jar",
+                # java_extra_args = "-cp /opt/mapr/hadoop/hadoop-0.20.2/conf:/opt/mapr/hadoop/hadoop-0.20.2/lib/hadoop-0.20.2-dev-core.jar:/opt/mapr/hadoop/hadoop-0.20.2/lib/maprfs-0.1.jar:.:/opt/mapr/hadoop/hadoop-0.20.2/lib/commons-logging-1.0.4.jar:/opt/mapr/hadoop/hadoop-0.20.2/lib/zookeeper-3.3.6.jar:/opt/mapr/hadoop/hadoop-0.20.2/lib/log4j-1.2.15.jar",
                 use_maprfs=True, 
                 hdfs_version='0.20.2mapr', 
                 hdfs_name_node='mr-0x1.0xdata.loc')
