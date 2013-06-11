0d3cfc052a1be8a45f4d6d9758e4d3777d7233ca
Tomas Nykodym <tomas.nykodym@gmail.com>
2013-06-10 17:21:15 -0700

    added python script to benchmark glm.

---
+++ py/testdir_hosts/glm_bench.py
@@ -0,0 +1,82 @@
+# this lets me be lazy..starts the cloud up like I want from my json, and gives me a browser
+# copies the jars for me, etc. Just hangs at the end for 10 minutes while I play with the browser
+import unittest
+import time,sys
+sys.path.extend(['.','..','py'])
+
+import h2o_cmd, h2o, h2o_hosts
+import h2o_browse as h2b
+import os
+import csv
+import time
+import socket
+
+
+
+csv_header = ('time','nodes#','dataset','y','x','family','alpha','lambda','n_folds','nLines','nCols','dof','nullDev','resDev','aic','auc','iterations','model_time','model_iterations','val_time','val_iterations','lsm_time')
+
+
+ec2_files = {'allstate':'s3n://h2o-datasets/allstate/train_set.zip','airlines':'s3n://h2o-airlines-unpacked/allyears.csv'}
+local_files = {'allstate':'hdfs://192.168.1.176/datasets/allstate.csv','airlines':'hdfs://192.168.1.176/datasets/airlines_all.csv'}
+
+def is_ec2():
+    return 'compute.internal' in socket.gethostname()
+
+class GLMBench(unittest.TestCase):
+    def tearDown(self):
+        h2o.check_sandbox_for_errors()
+        h2o.tear_down_cloud()
+    @classmethod
+    def setUpClass(cls):
+        print 'building cloud...'
+        h2o_hosts.build_cloud_with_hosts(use_hdfs=True,base_port=54321)
+        if is_ec2():
+            cls.files = ec2_files
+        else:
+            cls.files = local_files
+
+    def run_glms(self,file,configs):
+        output = None
+        if not os.path.exists('glmbench.csv'):
+            output = open('glmbench.csv','w')
+            output.write(','.join(csv_header)+'\n')
+        else:
+            output = open('glmbench.csv','a')
+        csvWrt = csv.DictWriter(output, fieldnames=csv_header, restval=None, dialect='excel', extrasaction='ignore',delimiter=',')
+        try:
+            k = self.parse_file(file)
+            try:
+                for kwargs in configs:
+                    res = h2o.nodes[0].GLM(k, timeoutSecs=6000000, **kwargs)
+                    glm = res['GLMModel']
+                    coefs = glm['coefficients']
+                    coefs = glm['coefficients']
+                    print 'model computed in',res['computation_time']
+                    max_len = 0
+                    val = glm['validations'][0]
+                    row = {'time':time.asctime(),'nodes#':len(h2o.nodes)}
+                    row.update(kwargs)
+                    row.update(glm)
+                    row.update(val)
+                    csvWrt.writerow(row)
+            finally:
+                h2o.nodes[0].remove_key(k)
+        finally:
+            output.close()
+                   
+    def parse_file(self,f):
+        v = h2o.nodes[0].import_hdfs(self.files[f])['succeeded'][0]
+        return h2o.nodes[0].parse(v['key'],timeoutSecs=3600)['destination_key']
+                   
+    def test_allstate(self):
+        self.run_glms('allstate',[{'y':'Claim_Amount','lambda':l,'alpha':a,'family':'poisson','n_folds':10} for l in (1e-1,1e-2,1e-3,1e-4,1e-5) for a in (1.0,0.5,0.0)])
+
+    def test_airlines(self):
+        self.run_glms('airlines',[{'y':'IsArrDelayed','x':'0,1,2,3,4,5,6,7,8,9,12,16,17,18','lambda':l,'alpha':a,'family':'binomial','n_folds':10,'case':1}
+                                          for l in (0.035,0.025,0.01,0.0075,0.0050,0.0025,0.0010,0.00075,0.00050,0.00025,0.0001,0.000075,0.00005,0.000025,0.00001,0.000001)
+                                          for a in (1.0,0.9,0.75,0.5,0.25,0.1,0.0)])
+
+if __name__ == '__main__':
+    h2o.unit_main()
+
+
