d44ce871bd6f1ec1821725aaa9096b04d2a11182
mmalohlava <michal.malohlava@gmail.com>
2013-06-24 11:12:06 -0700

    Explicit UTC timezone removed. Now log date format is using locale settings.

--- src/main/java/water/Timer.java
+++ src/main/java/water/Timer.java
@@ -2,7 +2,6 @@ package water;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
 /**
  * Simple Timer class.
  **/
@@ -10,17 +9,15 @@ public class Timer {
 
   /** SimpleDataFormat is not thread safe. To avoid constructing them repeatedly we store them into thread
    * local variables. */
-  private static final ThreadLocal<SimpleDateFormat> utcFormat = new ThreadLocal<SimpleDateFormat>() {
+  private static final ThreadLocal<SimpleDateFormat> dateFormat = new ThreadLocal<SimpleDateFormat>() {
     @Override protected SimpleDateFormat initialValue() {
       SimpleDateFormat format = new SimpleDateFormat("dd-MMM hh:mm:ss.SSS");
-      format.setTimeZone(TimeZone.getTimeZone("UTC"));
       return format;
     }
   };
   private static final ThreadLocal<SimpleDateFormat> utcShortFormat = new ThreadLocal<SimpleDateFormat>() {
     @Override protected SimpleDateFormat initialValue() {
       SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss.SSS");
-      format.setTimeZone(TimeZone.getTimeZone("UTC"));
       return format;
     }
   };
@@ -34,11 +31,11 @@ public class Timer {
    * string along with the time of creation in date format. */
   public String toString() {
     final long now = System.currentTimeMillis();
-    return PrettyPrint.msecs(now - _start, false) + " (Wall: " + utcFormat.get().format(new Date(now)) + ") ";
+    return PrettyPrint.msecs(now - _start, false) + " (Wall: " + dateFormat.get().format(new Date(now)) + ") ";
   }
 
   /** return the start time of this timer.**/
-  public String startAsString() { return utcFormat.get().format(new Date(_start)); }
+  public String startAsString() { return dateFormat.get().format(new Date(_start)); }
   /** return the start time of this timer.**/
   public String startAsShortString() { return utcShortFormat.get().format(new Date(_start)); }
 
