5b4aaf791a4947bfbc602c96e69f7fbc87bdf978
Kevin Normoyle <kevin@0xdata.com>
2013-07-02 15:52:23 -0700

    new test (fail) for single line header file during multi-file parse https://0xdata.atlassian.net/browse/HEX-731

---
+++ py/testdir_single_jvm/test_parse_multi_header_single.py
@@ -0,0 +1,136 @@
+import os, json, unittest, time, shutil, sys
+sys.path.extend(['.','..','py'])
+
+import h2o, h2o_cmd, h2o_hosts
+import h2o_browse as h2b
+import random
+
+# for test debug
+HEADER = True
+
+# Don't write headerData if None (for non-header files)
+# Don't write data if rowCount is None
+def write_syn_dataset(csvPathname, rowCount, headerData, rList):
+    dsf = open(csvPathname, "w+")
+    
+    if headerData is not None:
+        dsf.write(headerData + "\n")
+
+    if rowCount is not None:        
+        for i in range(rowCount):
+            # two choices on the input. Make output choices random
+            r = rList[random.randint(0,1)] + "," + str(random.randint(0,7))
+            dsf.write(r + "\n")
+    dsf.close()
+
+def rand_rowData(colCount):
+    rowData = [random.randint(0,7) for i in range(colCount)]
+    rowData1= ",".join(map(str,rowData))
+    rowData = [random.randint(0,7) for i in range(colCount)]
+    rowData2= ",".join(map(str,rowData))
+    # RF will complain if all inputs are the same
+    r = [rowData1, rowData2]
+    return r
+
+class Basic(unittest.TestCase):
+    def tearDown(self):
+        h2o.check_sandbox_for_errors()
+
+    @classmethod
+    def setUpClass(cls):
+        localhost = h2o.decide_if_localhost()
+        if (localhost):
+            h2o.build_cloud(2,java_heap_MB=1300,use_flatfile=True)
+        else:
+            h2o_hosts.build_cloud_with_hosts()
+
+    @classmethod
+    def tearDownClass(cls):
+        h2o.tear_down_cloud()
+    
+    def test_parse_multi_header_single(self):
+        SEED = random.randint(0, sys.maxint)
+        # if you have to force to redo a test
+        # SEED = 
+        random.seed(SEED)
+        print "\nUsing random seed:", SEED
+
+        SYNDATASETS_DIR = h2o.make_syn_dir()
+        csvFilename = "syn_ints.csv"
+        csvPathname = SYNDATASETS_DIR + '/' + csvFilename
+
+        headerData = "ID,CAPSULE,AGE,RACE,DPROS,DCAPS,PSA,VOL,GLEASON,output"
+
+        # cols must be 9 to match the header above, otherwise a different bug is hit
+        # extra output is added, so it's 10 total
+        tryList = [
+            (57, 300, 9, 'cA', 60),
+            ]
+
+        trial = 0
+        for (FILENUM, rowCount, colCount, key2, timeoutSecs) in tryList:
+            trial += 1
+            # FIX! should we add a header to them randomly???
+            print "Wait while", FILENUM, "synthetic files are created in", SYNDATASETS_DIR
+            rowxcol = str(rowCount) + 'x' + str(colCount)
+            totalCols = colCount + 1 # 1 extra for output
+            totalRows = 0
+            for fileN in range(FILENUM):
+                csvFilename = 'syn_' + str(fileN) + "_" + str(SEED) + "_" + rowxcol + '.csv'
+                csvPathname = SYNDATASETS_DIR + '/' + csvFilename
+                rList = rand_rowData(colCount)
+                write_syn_dataset(csvPathname, rowCount, headerData=None, rList=rList)
+                totalRows += rowCount
+
+            # create the header file
+            # can make it pass by not doing this
+            if HEADER:
+                csvFilename = 'syn_header_' + str(SEED) + "_" + rowxcol + '.csv'
+                csvPathname = SYNDATASETS_DIR + '/' + csvFilename
+                write_syn_dataset(csvPathname, None, headerData, rList)
+
+            # make sure all key names are unique, when we re-put and re-parse (h2o caching issues)
+            key = "syn_" + str(trial)
+            key2 = "syn_" + str(trial) + ".hex"
+
+            # DON"T get redirected to S3! (EC2 hack in config, remember!)
+            # use it at the node level directly (because we gen'ed the files.
+            # I suppose we could force the redirect state bits in h2o.nodes[0] to False, instead?:w
+            h2o.nodes[0].import_files(SYNDATASETS_DIR)
+            # use regex. the only files in the dir will be the ones we just created with  *fileN* match
+            print "Don't force header or comma selection"
+            start = time.time()
+            parseKey = h2o.nodes[0].parse('*'+rowxcol+'*', key2=key2, timeoutSecs=timeoutSecs)
+            print "parseKey['destination_key']: " + parseKey['destination_key']
+            print 'parse time:', parseKey['response']['time']
+
+            inspect = h2o_cmd.runInspect(None, parseKey['destination_key'])
+            h2o_cmd.infoFromInspect(inspect, csvPathname)
+            print "\n" + csvPathname, \
+                "    num_rows:", "{:,}".format(inspect['num_rows']), \
+                "    num_cols:", "{:,}".format(inspect['num_cols'])
+
+            # should match # of cols in header or ??
+            self.assertEqual(inspect['num_cols'], totalCols, 
+                "parse created result with the wrong number of cols %s %s" % (inspect['num_cols'], totalCols))
+            self.assertEqual(inspect['num_rows'], totalRows,
+                "parse created result with the wrong number of rows (header shouldn't count) %s %s" % \
+                (inspect['num_rows'], totalCols))
+
+            # put in an ignore param, that will fail unless headers were parsed correctly
+            if HEADER:
+                kwargs = {'sample': 75, 'depth': 25, 'ntree': 1, 'ignore': 'ID,CAPSULE'}
+            else:
+                kwargs = {'sample': 75, 'depth': 25, 'ntree': 1}
+
+            start = time.time()
+            rfv = h2o_cmd.runRFOnly(parseKey=parseKey, timeoutSecs=timeoutSecs, **kwargs)
+            elapsed = time.time() - start
+            print "%d pct. of timeout" % ((elapsed/timeoutSecs) * 100)
+            print "trial #", trial, "totalRows:", totalRows, "parse end on ", csvFilename, \
+                'took', time.time() - start, 'seconds'
+
+            h2o.check_sandbox_for_errors()
+
+if __name__ == '__main__':
+    h2o.unit_main()
