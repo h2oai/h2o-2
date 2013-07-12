f4bab1b4ac37f1c9128aa539d6c43740982de598
Kevin Normoyle <kevin@0xdata.com>
2013-07-12 15:54:55 -0700

    updated

--- py/testdir_single_jvm/test_fp_many_cols.py
+++ py/testdir_single_jvm/test_fp_many_cols.py
@@ -1,5 +1,5 @@
 import unittest
-import random, sys, time, os
+import random, sys, time, os, getpass
 sys.path.extend(['.','..','py'])
 
 import h2o, h2o_cmd, h2o_hosts, h2o_browse as h2b, h2o_import as h2i, h2o_exec as h2e
