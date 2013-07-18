4a97c40efcf8b686e3077865780a463df501df8c
Tom Kraljevic <tomk@tomk.net>
2013-07-18 00:12:30 -0700

    Add logging at startup about build info.

---
+++ src/main/java/water/AbstractBuildVersion.java
@@ -0,0 +1,8 @@
+package water;
+
+abstract public class AbstractBuildVersion {
+    abstract public String branchName();
+    abstract public String lastCommitHash();
+    abstract public String compiledOn();
+    abstract public String compiledBy();
+}
