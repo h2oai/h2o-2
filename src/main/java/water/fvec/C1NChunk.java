1da8c9f1b71bd624200bcdc9963e17fb49b1dcde
Cliff Click <cliffc@acm.org>
2013-07-21 13:50:03 -0700

    Minor miscounting in new C1xChunks

--- src/main/java/water/fvec/C1NChunk.java
+++ src/main/java/water/fvec/C1NChunk.java
@@ -6,8 +6,8 @@ import water.*;
 public class C1NChunk extends Chunk {
   static final int OFF=0;
   C1NChunk(byte[] bs) { _mem=bs; _start = -1; _len = _mem.length; }
-  @Override protected final long   at8_impl( int i ) { return 0xFF&_mem[i+OFF]; }
-  @Override protected final double atd_impl( int i ) { return 0xFF&_mem[i+OFF]; }
+  @Override protected final long   at8_impl( int i ) { return 0xFF&_mem[i]; }
+  @Override protected final double atd_impl( int i ) { return 0xFF&_mem[i]; }
   @Override boolean hasFloat() { return false; }
   @Override public AutoBuffer write(AutoBuffer bb) { return bb.putA1(_mem,_mem.length); }
   @Override public C1NChunk read(AutoBuffer bb) {
@@ -16,11 +16,11 @@ public class C1NChunk extends Chunk {
     _len = _mem.length;
     return this;
   }
-  public int get2(int off) { return UDP.get2(_mem,off+OFF); }
-  public int get4(int off) { return UDP.get4(_mem,off+OFF); }
+  public int get2(int off) { return UDP.get2(_mem,off); }
+  public int get4(int off) { return UDP.get4(_mem,off); }
   @Override boolean set8_impl(int i, long l) {
     if( !(0 <= l && l < 256) ) return false;
-    _mem[i+OFF] = (byte)l;
+    _mem[i] = (byte)l;
     return true; 
   }
   @Override boolean set8_impl(int i, double d) { return false; }
