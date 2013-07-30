3b5966f354a4a701e97670beedb7e2202ae99c57
Anqi Fu <anqi_fu@Anqis-MacBook-Pro.local>
2013-07-29 17:27:12 -0700

    Added a few RF test for R package

--- R/tests/test_R_RF_diff_ignore.R
+++ R/tests/test_R_RF_diff_ignore.R
@@ -14,7 +14,9 @@ h2o = new("H2OClient", ip="localhost", port=54321)
 
 # Test of random forest using iris data set, different classes
 iris.hex = importURL(h2o, "https://raw.github.com/0xdata/h2o/master/smalldata/iris/iris22.csv", "iris.hex")
-iris.rf = h2o.randomForest(y = "4", data = iris.hex, ntree = 50, depth = 100, classwt = c("Iris-versicolor"=20.0, "Iris-virginica"=30.0))
-print(iris.rf)
-iris.rf = h2o.randomForest(y = "5", data = iris.hex, ntree = 50, depth = 100 )
-print(iris.rf)
+h2o.randomForest(y = "4", data = iris.hex, ntree = 50, depth = 100)
+for(maxx in 0:3) {
+  myIgnore = as.character(seq(0, maxx))
+  iris.rf = h2o.randomForest(y = "4", x_ignore = myIgnore, data = iris.hex, ntree = 50, depth = 100)
+  print(iris.rf)
+}
