3a08cf06e9f79098df84c4b38a0271aa3264ef41
Kevin Normoyle <kevin@0xdata.com>
2013-08-02 00:29:11 -0700

    changed these all to just check for min >1 file in the import list, was check 8 before (and now they have dedicated subdirs, so that isn't always right for a min)

--- py/testdir_ec2_only/test_parse_covtype20x_loop_s3n_hdfs.py
+++ py/testdir_ec2_only/test_parse_covtype20x_loop_s3n_hdfs.py
@@ -43,7 +43,7 @@ class Basic(unittest.TestCase):
                 s3nFullList = importHDFSResult['succeeded']
                 ### print "s3nFullList:", h2o.dump_json(s3nFullList)
                 # error if none? 
-                self.assertGreater(len(s3nFullList),8,"Didn't see more than 8 files in s3n?")
+                self.assertGreater(len(s3nFullList),1,"Didn't see more than 1 files in s3n?")
 
                 key2 = csvFilename + "_" + str(trial) + ".hex"
                 print "Loading s3n key: ", s3nKey, 'thru HDFS'
