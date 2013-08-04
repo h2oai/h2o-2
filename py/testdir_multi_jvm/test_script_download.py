3999bc4cd47d5c250d42f27a3cc9ac99c6760d70
Kevin Normoyle <kevin@0xdata.com>
2013-08-03 18:41:32 -0700

    download h2o script.txt. Not sure if it's supposed to have all requests. seems empty in this test

---
+++ py/testdir_multi_jvm/test_script_download.py
@@ -0,0 +1,36 @@
+import unittest, time, sys
+sys.path.extend(['.','..','py'])
+import h2o, h2o_hosts, h2o_log
+
+class Basic(unittest.TestCase):
+    def tearDown(self):
+        h2o.check_sandbox_for_errors()
+
+    @classmethod
+    def setUpClass(cls):
+        localhost = h2o.decide_if_localhost()
+        if (localhost):
+            h2o.build_cloud(3)
+        else:
+            h2o_hosts.build_cloud_with_hosts()
+
+    @classmethod
+    def tearDownClass(cls):
+        h2o.tear_down_cloud()
+
+    def test_script_download(self):
+        print "Assuming all json requests are logged in the script? maybe not"
+        h2o.verify_cloud_size()
+
+        (logNameList, lineCountList) = h2o_log.getH2OScripts()
+
+        self.assertEqual(len(logNameList), len(h2o.nodes), "Should be %d scripts" % len(h2o.nodes))
+        self.assertEqual(len(lineCountList), len(h2o.nodes), "Should be %d scripts" % len(h2o.nodes))
+        # line counts seem to vary..check for "too small"
+        # variance in polling (cloud building and status)?
+        for i, l in enumerate(lineCountList):
+            self.assertGreater(l, 12, "node %d script is too small" % i)
+
+
+if __name__ == '__main__':
+    h2o.unit_main()
