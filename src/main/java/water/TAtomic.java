a0df7fd16c31149a757f00a54f35741689303769
cliffclick <cliffc@acm.org>
2013-08-06 13:51:18 -0700

    Fix possible TAtomic bug Rare race for doing update-in-place TAtomic updates. Do a shallow defensive clone first.

--- src/main/java/water/TAtomic.java
+++ src/main/java/water/TAtomic.java
@@ -5,13 +5,13 @@ package water;
  */
 public abstract class TAtomic<T extends Iced> extends Atomic {
   /** Atomically update an old value to a new one.
-   * @param old  The old value, it may be null
+   * @param old  The old value, it may be null.  It is a defensive copy.
    * @return The new value; if null if this atomic update no longer needs to be run
    */
   public abstract T atomic(T old);
 
   @Override public Value atomic(Value val) {
-    T old = val == null ? null : (T)val.get();
+    T old = val == null ? null : (T)(val.get().clone());
     T nnn = atomic(old);
     return  nnn == null ? null : new Value(_key,nnn);
   }
