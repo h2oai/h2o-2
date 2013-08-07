7d1fd5b86885278dfb2a49f5dc0f9168b39cf2af
Anqi Fu <anqi.fu.1987@gmail.com>
2013-08-06 17:17:58 -0700

    Split H2O.R into more organized sub-files (DO NOT CHANGE NUMBERING)

--- R/tests/test_R_B_kmeans_benign.R
+++ R/tests/test_R_B_kmeans_benign.R
@@ -1,8 +1,16 @@
-# R -f test_R_B_kmeans_benign.R --args Path/To/H2O.R H2OServer:Port
+# R -f test_R_B_kmeans_benign.R --args Path/To/h2o-package/R H2OServer:Port
 args <- commandArgs(trailingOnly = TRUE)
 if(length(args) != 2)
-  stop("Usage: R -f test_R_B_kmeans_benign.R --args Path/To/H2O.R H2OServer:Port")
-source(args[1])
+  stop("Usage: R -f test_R_B_kmeans_benign.R --args Path/To/h2o-package/R H2OServer:Port")
+
+sourceDir <- function(path, trace = TRUE, ...) {
+  for (nm in list.files(path, pattern = "\\.[RrSsQq]$")) {
+    if(trace) cat(nm,":")           
+    source(file.path(path, nm), ...)
+    if(trace) cat("\n")
+  }
+}
+sourceDir(args[1])
 argsplit = strsplit(args[2], ":")[[1]]
 localH2O = new("H2OClient", ip=argsplit[1], port=as.numeric(argsplit[2]))
 
