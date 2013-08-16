dfc2cdbcfb407433c04b869f693ab57f0712abd3
Kevin Normoyle <kevin@0xdata.com>
2013-08-15 22:42:51 -0700

    random combinations of header=, header_from_file=, header file, data file(s) (multiple with parse pattern match) and commented first lines in data and header files. Not sure if my checking matches what we say is "right" ..or whether we care about some cases.

--- py/h2o.py
+++ py/h2o.py
@@ -1057,6 +1057,7 @@ class H2O(object):
             'destination_key': key2,
             }
         params_dict.update(kwargs)
+        print "\nParse params list:", params_dict
 
         if benchmarkLogging:
             cloudPerfH2O.get_log_save(initOnly=True)
