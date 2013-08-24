d152e2d96a67f16765007290d77f03f975e7fa97
Kevin Normoyle <kevin@0xdata.com>
2013-08-23 17:53:01 -0700

    adding back again

---
+++ py/testdir_0xdata_only/test_four_billion_rows.py
@@ -0,0 +1,70 @@
+import unittest, time, sys
+sys.path.extend(['.','..','py'])
+import h2o, h2o_cmd, h2o_hosts, h2o_glm
+import h2o_browse as h2b
+import h2o_import as h2i
+import time, random
+
+class Basic(unittest.TestCase):
+    def tearDown(self):
+        h2o.check_sandbox_for_errors()
+
+    @classmethod
+    def setUpClass(cls):
+        global localhost
+        localhost = h2o.decide_if_localhost()
+        if (localhost):
+            h2o.build_cloud(1,java_heap_GB=14)
+        else:
+            h2o_hosts.build_cloud_with_hosts(base_port=54325, java_heap_GB=100)
+
+    @classmethod
+    def tearDownClass(cls):
+        h2o.tear_down_cloud()
+
+    def test_four_billion_rows(self):
+        # just do the import folder once
+        importFolderPath = "/home/0xdiag/datasets/billions"
+        h2i.setupImportFolder(None, importFolderPath)
+        timeoutSecs = 1500
+
+        csvFilenameAll = [
+            "four_billion_rows.csv",
+            ]
+        # csvFilenameList = random.sample(csvFilenameAll,1)
+        csvFilenameList = csvFilenameAll
+
+        # pop open a browser on the cloud
+        ### h2b.browseTheCloud()
+
+        for csvFilename in csvFilenameList:
+            # creates csvFilename.hex from file in importFolder dir 
+            start = time.time()
+            parseKey = h2i.parseImportFolderFile(None, csvFilename, importFolderPath, 
+                timeoutSecs=timeoutSecs, pollTimeoutSecs=60)
+            elapsed = time.time() - start
+            print csvFilename, 'parse time:', parseKey['response']['time']
+            print "Parse result['destination_key']:", parseKey['destination_key']
+            print csvFilename, "completed in", elapsed, "seconds.", "%d pct. of timeout" % ((elapsed*100)/timeoutSecs)
+
+            # We should be able to see the parse result?
+            inspect = h2o_cmd.runInspect(key=parseKey['destination_key'])
+
+            print "\n" + csvFilename
+            kwargs = {'x': 0, 'y': 1, 'n_folds': 0, 'case_mode': '=', 'case': 1}
+            # one coefficient is checked a little more
+            colX = 0
+
+            # L2 
+            kwargs.update({'alpha': 0, 'lambda': 0})
+            start = time.time()
+            glm = h2o_cmd.runGLMOnly(parseKey=parseKey, timeoutSecs=timeoutSecs, **kwargs)
+            elapsed = time.time() - start
+            print "glm (L2) end on ", csvFilename, 'took', elapsed, 'seconds.', "%d pct. of timeout" % ((elapsed/timeoutSecs) * 100)
+            h2o_glm.simpleCheckGLM(self, glm, colX, **kwargs)
+
+            sys.stdout.write('\n.')
+            sys.stdout.flush() 
+
+if __name__ == '__main__':
+    h2o.unit_main()
