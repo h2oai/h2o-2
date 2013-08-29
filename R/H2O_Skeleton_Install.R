a067544a2d7d1eb448afd2db143fc7e71ec70feb
Anqi Fu <anqi_fu@Anqis-MacBook-Pro.local>
2013-08-28 18:41:41 -0700

    Additional info on R package and MD5 checksum

--- R/H2O_Skeleton_Install.R
+++ R/H2O_Skeleton_Install.R
@@ -5,32 +5,31 @@ if(!"rjson" %in% myPackages) install.packages("rjson")
 
 library(RCurl)
 library(rjson)
+library(tools)
 
 ip = readline("Enter IP address: ")
 port = readline("Enter port number: ")
 
 myURL = paste("http://", ip, ":", port, sep="")
 if(!url.exists(myURL)) stop("Cannot connect to H2O server")
-temp = postForm(paste(myURL, "Cloud.json", sep="/"), style = "POST")
+temp = postForm(paste(myURL, "RPackage.json", sep="/"), style = "POST")
 res = fromJSON(temp)
 H2OVersion = res$version
+myFile = res$filename
+serverMD5 = res$md5_hash
+
 if(!("h2o" %in% myPackages && packageVersion("h2o") == H2OVersion)) {
   if("h2o" %in% myPackages) {
     cat("Removing old H2O R package version", H2OVersion, "\n")
     remove.packages("h2o")
   }
   cat("Downloading and installing H2O R package version", H2OVersion, "\n")
-  myFile = paste("h2o_", H2OVersion, ".tar.gz", sep="")
   # download.file(paste(myURL, "R", myFile, sep="/"), destfile = paste(getwd(), myFile, sep="/"), mode = "wb")
   temp = getBinaryURL(paste(myURL, "R", myFile, sep="/"))
   writeBin(temp, paste(getwd(), myFile, sep="/"))
   
-  if(url.exists(paste(myURL, "R/md5.txt", sep="/"))) {
-    library(tools)
-    serverMD5 = read.table(url(paste(myURL, "R/md5.txt", sep="/")))
-    if(as.character(serverMD5[1,2]) != as.character(md5sum(paste(getwd(), myFile, sep="/"))))
-      warning("Mismatched MD5 hash! Check you have downloaded the complete R package.")
-  }
+  if(as.character(serverMD5) != as.character(md5sum(paste(getwd(), myFile, sep="/"))))
+    warning("Mismatched MD5 hash! Check you have downloaded complete R package.")
   install.packages(paste(getwd(), myFile, sep="/"), repos = NULL, type = "source")
 } else
   cat("H2O R package and server version", H2OVersion, "match\n")
--- src/main/java/water/api/RPackage.java
+++ src/main/java/water/api/RPackage.java
@@ -7,10 +7,10 @@ public class RPackage extends Request {
   @Override protected Response serve() {
     JsonObject result = new JsonObject();
     result.addProperty(VERSION, H2O.VERSION);
-    // InputStream is_file = getClass().getResourceAsStream("/R/h2o_1.7.0.99999.tar.gz");
-    result.addProperty("filename", "h2o_1.7.0.99999.tar.gz");
-    String md5 = Boot._init.loadContent("/R/md5.txt");
-    result.addProperty("md5_hash", md5);
+    String info = Boot._init.loadContent("/R/info.txt");
+    String info_split[] = info.split("\\r?\\n");
+    result.addProperty("filename", info_split[0]);
+    result.addProperty("md5_hash", info_split[1]);
     Response response = Response.done(result);
     return response;
   }
