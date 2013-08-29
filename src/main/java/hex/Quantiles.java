2f1cb4e7864811df051b39fc1fdd5645e999530a
Stephen McInerney <spmcinerney@hotmail.com>
2013-08-28 19:27:12 -0700

    Rewrite Quantiles with bin-based MR algorithm. Partition HTML stuff into QuantilesPage, similar to Summary/SummaryPage.

--- src/main/java/hex/Quantiles.java
+++ src/main/java/hex/Quantiles.java
@@ -1,200 +1,133 @@
 package hex;
-import java.util.TreeSet;
 
 import water.*;
-import water.api.DocGen;
-import water.api.Request.API;
-import water.api.RequestArguments.FrameKey;
-import water.api.RequestBuilders.Response;
-import water.api.Request.VecSelect;
 import water.fvec.*;
-import water.nbhm.NonBlockingHashSet;
-import water.util.RString;
 
 /**
- * Class for computing quantiles (epsilon-approximate, deterministic)
  * @author smcinerney
  *
+ * Quantiles by exact multipass bucket-based method, similar to Summary.
+ * If Col summary was available for FluidVec, we could preinitialize the bins to cover col (min,max)
+ *
  * Reference:
- *
- * "A Cost Model for Interval Intersection Queries on RI-Trees",
- * Kriegel, Pfeifle, Pštke, Seidl - SSDBM 2002
- *
- * Mahout OnlineSummarizer (Exponentially Weighted Stochastic Approximation)
- * http://svn.apache.org/repos/asf/mahout/trunk/math/src/main/java/org/apache/mahout/math/stats/OnlineSummarizer.java
- * Welford's method: http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#On-line_algorithm
- * The method used for computing the quartiles is a simplified form of the stochastic approximation
- * method described in the article "Incremental Quantile Estimation for Massive Tracking"
- * by Chen, Lambert and Pinheiro, http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.105.1580
- *
- * An epsilon-approximate deterministic estimator of quantiles on large datasets,
- * with low memory requirements, per:
- * "Approximate medians and other quantiles in one pass and with limited memory",
- * Manku. Rajagopalan & Lindsay, ACM SIGMOD 1998
-
-(A subsequent improvement O(log^2 n / m) on that method is given in:
-"Space-Efficient Online Computation of Quantile Summaries"
-- Michael Greenwald, Sanjeev Khanna - SIGMOD, 2001)
-
-"Selection and Sorting with Limited Storage",
-J. I. Munro and M. S. Paterson, Theoretical Computer Science, vol. 12, pp. 315Ð323, 1980
-
-The 9 methods used in R stats::quantile() are defined in:
-"Sample quantiles in statistical packages" - Hyndman & Fan - ASA 1996
-
-A method with much reduced communication overhead but higher error is:
-"Medians and Beyond: New Aggregation Techniques for Sensor Networks",
-Shrivastava, Buragohain, Agrawal, - 2004 Suri
+ * The 9 methods used in R stats::quantile() are defined in:
+ * "Sample quantiles in statistical packages" - Hyndman & Fan - ASA 1996
  */
 
-
-
-public class Quantiles extends Request2 {
-  static final int API_WEAVER = 1; // This file has auto-gen'd doc & json fields
-  static public DocGen.FieldDoc[] DOC_FIELDS; // Initialized from Auto-Gen code.
-
-  // This Request supports the HTML 'GET' command, and this is the help text
-  // for GET.
-  static final String DOC_GET = "Quantiles (epsilon-approximate) on arbitrary column";
-
-  @API(help="Data Frame", required=true, filter=FrameKey.class)
-  Frame source;
-
-  /* BUG: API will not accept plain VecSelect here:
-   * java.lang.Exception: Class water.api.Request$VecSelect must have an empty constructor
-   *@API(help="Column", required=true, filter=VecSelect.class)
-   *Vec vec; */
-  @API(help="Column", required=true, filter=QVecSelect.class)
-  Vec vec;
-  class QVecSelect extends VecSelect { QVecSelect() { super("source"); } }
-
-  // API can't handle declaring these as double[]
-  @API(help="Quantile_a", required=true, filter=Default.class)  // BUG: filter=Real.class doesn't appear
-  double quantile_a = .05;
-  @API(help="Quantile_b", required=true, filter=Default.class)
-  double quantile_b = .1;
-  @API(help="Quantile_c", required=true, filter=Default.class)
-  double quantile_c = .15;
-  @API(help="Quantile_d", required=true, filter=Default.class)
-  double quantile_d = .85;
-  @API(help="Quantile_e", required=true, filter=Default.class)
-  double quantile_e = .9;
-  @API(help="Quantile_f", required=true, filter=Default.class)
-  double quantile_f = .95;
-
-  //REMOVE @API(help="Buffer size, k")  long k;
-  @API(help="Pass 1 msec")     long pass1time;
-  @API(help="Pass 2 msec")     long pass2time;
-  @API(help="Pass 3 msec")     long pass3time;
-  @API(help="nrows (N)")       long nrows;
-  //@API(help="epsilon")         double epsilon;
-  //@API(help="epsilon*N, ranking error") double epsilon_N;
-
-
-  @Override public Response serve() {
-
-    //quantiles -> map somehow to bucket sizes
-
     // Pass 1: ...
-    long start = System.currentTimeMillis();
-    nrows = vec.length();
+    //long start = System.currentTimeMillis();
+    //nrows = vec.length();
     /* Chunk dataset out to fixed-size subtrees */
-    MergeSortTask ms1 = new MergeSortTask().doAll(vec);
-    long pass1 = System.currentTimeMillis();
-    pass1time = pass1 - start;
+    //MergeSortTask ms1 = new MergeSortTask().doAll(vec);
+    //long pass1 = System.currentTimeMillis();
+    //pass1time = pass1 - start;
 
     // Split the column chunks(?) and buffers out to nodes... make sure the buffers are local
 
-    // ...
 
-    return new Response(Response.Status.done, this, -1, -1, null);
+public class Quantiles extends Iced {
+
+  // Initialize bucket sizes to cover entire range of double
+  //Vec _bins, _counts;
+  byte[] _initchunk;
+  double _binSizeRatio;
+  //whether to implement nDefLess, nDefGrtr, nUnseen counters
+  // Is bin-merging worth it? (messes up add()?) assume not.
+
+  // Could switch directly on (sign,exponent) components of double:
+  //  Sign bit: 1 bit.
+  //  Binary exponent, biased by 1023: 11 bits.
+  //  long exponent = ((bits & 0x7ff0000000000000L) >> 52) - 1023; // check this is correct
+  //  Mantissa: 53 bits.
+  //  long mantissa = (bits & 0x000fffffffffffffL) | 0x0010000000000000L;
+
+
+  public Quantiles(Frame frm, Vec vec, double[] quantiles) {
+    assert vec != null;
+    // from vec, could look up Column c to get its min, max
+    System.err.println("Vec" + vec + "has min:" + vec.min() + ", max:" + vec.max());
+
+    // Allocate and add our temporary Vecs/Chunks to frame
+    // Note: must allocate Vec, can't directly allocate Chunk / C8DChunk/ C8Chunk
+
+    // Allocate Chunk (1M) for bins+counters. Should handle at least 16K? levels: 8K positive, 8K negative
+    // Can't see how to get key for given frame?
+    //Key countkey = frm...
+
+    //long[] _initchunk = new long[2 << (Vec.LOG_CHK)];
+    //Vec _counts = new Vec(countkey, _initchunk, false, 0); // ERROR: Vec(...) ctor not visible
+    //frm.add("_counts", _counts);
+
+    // TODO: bin min,max determination go here
+
+    // TODO: get result by taking bins at corresponding indices phi*N
+    // Optionally use one of the 9 averaging schemes as per R/ Hyndman & Fan
+    double[] res = new double[quantiles.length];
+    // TODO
+
+  }
+
+
+  public static class QuantilesTask extends MRTask2<QuantilesTask> {
+
+    long[] _counts;
+
+    @Override public void map(Chunk xs, Chunk bins) {
+      System.err.println("Chunk 0x" + xs +" of length=" + xs._len); // printing will distort runtime
+      PrintChunk("", xs);
+
+      //double[] _bins = MemoryManager.malloc8d(bins._len);
+      //for (int i=0; i<bins._len; i++) _bins[i] = bins.at0(i);
+
+      // Output
+      _counts = new long[bins._len];
+      //Arrays.fill(_counts, 0);
+
+      //double value = MemoryManager.malloc8d(1);
+      double value;
+      for (int i=0; i<xs._len; i++) {
+        value = xs.at0(i); // cannot be isNan()
+
+        // binary-search bins for value, inexact comparison, bin b contains the interval [ bins(b), bins(b+1) )
+        int b = bins._len/2;
+        int oldB = b;
+        int deltaB = Math.max((bins._len/2 +1)/2, 1);
+        int oldDeltaB = deltaB;
+        while(b!=oldB && oldDeltaB!=1) { // TODO: test termination condition corner-cases
+          oldB = b;
+
+          // TODO: Double.MINUS_INFINITY might not match for equality
+          if (value == bins.at0(b)) {
+            break;
+          } else if (value < bins.at0(b)) {
+            b -= deltaB;
+          } else if (value > bins.at0(b)) {
+            b += deltaB;
+          }
+
+          oldDeltaB = deltaB;
+          deltaB = Math.min((deltaB+1)/2, 1); // maybe suspect
+        }
+        _counts[b]++;
+      }
+
+      // DEBUG: Print counts
+    }
+
+    // reduce: accumulate bin counts
+    @Override public void reduce( QuantilesTask other ) {
+      for (int b=0; b<_counts.length; b++) {
+        this._counts[b] += other._counts[b];
+      }
+    }
   }
 
   private static void PrintChunk(String msg, Chunk xs) {
     System.err.print("\n" + msg);
     for (int i=0; i<xs._len; i++) {
-      System.err.print((long) xs.at0(i) + ","); // cast to long
+      System.err.print(xs.at0(i) + ",");
     }
     System.err.println();
   }
 
-  public static class MergeSortTask extends MRTask2<MergeSortTask> {
-    private double[] _tmp, _tmp2;
-
-    /*private void sort(double[] values) {
-      this._tmp = values;
-      this._tmp2 = new double[_tmp.length];
-      mergesort(0, _tmp.length-1);
-    }*/
-
-    private void mergesort(int low, int high) {  // TODO: templatize doubles/longs as <T>
-      if (low<high) {
-        int middle = low + (high-low)/2;
-        mergesort(low, middle);
-        mergesort(middle+1, high);
-        merge(low,middle,high);
-      }
-    }
-    private void merge(int low, int middle, int high) {
-      for (int i=low; i<=high; i++) {
-        _tmp2[i] = _tmp[i];
-      }
-
-      int i = low;
-      int j = middle + 1;
-      int k = low;
-
-      for ( ; i<=middle && j<=high; k++) {
-        if (_tmp2[i] <= _tmp2[j]) {
-          _tmp2[k] = _tmp[i++];
-        } else {
-          _tmp2[k] = _tmp[j++];
-        }
-      }
-
-      for ( ; i<=middle; k++, i++) {
-        _tmp[k] = _tmp2[i];
-      }
-    }
-
-    MergeSortTask() {}
-    @Override public void map(Chunk xs) {
-      System.err.println("Mergesorting chunk 0x" + xs +" of length=" + xs._len); // printing will mess up runtime
-      PrintChunk("BEFORE:", xs);
-
-      // Copy in data to local temporary arrays
-      int length = xs._len;
-      _tmp  = new double[length];
-      _tmp2 = new double[length];
-      for (int i=0; i<length; i++) {
-        _tmp[i] = xs.at0(i); // .at80() for long, at0() for double
-      }
-
-      mergesort(0, length-1);
-
-      // Writeback results
-      for (int i=0; i<length; i++) {
-        xs.set80(i, _tmp2[i]);
-      }
-      //xs.close(cidx, fs); // ??? int cidx, Futures fs
-
-      PrintChunk("AFTER: ", xs);
-      System.err.println();
-    }
-    @Override public void reduce(MergeSortTask drt) {}
-  }
-
-  /*public static class GetQuantilesTask extends MRTask2<GetQuantilesTask> {
-  }*/
-
-
-  /** Return the HTML query link to this page */
-  public static String link(Key k, String content) {
-    RString rs = new RString("<a href='Quantiles.query?data_key=%$key'>%content</a>");
-    rs.replace("key", k.toString());
-    rs.replace("content", content);
-    return rs.toString();
-  }
-
 }
-
