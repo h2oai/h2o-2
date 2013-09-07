e2d89768fd5c9835289e79800c8370509d00d56e
Kevin Normoyle <kevin@0xdata.com>
2013-09-07 16:09:30 -0700

    simple test config setup. see test_config_basic.py for use

---
+++ py/test_config_basic.py
@@ -0,0 +1,20 @@
+import h2o, h2o_test
+
+l = h2o_test.setup_test_config(test_config_json='test_config.json')
+print "\nsetup_test_config returns list of test config objs:", l
+
+# Here are some ways to reference the config state that the json created
+
+print "\nHow to reference.."
+for i, obj in enumerate(h2o_test.configs):
+    print "keys in config", i, ":", obj.__dict__.keys()
+
+print h2o_test.configs[0].trees
+
+for t in h2o_test.configs:
+    print "\nTest config_name:", t.config_name
+    print "trees:", t.trees
+    print "params:", t.params
+
+
+
