cfa704e653bc36300c6b7681eba902e4ec01d359
anqi <anqi@0xdata.com>
2013-09-11 15:52:39 -0700

    PCA scoring works on standardized data and gives option to choose number of PCs

--- src/main/java/water/api/PCA.java
+++ src/main/java/water/api/PCA.java
@@ -89,14 +89,13 @@ public class PCA extends Request {
       if(cols.length > MAX_COL)
         throw new RuntimeException("Cannot run PCA on more than " + MAX_COL + " columns");
 
-      // DataFrame data = DataFrame.makePCAData(ary, cols, true);
       DataFrame data = DataFrame.makePCAData(ary, cols, _standardize.value());
       PCAJob job = DPCA.startPCAJob(dest, data, pcaParams);
       j.addProperty(JOB, job.self().toString());
       j.addProperty(DEST_KEY, job.dest().toString());
 
       Response r = Progress.redirect(j, job.self(), job.dest());
-      r.setBuilder(Constants.DEST_KEY, new KeyElementBuilder());
+      r.setBuilder(DEST_KEY, new KeyElementBuilder());
       return r;
     } catch(RuntimeException e) {
       Log.err(e);
@@ -121,6 +120,9 @@ public class PCA extends Request {
     }
 
     private void modelHTML(PCAModel m, JsonObject json, StringBuilder sb) {
+      sb.append("<div class='alert'>Actions: " + PCAScore.link(m._selfKey, "Score on dataset") + ", "
+          + PCA.link(m._dataKey, "Compute new model") + "</div>");
+
       sb.append("<span style='display: inline-block;'>");
       sb.append("<table class='table table-striped table-bordered'>");
       sb.append("<tr>");
--- src/main/java/water/api/PCAScore.java
+++ src/main/java/water/api/PCAScore.java
@@ -1,23 +1,29 @@
 package water.api;
 
 import hex.DPCA.PCAModel;
+import hex.NewRowVecTask.DataFrame;
+import hex.DPCA;
 import hex.PCAScoreTask;
 
 import com.google.gson.JsonObject;
 
 import water.*;
+import water.api.RequestArguments.HexColumnSelect;
+import water.api.RequestArguments.HexPCAColumnSelect;
+import water.fvec.Frame;
 import water.util.RString;
 
 public class PCAScore extends Request {
   protected final H2OPCAModelKey _modelKey = new H2OPCAModelKey(MODEL_KEY, true);
   protected final H2OHexKey _dataKey = new H2OHexKey(KEY);
   protected final H2OKey _destKey = new H2OKey(DEST_KEY, true);
+  protected final Int _numPC = new Int("num_pc", 2, 1, 1000000);   // TODO: Set default to # features
 
   @Override protected void queryArgumentValueSet(water.api.RequestArguments.Argument arg, java.util.Properties inputArgs) {
     if( arg == _dataKey ) {     // Check for dataset compatibility
       ValueArray va = _dataKey.value();
       PCAModel model = _modelKey.value();
-      int colIds[] = model.columnMapping(va.colNames());
+      int colIds[] = model == null ? null : model.columnMapping(va.colNames());
       if( !Model.isCompatible(colIds) ) {
         for( int i = 0; i < colIds.length; i++ )
           if( colIds[i] == -1 )
@@ -25,11 +31,20 @@ public class PCAScore extends Request {
                 + model._va._cols[i]._name + "'");
       }
     }
+    if( arg == _numPC ) {
+      PCAModel model = _modelKey.value();
+      if(model != null && model._eigVec[0].length < _numPC.value())
+        throw new IllegalArgumentException("Maximum number of principal components is " + model._eigVec[0].length);
+    }
   }
 
-  public static String link(Key k, String content) {
+  public static String link(Key modelKey, String content) {
+    return link(MODEL_KEY, modelKey, content);
+  }
+
+  public static String link(String key_param, Key k, String content) {
     RString rs = new RString("<a href='PCAScore.query?%key_param=%$key'>%content</a>");
-    rs.replace("key_param", MODEL_KEY);
+    rs.replace("key_param", key_param);
     rs.replace("key", k.toString());
     rs.replace("content", content);
     return rs.toString();
@@ -38,12 +53,21 @@ public class PCAScore extends Request {
   @Override protected Response serve() {
     try {
       JsonObject res = new JsonObject();
+      res.addProperty(RequestStatics.DEST_KEY, _destKey.value().toString());
       ValueArray ary = _dataKey.value();
       PCAModel m = _modelKey.value();
-      if(m._pcaParams._standardized)
-        // TODO: Need to standardize ary data set if PCs calculated on standardized data!
-      PCAScoreTask.score(ary.asFrame(), m._eigVec, _destKey.value());
-      return Response.done(res);
+
+      int[] cols = new int[ary._cols.length];
+      for(int i = 0; i < cols.length; i++) cols[i] = i;
+      DataFrame temp = DataFrame.makePCAData(ary, cols, m._pcaParams._standardized);
+      Frame data = DPCA.StandardizeTask.standardize(temp);
+      PCAScoreTask.score(data, m._eigVec, _numPC.value(), _destKey.value());
+
+      JsonObject redir = new JsonObject();
+      // if( job != null ) redir.addProperty(JOB, job.toString());
+      redir.addProperty(KEY, _destKey.value().toString());
+      // return Progress.redirect(response, job.job_key, _destKey.value());
+      return Response.redirect(res, Inspect.class, redir);
     } catch( Error e ) {
       return Response.error(e.getMessage());
     }
