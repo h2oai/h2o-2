2ea871363a984ab97995775c0c1afada80963c89
Kevin Normoyle <kevin@0xdata.com>
2013-09-12 13:31:57 -0700

    coolness

--- py/testdir_release/c1/test_c1_rel.py
+++ py/testdir_release/c1/test_c1_rel.py
@@ -1,10 +1,9 @@
-import unittest, time, sys, random, logging
-sys.path.extend(['.','..','py'])
-import h2o, h2o_cmd, h2o_import2 as h2i, h2o_glm, h2o_common
-import h2o_print
+import unittest, sys
+sys.path.extend(['.','..','../..','py'])
+import h2o, h2o_cmd, h2o_import2 as h2i, h2o_common, h2o_print
 
 print "Assumes you ran ../build_for_clone.py in this directory"
-print "Creating a h2o-nodes.json to use. Also a sandbox dir!"
+print "Using h2o-nodes.json. Also the sandbox dir"
 class releaseTest(h2o_common.ReleaseCommon, unittest.TestCase):
 
     def test_A_c1_rel_short(self):
