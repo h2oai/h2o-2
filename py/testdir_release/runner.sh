9251d154de76f23a2cef9923ceaf4d62ad5fa76c
Kevin Normoyle <kevin@0xdata.com>
2013-09-12 11:51:55 -0700

    add a shutdown test as the last test in runner.sh

--- py/testdir_release/runner.sh
+++ py/testdir_release/runner.sh
@@ -23,12 +23,8 @@ trap "kill -- -$BASHPID" INT TERM EXIT
 # Process groups have the same id as the spawning process, 
 # The process group id remains even after processes have been reparented. (say by init)
 # The -- gets kill not to interpret this as a signal ..
-
-# don't use kill -9 though to kill this script though!
-
-# get the latest jar from s3
-# has to execute up there
-
+# Don't use kill -9 though to kill this script though!
+# Get the latest jar from s3. Has to execute up in h2o
 
 # a secret way to skip the download (use any arg)
 if [ $# -eq 0 ]
@@ -65,22 +61,22 @@ ls -lt ./h2o-nodes.json
 # We now have the h2o-nodes.json, that means we started the jvms
 # Shouldn't need to wait for h2o cloud here..
 # the test should do the normal cloud-stabilize before it does anything.
-
 # n0.doit uses nosetests so the xml gets created on completion. (n0.doit is a single test thing)
 # A little '|| true' hack to make sure we don't fail out if this subtest fails
 # test_c1_rel has 1 subtest
 
 # This could be a runner, that loops thru a list of tests.
-../testdir_single_jvm/n0.doit test_c1_rel || true
 
+../testdir_single_jvm/n0.doit test_c1_rel || true
 ../testdir_single_jvm/n0.doit test_c2_rel || true
+# If this one fails, fail this script so the bash dies 
+# We don't want to hang waiting for the cloud to terminate.
+../testdir_single_jvm/n0.doit test_shutdown
 
 # test_c2_rel has about 11 subtests inside it, that will be tracked individually by jenkins
 # ../testdir_single_jvm/n0.doit test_c2_rel || true
-
-# we don't want the jenkins job to complete until we kill it, so the cloud stays alive for debug
+# We don't want the jenkins job to complete until we kill it, so the cloud stays alive for debug
 # also prevents us from overrunning ourselves with cloud building
-
 # If we don't wait, the cloud will get torn down.
 
 jobs -l
