da7ea21a4002c178a6335254a25c2547ae8af9d9
anqi <anqi@0xdata.com>
2013-09-24 18:11:26 -0700

    More work on streamlining GBM and GLM grid search

--- R/h2o-package/R/Algorithms.R
+++ R/h2o-package/R/Algorithms.R
@@ -73,13 +73,13 @@ setMethod("h2o.gbmgrid", signature(data="H2OParsedData", destination="character"
               # rownames(mse_matrix) = "MSE"
               # result$err = mse_matrix
               result$err = resModel$gbm_model$errs
-              
               myModels[[i]] = new("H2OGBMModel", key=destKey, data=data, model=result)
             }
-            return(myModels)
+            resGBMGrid = new("H2OGBMGrid", key=destination, data=data, models=myModels, sumtable=resGrid)
+            resGBMGrid
           })
 
-setMethod("h2o.gbmgrid", signature(data="H2OParsedData", destination="character",y="character",x_ignore="ANY",ntrees="ANY", max_depth="ANY", learn_rate="ANY", min_rows="ANY"),
+setMethod("h2o.gbmgrid", signature(data="H2OParsedData", destination="character",y="character", x_ignore="ANY", ntrees="ANY", max_depth="ANY", learn_rate="ANY", min_rows="ANY"),
           function(data, destination, y, x_ignore, ntrees, max_depth, learn_rate, min_rows) {
             if(!(missing(x_ignore) || class(x_ignore) == "numeric"))
               stop(paste("ignore cannot be of class", class(x_ignore)))
@@ -178,33 +178,32 @@ setMethod("h2o.glm", signature(x="character", y="character", data="H2OParsedData
 
 setMethod("h2o.glmgrid", signature(x="character", y="character", data="H2OParsedData", family="character", nfolds="numeric", alpha="numeric", lambda="numeric"),
           function(x, y, data, family, nfolds, alpha, lambda) {
-            res = h2o.__remoteSend(data@h2o, h2o.__PAGE_GLMGrid, key = data@key, y = y, x = paste(x, sep="", collapse=","), family = family, n_folds = nfolds, alpha = alpha, lambda = lambda, case_mode="=",case=1.0,parallel= 1 )
+            res = h2o.__remoteSend(data@h2o, h2o.__PAGE_GLMGrid, key = data@key, y = y, x = paste(x, sep="", collapse=","), family = family, n_folds = nfolds, alpha = alpha, lambda = lambda, case_mode="=", case=1.0, parallel= 1 )
             while(h2o.__poll(data@h2o, res$response$redirect_request_args$job) != -1) { Sys.sleep(1) }
             destKey = res$destination_key
             res = h2o.__remoteSend(data@h2o, h2o.__PAGE_GLMGridProgress, destination_key=res$destination_key)
-            result = list()
-            result$Summary = t(sapply(res$models,c))
-            #            result=rep( list(list()),length(res$models)+1  ) 
-            #            result[[length(result)]]$Summary = t(sapply(res$models,c))
+            allModels = res$models
             
-            #		for(i in 1:length(res$models)){
-            #			resH=h2o.__remoteSend(data@h2o, h2o.__PAGE_INSPECT, key=res$models[[i]]$key)
-            #			resH = resH$GLMModel
-            #			    result[[i]]$LSMParams=unlist(resH$LSMParams)
-            #	            result[[i]]$coefficients = unlist(resH$coefficients)
-            #	            result[[i]]$normalized_coefficients = unlist(resH$normalized_coefficients)
-            #            		result[[i]]$dof = resH$dof            
-            #			result[[i]]$null.deviance = resH$validations[[1]]$nullDev
-            #			result[[i]]$deviance = resH$validations[[1]]$resDev
-            #			result[[i]]$aic = resH$validations[[1]]$aic
-            #			result[[i]]$auc = resH$validations[[1]]$auc
-            #			result[[i]]$iter = resH$iterations
-            #		        result[[i]]$threshold = resH$validations[[1]]$threshold
-            #	                result[[i]]$error_table = t(sapply(resH$validations[[1]]$cm,c))
-            #
-            #            }
-            resGLMGridModel = new("H2OGLMGridModel", key=destKey, data=data, model=result)
-            resGLMGridModel
+            result = list()
+            tweedie.p = "NA"
+            # result$Summary = t(sapply(res$models,c))
+            for(i in 1:length(allModels)) {
+              resH = h2o.__remoteSend(data@h2o, h2o.__PAGE_INSPECT, key=allModels[[i]]$key)
+              modelOrig = h2o.__getGLMResults(resH$GLMModel, y, family, tweedie.p)
+              
+              res_xval = list()
+              for(j in 1:nfolds) {
+                xvalKey = resH$GLMModel$validations[[1]]$xval_models[j]
+                resX = h2o.__remoteSend(data@h2o, h2o.__PAGE_INSPECT, key=xvalKey)
+                modelXval = h2o.__getGLMResults(resX$GLMModel, y, family, tweedie.p)
+                res_xval[[j]] = new("H2OGLMModel", key=xvalKey, data=data, model=modelXval, xval=list())
+              }
+              result[[i]] = new("H2OGLMModel", key=allModels[[i]]$key, data=data, model=modelOrig, xval=res_xval)
+            }
+            
+            # temp = data.frame(t(sapply(allModels, c)))
+            resGLMGrid = new("H2OGLMGrid", key=destKey, data=data, models=result, sumtable=allModels)
+            resGLMGrid
           })
 
 setMethod("h2o.glmgrid", signature(x="character", y="character", data="H2OParsedData", family="character", nfolds="ANY", alpha="ANY", lambda="ANY"),
