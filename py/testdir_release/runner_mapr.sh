c3a6de6f1adf29376f44db6e5a2e94a20972038b
Kevin Normoyle <kevin@0xdata.com>
2013-09-28 18:22:36 -0700

    use a maprfs test when running on mapr cluster

--- py/testdir_release/runner_mapr.sh
+++ py/testdir_release/runner_mapr.sh
@@ -144,7 +144,7 @@ $DOIT c1/test_c1_rel.py || true
 $DOIT c2/test_c2_rel.py || true
 # $DOIT c3/test_c3_rel.py || true
 # $DOIT c4/test_c4_four_billion_rows.py || true
-$DOIT c6/test_c6_hdfs.py || true
+$DOIT c6/test_c6_maprfs.py || true
 
 # If this one fails, fail this script so the bash dies 
 # We don't want to hang waiting for the cloud to terminate.
