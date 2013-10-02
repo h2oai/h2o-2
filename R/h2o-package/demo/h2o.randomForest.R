5cefccf13ac7756246c8e228be7abf06f49d75e6
anqi <anqi@0xdata.com>
2013-10-01 19:20:18 -0700

    Updated tests, docs, and demo to reflect RF changes

--- R/h2o-package/demo/h2o.randomForest.R
+++ R/h2o-package/demo/h2o.randomForest.R
@@ -8,12 +8,12 @@ h2o.checkClient(localH2O)
 
 iris.hex = h2o.importFile(localH2O, path = system.file("extdata", "iris.csv", package="h2o"), key = "iris.hex")
 summary(iris.hex)
-iris.rf = h2o.randomForest(y = 4, data = iris.hex, ntree = 50, depth = 100, classwt = c("Iris-versicolor" = 20.0, "Iris-virginica" = 30.0))
+iris.rf = h2o.randomForest(y = 4, x = c(1,2,3), data = iris.hex, ntree = 50, depth = 100, classwt = c("Iris-versicolor" = 20.0, "Iris-virginica" = 30.0))
 print(iris.rf)
 
 invisible(readline("Hit <Return> to continue: "))
 
 covtype.hex = h2o.importFile(localH2O, path = system.file("extdata", "covtype.csv", package="h2o"), key = "covtype.hex")
 summary(covtype.hex)
-covtype.rf = h2o.randomForest(y = "Cover_Type", x_ignore = c("Aspect", "Hillshade_9am"), data = covtype.hex, ntree = 50, depth = 150)
+covtype.rf = h2o.randomForest(y = "Cover_Type", x = setdiff(colnames(covtype.hex), c("Cover_Type", "Aspect", "Hillshade_9am")), data = covtype.hex, ntree = 50, depth = 150)
 print(covtype.rf)
