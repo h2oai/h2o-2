b51ac4662c418391b52cc5fd518503cc405884ba
SpencerA <spnrpa@gmail.com>
2013-10-14 14:03:30 -0700

    Benchmark scripts updated to pull from S3

---
+++ bench/BMscripts/kmeansBench.py
@@ -0,0 +1,78 @@
+#KMeans bench
+import os, sys, time, csv
+sys.path.append('../py/')
+sys.path.extend(['.','..'])
+import h2o_cmd, h2o, h2o_hosts, h2o_browse as h2b, h2o_import as h2i, h2o_rf
+
+csv_header = ('h2o_build','java_heap_GB','dataset','nRows','nCols','parseWallTime','kmeansBuildTime')
+
+files      = {'Airlines'    : {'train': ('AirlinesTrain1x', 'AirlinesTrain10x', 'AirlinesTrain100x'),          'test' : 'AirlinesTest'},
+              'AllBedrooms' : {'train': ('AllBedroomsTrain1x', 'AllBedroomsTrain10x', 'AllBedroomsTrain100x'), 'test' : 'AllBedroomsTest'},
+             }   
+build = ""
+
+def doKMeans(fs, folderPath): 
+    for f in fs['train']:
+        overallWallStart = time.time()
+        date = '-'.join([str(x) for x in list(time.localtime())][0:3])
+        kmeansbenchcsv = 'benchmarks/'+build+date+'/kmeansbench.csv'
+        if not os.path.exists(kmeansbenchcsv):
+            output = open(kmeansbenchcsv,'w')
+            output.write(','.join(csv_header)+'\n')
+        else:
+            output = open(kmeansbench.csv,'a')
+        csvWrt = csv.DictWriter(output, fieldnames=csv_header, restval=None, 
+                        dialect='excel', extrasaction='ignore',delimiter=',')
+        try:
+            java_heap_GB = h2o.nodes[0].java_heap_GB
+            #Train File Parsing#
+            importFolderPath = "bench/" + folderPath
+            if (f in ['AirlinesTrain1x','AllBedroomsTrain1x', 'AllBedroomsTrain10x', 'AllBedroomsTrain100x']): csvPathname = importFolderPath + "/" + f + '.csv'
+            else: csvPathname = importFolderPath + "/" + f + "/*linked*"
+            hex_key = f + '.hex'
+            hK = folderPath + "Header.csv"
+            headerPathname = importFolderPath + "/" + hK
+            h2i.import_only(bucket='home-0xdiag-datasets', path=headerPathname)
+            headerKey = h2i.find_key(hK)
+            trainParseWallStart = time.time()
+            parseResult = h2i.import_parse(bucket='home-0xdiag-datasets', path=csvPathname, schema='local', hex_key=hex_key, header=1, header_from_file=headerKey, separator=44,
+                timeoutSecs=3600,retryDelaySecs=5,pollTimeoutSecs=3600)
+            parseWallTime = time.time() - trainParseWallStart
+            #End Train File Parse#
+            print "Parsing training file took ", parseWallTime ," seconds." 
+            
+            inspect  = h2o.nodes[0].inspect(parseResult['destination_key'])
+            
+            row      =  {'h2o_build'          : build,  
+                         'java_heap_GB'       : java_heap_GB,
+                         'dataset'            : f,
+                         'nRows'              : inspect['num_rows'],
+                         'nCols'              : inspect['num_cols'],
+                         'parseWallTime'      : parseWallTime,
+                        }
+        
+            params   =  {'source_key'         : hex_key,
+                         'k'                  : 6,
+                         'initialization'     : 'Furthest',
+                         'max_iter'           : 100,
+                         'seed'               : 1234567,
+                         'normalize'          : 0,
+                         #'cols'               : ,
+                         'destination_key'    : "KMeans("+f+")",
+                        }
+            kwargs       = params.copy()
+            kmeansStart  = time.time()
+            kmeans       = h2o_cmd.runKMeans(parseResult=parseResult, timeoutSecs=3600, **kwargs)
+            kmeansTime   = time.time() - kmeansStart
+            row.update({'kmeansBuildTime' : kmeansTime})
+            csvWrt.writerow(row)
+        finally:
+            output.close()
+
+if __name__ == '__main__':
+    build = sys.argv.pop(-1)
+    h2o.parse_our_args()
+    h2o_hosts.build_cloud_with_hosts()
+    doKMeans(files['Airlines'], 'Airlines')
+    doKMeans(files['AllBedrooms'], 'AllBedrooms')
+    h2o.tear_down_cloud()
---
+++ bench/runBench.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+#set -x
+
+h2oBuild=
+benchmarks="benchmarks"
+DATE=`date +%Y-%m-%d`
+
+function ALL() {
+    echo "Running PCA benchmark..."
+    PCA
+    wait
+    echo "Running KMeans benchmark..."
+    KMeans
+    wait
+    echo "Running GLM benchmark..."
+    GLM
+    wait
+    echo "Running Big KMeans..."
+    #BigKMeans
+    #wait
+}
+
+function PCA() {
+    pyScript="BMscripts/pcaBench.py"
+    python ${pyScript} --config_json BMscripts/${JSON} ${h2oBuild}
+    wait
+}
+
+function KMeans() {
+    py_args="BMscripts/kmeansBench.py -cj ${JSON}"
+    python "$py_args"
+    echo ${py_args}
+    wait
+}
+
+function BigKMeans() {
+    #py_args="BMscripts/BigKMeansBench.py -cj ${JSON}"
+    #python "$py_args"
+    #echo ${py_args}
+    #wait
+    echo "No Big KMeans yet..."
+}
+
+function GLM() {
+    py_args="BMscripts/glmBench.py -cj ${JSON}"
+    python "$py_args"
+    echo ${py_args}
+    wait
+}
+
+usage()
+{
+cat << EOF
+
+usage: $0 [options]
+
+This script obtains the latest h2o jar from S3 and runs the benchmarks for PCA, KMeans, GLM, and BigKMeans.
+
+OPTIONS:
+   -h      Show this message
+   -t      Run task:
+               Choices are:
+                   ALL        -- Runs PCA, KMeans, GLM, and BigKMeans
+                   PCA        -- Runs PCA on Airlines/AllBedrooms data
+                   KMeans     -- Runs KMeans on Airlines/AllBedrooms data
+                   GLM        -- Runs logistic regression on Airlines/AllBedrooms data
+                   BigKMeans  -- Runs KMeans on 180 GB of synthetic data
+   -j      JSON config:
+               Choices are:
+                   161        -- Runs benchmark(s) on single machine on 161
+                   164        -- Runs benchmark(s) on single machine on 164
+                   161_164    -- Runs benchmark(s) on four machines 161 - 164
+EOF
+}
+
+TASK=
+JSON=
+while getopts "ht:j:" OPTION
+do
+  case $OPTION in
+    h)
+      usage
+      exit 1
+      ;;
+    t)
+      TEST=$OPTARG
+      ;;
+    j)
+      JSON=$OPTARG
+      ;;
+    ?)
+      usage
+      exit 1
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$TEST" ] || [ -z "$JSON" ]
+then
+    usage
+    exit
+fi
+
+#bash S3getLatest.sh
+#wait
+h2oBuild=`cat latest`
+
+if [ -a ${benchmarks}/${h2oBuild}/${DATE}/"pcabench.csv" ]; then
+ rm -f ${benchmarks}/${h2oBuild}/${DATE}/"pcabench.csv"
+fi
+
+if [ -a ${benchmarks}/${h2oBuild}/${DATE}/"glmbench.csv" ]; then
+ rm -f ${benchmarks}/${h2oBuild}/${DATE}/"glmbench.csv"
+fi
+
+if [ -a ${benchmarks}/${h2oBuild}/${DATE}/"kmeansbench.csv" ]; then
+ rm -f ${benchmarks}/${h2oBuild}/${DATE}/"kmeansbench.csv"
+fi
+
+#if [ -a ${benchmarks}/${h2oBuild}/${DATE}/bigkmeansbench.csv ]; then
+# rm -f ${benchmarks}/${h2oBuild}/${DATE}/bigkmeansbench.csv
+#fi
+
+if [ ! -d ${benchmarks}/${h2oBuild}/${DATE} ]; then
+  mkdir -p ${benchmarks}/${h2oBuild}/${DATE}
+fi
+rm latest
+$TEST
+
