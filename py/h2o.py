18cfac128b1dafa08fb68ffc63ba6e3fa3aed9e5
SpencerA <spnrpa@gmail.com>
2013-10-15 13:33:40 -0700

    2/glm2 if beta_features else glm

--- py/h2o.py
+++ py/h2o.py
@@ -1853,8 +1853,8 @@ class H2O(object):
     def GLM(self, key,
         timeoutSecs=300, retryDelaySecs=0.5, initialDelaySecs=None, pollTimeoutSecs=180,
         noise=None, benchmarkLogging=None, noPoll=False, destination_key='GLM_model_$python_0_default_0',**kwargs):
-
-        a = self.GLM_shared(key, timeoutSecs, retryDelaySecs, initialDelaySecs, parentName="GLM",destination_key=destination_key, **kwargs)
+        parentName = "2/GLM2" if beta_features else "GLM"
+        a = self.GLM_shared(key, timeoutSecs, retryDelaySecs, initialDelaySecs, parentName=parentName ,destination_key=destination_key, **kwargs)
         # Check that the response has the right Progress url it's going to steer us to.
         if a['response']['redirect_request']!='GLMProgressPage':
             print dump_json(a)
