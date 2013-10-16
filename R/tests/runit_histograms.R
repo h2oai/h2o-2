d2a55dbdc2dcfb8c45a29106f0c3beeb4b81243c
Tongxin Bai <waffle.bai@gmail.com>
2013-10-15 17:33:26 -0700

    runit on histograms added

--- R/tests/runit_histograms.R
+++ R/tests/runit_histograms.R
@@ -8,17 +8,20 @@ H2Ocon <- new("H2OClient", ip=myIP, port=myPort)
 test.histogram <- function (con, path, key) {
   h2o.uploadFile(H2Ocon, path, key)
   h2o.data <- new('H2OParsedData2')
-  h2o@h2o <- con
-  h2o@key <- key
+  h2o.data@h2o <- con
+  h2o.data@key <- key
   h2o.hists <- histograms(h2o.data)
   r.data <- read.csv(path)
   for (i in 1:length(h2o.hists)) {
     if (is.numeric(r.data[,i])) {
-      r.hist <- hist(r.data[,i], breaks='scott', right=FALSE)
       h2o.counts <- h2o.hists[[i]]$counts
       h2o.breaks <- h2o.hists[[i]]$breaks
-      expect_that(h2o.counts, equals(r.hist$counts))
-      expect_that(h2o.breaks, equals(r.hist$breaks))
+      
+      r.hist <- hist(r.data[,i], breaks=h2o.breaks, right=FALSE)
+      expect_that(h2o.counts, equals(r.hist$counts, tolerance=0.001))
+      logging(cat("Column ", i, " in ", path," successful.\n"))
+    } else {
+      logging(cat("Column ", i, " in ", path," skipped.\n"))
     }
   }
 }
@@ -26,4 +29,5 @@ test.histogram <- function (con, path, key) {
 test.histogram(H2Ocon, '../../smalldata/cars.csv', 'cars.hex')
 test.histogram(H2Ocon, '../../smalldata/pca_test/USArrests.csv', 'usarrests.hex')
 test.histogram(H2Ocon, '../../smalldata/iris/iris22.csv', 'iris.hex')
+test.histogram(H2Ocon, '../../smalldata/test/arit.csv', 'arit.hex')
 
