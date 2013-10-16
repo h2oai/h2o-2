b02d7ddc4e8486555254ed81ef56fe147bbecc81
SpencerA <spnrpa@gmail.com>
2013-10-16 01:18:26 -0700

    add 5 Runits to jenkins

--- py/testdir_release/runner_Runit.sh
+++ py/testdir_release/runner_Runit.sh
@@ -186,6 +186,11 @@ myR runit_tail_numeric 60
 myR runit_summary_numeric 60
 myR runit_GBM_ecology 1200
 myR runit_RF 120
+myR runit_libR_airlines.R 120
+myR runit_libR_prostate.R 120
+myR runit_sliceColHeadTail_iris.R 60
+myR runit_sliceColSummary_iris.R 60
+myR runit_sliceColTypes_iris.R 60
 # If this one fals, fail this script so the bash dies 
 # We don't want to hang waiting for the cloud to terminate.
 # produces xml too!
