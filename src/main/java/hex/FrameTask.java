773f75ee03bb5171dc8e3da6f8ed83b55bc65d73
Tomas Nykodym <tomas.nykodym@gmail.com>
2013-10-18 11:51:57 -0700

    Added separate task to compute gram matrix with categorical expansion and optional on the fly data-standardization and optional intercept..

---
+++ src/main/java/hex/FrameTask.java
@@ -0,0 +1,177 @@
+package hex;
+
+import java.util.Arrays;
+
+import water.*;
+import water.fvec.*;
+
+public abstract class FrameTask<T extends FrameTask<T>> extends MRTask2<T>{
+  protected final boolean _standardize;
+  final int _offset;
+  final int _step;
+  final boolean _complement;
+  final boolean _hasResponse = false;
+  final Job _job;
+
+  protected int _nums = -1;
+  protected int _cats = -1;
+  // offsets of categorcial variables
+  // each categorical with n levels is virtually expanded into a binary vector of length n -1.
+  // _catOffsets basically hold a sum of cardinlities of all preceding categorical variables
+  // to be able to address correct elements in the beta vector (which must be fully expanded).
+  protected int [] _catOffsets;
+  // data-regularization params
+  double [] _normSub; // means to be subtracted
+  double [] _normMul; // 1/sigma to multiply each numeric param with
+  double    _ymu = Double.NaN; // mean of the response
+
+  // size of the expanded vector of parameters
+  final protected int fullN(){
+    return _nums + _catOffsets[_cats];
+  }
+  // size of the top-left strictly diagonal region of the gram matrix, currently just the size of the largest categorical
+  final protected int diagN(){if(_catOffsets.length < 2)return 0; return _catOffsets[1];}
+
+  public FrameTask(Job job, boolean standardize) {this(job,standardize,1,0,false);}
+  public FrameTask(Job job, boolean standardize, int step, int offset, boolean complement) {
+    _standardize = standardize;
+    _job = job;
+    _step = step;
+    _offset = offset;
+    _complement = complement;
+  }
+  protected FrameTask(FrameTask ft){
+    _standardize = ft._standardize;
+    _nums = ft._nums;
+    _cats = ft._cats;
+    _catOffsets = ft._catOffsets;
+    _normMul = ft._normMul;
+    _normSub = ft._normSub;
+    _step = ft._step;
+   _offset = ft._offset;
+   _complement = ft._complement;
+   _job = ft._job;
+  }
+
+  /**
+   * Method to process one row of the data for GLM functions.
+   * Numeric and categorical values are passed separately, as is reponse.
+   * Categoricals are passed as absolute indexes into the expanded beta vector, 0-levels are skipped
+   * (so the number of passed categoricals will not be the same for every row).
+   *
+   * Categorical expansion/indexing:
+   *   Categoricals are placed in the beginning of the beta vector.
+   *   Each cat variable with n levels is expanded into n-1 independent binary variables.
+   *   Indexes in cats[] will point to the appropriate coefficient in the beta vector, so e.g.
+   *   assume we have 2 categorical columns both with values A,B,C, then the following rows will have following indexes:
+   *      A,A - ncats = 0, we do not pass any categorical here
+   *      A,B - ncats = 1, indexes = [2]
+   *      B,B - ncats = 2, indexes = [0,2]
+   *      and so on
+   *
+   * @param nums     - numeric values of this row
+   * @param ncats    - number of passed (non-zero) categoricals
+   * @param cats     - indexes of categoricals into the expanded beta-vector.
+   * @param response - numeric value for the response
+   */
+  protected void processRow(double [] nums, int ncats, int [] cats, double response){throw new RuntimeException("should've been overriden!");}
+  protected void processRow(double [] nums, int ncats, int [] cats){throw new RuntimeException("should've been overriden!");}
+
+  /**
+   * Reorder the frame's columns so that numeric columns come first followed by categoricals ordered by cardinality in decreasing order and
+   * the response is the last.
+   * @param fr
+   * @return
+   */
+  public Frame adaptFrame(Frame fr){
+    final Vec [] vecs = fr.vecs();
+    final int n = vecs.length-(_hasResponse?1:0); // -1 for response
+    int [] nums = MemoryManager.malloc4(n);
+    int [] cats = MemoryManager.malloc4(n);
+    int nnums = 0, ncats = 0;
+    for(int i = 0; i < n; ++i){
+      if(vecs[i].isEnum())
+        cats[ncats++] = i;
+      else
+        nums[nnums++] = i;
+    }
+    // sort the cats in the decreasing order according to their size
+    for(int i = 0; i < ncats; ++i)
+      for(int j = i+1; j < ncats; ++j)
+        if(vecs[cats[i]].domain().length < vecs[cats[j]].domain().length){
+          int x = cats[i];
+          cats[i] = cats[j];
+          cats[j] = x;
+        }
+    Vec [] vecs2 = vecs.clone();
+    String [] names = fr._names.clone();
+    for(int i = 0; i < ncats; ++i){
+      vecs2[i] = vecs[cats[i]];
+      names[i] = fr._names[cats[i]];
+    }
+    for(int i = 0; i < nnums; ++i){
+      vecs2[i+ncats]  = vecs [nums[i]];
+      names[i+ncats] = fr._names[nums[i]];
+    }
+    return new Frame(names,vecs2);
+  }
+  public T doIt(Frame fr){
+    if(_cats == -1 && _nums == -1 ){
+      fr = adaptFrame(fr);
+      assert _normMul == null;
+      assert _normSub == null;
+      int i = 0;
+      final Vec [] vecs = fr.vecs();
+      final int n = _hasResponse?vecs.length-1:vecs.length; // -1 for response
+      while(i < n && vecs[i].isEnum())++i;
+      _cats = i;
+      while(i < n && !vecs[i].isEnum())++i;
+      _nums = i-_cats;
+      if(i != n)
+        throw new RuntimeException("Incorrect format of the input frame. Frame is assumed to be ordered so that categorical columns come before numerics.");
+      _normSub = MemoryManager.malloc8d(_nums);
+      _normMul = MemoryManager.malloc8d(_nums); Arrays.fill(_normMul, 1);
+      if(_standardize) for(i = 0; i < _nums; ++i){
+        _normSub[i] = vecs[i+_cats].mean();
+        _normMul[i] = 1.0/vecs[i+_cats].sigma();
+      }
+      _catOffsets = MemoryManager.malloc4(_cats+1);
+      int len = _catOffsets[0] = 0;
+      for(i = 0; i < _cats; ++i)
+        _catOffsets[i+1] = (len += vecs[i].domain().length - 1);
+      // get mean of response...
+      if(Double.isNaN(_ymu))_ymu = vecs[vecs.length-1].mean();
+    }
+    return dfork(fr).getResult();
+  }
+
+  protected abstract void mapInit();
+  /**
+   * Extracts the values, applies regularization to numerics, adds appropriate offsets to categoricals,
+   * and adapts response according to the CaseMode/CaseValue if set.
+   */
+  @Override public void map(Chunk [] chunks){
+    if(_job != null && _job.cancelled())throw new RuntimeException("Cancelled");
+    mapInit();
+    final int nrows = chunks[0]._len;
+    double [] nums = MemoryManager.malloc8d(_nums);
+    int    [] cats = MemoryManager.malloc4(_cats);
+    final int step = _complement?_step:1;
+    final int start = _complement?_offset:0;
+    OUTER:
+    for(int r = start; r < nrows; r += step){
+      if(_step > step && (r % _step) == _offset)continue;
+      for(Chunk c:chunks)if(c.isNA0(r))continue OUTER; // skip rows with NAs!
+      int i = 0, ncats = 0;
+      for(; i < _cats; ++i){
+        int c = (int)chunks[i].at80(r);
+        if(c != 0)cats[ncats++] = c + _catOffsets[i] - 1;
+      }
+      final int n = _hasResponse?chunks.length-1:chunks.length;
+      for(;i < n;++i)
+        nums[i-_cats] = (chunks[i].at0(r) - _normSub[i-_cats])*_normMul[i-_cats];
+      if(!_hasResponse) processRow(nums, ncats, cats);
+      else processRow(nums, ncats, cats,chunks[chunks.length-1].at0(r));
+    }
+  }
+}
--- src/main/java/hex/gram/Gram.java
+++ src/main/java/hex/gram/Gram.java
@@ -1,6 +1,7 @@
 package hex.gram;
 
 import hex.DLSM.ADMMSolver.NonSPDMatrixException;
+import hex.FrameTask;
 
 import java.text.DecimalFormat;
 import java.util.Arrays;
@@ -12,8 +13,9 @@ import Jama.CholeskyDecomposition;
 import Jama.Matrix;
 
 public final class Gram extends Iced {
-  public static final DecimalFormat dformat = new DecimalFormat("0.#####");
 
+
+  final boolean _hasIntercept;
   double[][] _xx;
   double[] _diag;
   final int _diagN;
@@ -23,51 +25,16 @@ public final class Gram extends Iced {
 //  double _yy;
 //  long _nobs;
 
-  public Gram() {_diagN = _denseN = _fullN = 0;}
+  public Gram() {_diagN = _denseN = _fullN = 0; _hasIntercept = false;}
 
-  public Gram(int N, int diag, int dense, int sparse) {
-//    _xy = MemoryManager.malloc8d(N);
-    _xx = new double[N - diag][];
+  public Gram(int N, int diag, int dense, int sparse, boolean hasIntercept) {
+    _hasIntercept = hasIntercept;
+    _fullN = N + (_hasIntercept?1:0);
+    _xx = new double[_fullN - diag][];
     _diag = MemoryManager.malloc8d(_diagN = diag);
     _denseN = dense;
-    _fullN = N;
     for( int i = 0; i < (_fullN - _diagN); ++i )
       _xx[i] = MemoryManager.malloc8d(diag + i + 1);
-  }
-
-  public String pprint(double[][] arr) {
-    int colDim = 0;
-    for( double[] line : arr )
-      colDim = Math.max(colDim, line.length);
-    StringBuilder sb = new StringBuilder();
-    int max_width = 0;
-    int[] ilengths = new int[colDim];
-    Arrays.fill(ilengths, -1);
-    for( double[] line : arr ) {
-      for( int c = 0; c < line.length; ++c ) {
-        double d = line[c];
-        String dStr = dformat.format(d);
-        if( dStr.indexOf('.') == -1 ) dStr += ".0";
-        ilengths[c] = Math.max(ilengths[c], dStr.indexOf('.'));
-        int prefix = (d >= 0 ? 1 : 2);
-        max_width = Math.max(dStr.length() + prefix, max_width);
-      }
-    }
-    for( double[] line : arr ) {
-      for( int c = 0; c < line.length; ++c ) {
-        double d = line[c];
-        String dStr = dformat.format(d);
-        if( dStr.indexOf('.') == -1 ) dStr += ".0";
-        for( int x = dStr.indexOf('.'); x < ilengths[c] + 1; ++x )
-          sb.append(' ');
-        sb.append(dStr);
-        if( dStr.indexOf('.') == -1 ) sb.append('.');
-        for( int i = dStr.length() - Math.max(0, dStr.indexOf('.')); i <= 5; ++i )
-          sb.append('0');
-      }
-      sb.append("\n");
-    }
-    return sb.toString();
   }
 
   public void addDiag(double d) {
@@ -236,27 +203,31 @@ public final class Gram extends Iced {
   }
 
   public final void addRow(final double[] x, final int catN, final int [] catIndexes, final double w) {
-    final int denseRowStart = _fullN - _denseN - _diagN - 1; // we keep dense numbers at the right bottom of the matrix, -1 is for intercept
-    final int denseColStart = _fullN - _denseN - 1;
+    final int intercept = _hasIntercept?1:0;
+    final int denseRowStart = _fullN - _denseN - _diagN - intercept; // we keep dense numbers at the right bottom of the matrix, -1 is for intercept
+    final int denseColStart = _fullN - _denseN - intercept;
 
-    assert _denseN + denseRowStart == _xx.length-1;
-    final double [] interceptRow = _xx[_denseN + denseRowStart];
+    assert _denseN + denseRowStart == _xx.length-intercept;
+    final double [] interceptRow = _hasIntercept?_xx[_denseN + denseRowStart]:null;
     // nums
     for(int i = 0; i < _denseN; ++i) if(x[i] != 0) {
       final double [] mrow = _xx[i+denseRowStart];
       final double d = w*x[i];
       for(int j = 0; j <= i; ++j)if(x[j] != 0)
         mrow[j+denseColStart] += d*x[j];
-      interceptRow[i+denseColStart] += d; // intercept*x[i]
+      if(_hasIntercept)
+        interceptRow[i+denseColStart] += d; // intercept*x[i]
       // nums * cats
       for(int j = 0; j < catN; ++j)
         mrow[catIndexes[j]] += d;
     }
-    // intercept*intercept
-    interceptRow[_denseN+denseColStart] += w;
-    // intercept X cat
-    for(int j = 0; j < catN; ++j)
-      interceptRow[catIndexes[j]] += w;
+    if(_hasIntercept){
+      // intercept*intercept
+      interceptRow[_denseN+denseColStart] += w;
+      // intercept X cat
+      for(int j = 0; j < catN; ++j)
+        interceptRow[catIndexes[j]] += w;
+    }
     final boolean hasDiag = (_diagN > 0 && catN > 0 && catIndexes[0] < _diagN);
     // cat X cat
     for(int i = hasDiag?1:0; i < catN; ++i){
@@ -273,6 +244,26 @@ public final class Gram extends Iced {
     for(int i = 0; i < _xx.length; ++i)
       for(int j = 0; j < _xx[i].length; ++j)
         _xx[i][j] *= x;
+  }
+
+  public static class GramTask extends FrameTask<GramTask> {
+    final boolean _hasIntercept;
+    public Gram _gram;
+
+    public GramTask(Job job, boolean standardize, boolean hasIntercept){
+      super(job,standardize);
+      _hasIntercept = hasIntercept;
+    }
+    @Override protected void mapInit(){
+      _gram = new Gram(fullN(), diagN(), _nums, _cats,_hasIntercept);
+    }
+    @Override protected void processRow(double[] nums, int ncats, int[] cats) {
+      _gram.addRow(nums, ncats, cats, 1.0);
+    }
+    @Override public void reduce(GramTask gt){
+      _gram.add(gt._gram);
+    }
+
   }
 }
 
