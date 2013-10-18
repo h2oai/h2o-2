217b3f3b70ee25abdd4a5c0e68a48e2db2a7a2bd
Kevin Normoyle <kevin@0xdata.com>
2013-10-18 10:51:16 -0700

    airlines is failing summary. put it last

--- py/testdir_release/runner_Runit.sh
+++ py/testdir_release/runner_Runit.sh
@@ -187,11 +187,12 @@ myR runit_summary_numeric 60
 myR runit_GBM_ecology 1200
 myR runit_RF 120
 myR runit_histograms 60
-myR runit_libR_airlines 120
 myR runit_libR_prostate 120
 myR runit_sliceColHeadTail_iris 60
 myR runit_sliceColSummary_iris 60
 myR runit_sliceColTypes_iris 60
+# airlines is failing summary. put it last
+myR runit_libR_airlines 120
 # If this one fals, fail this script so the bash dies 
 # We don't want to hang waiting for the cloud to terminate.
 # produces xml too!
