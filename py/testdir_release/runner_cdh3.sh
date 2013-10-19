16b33631e349b02628fa8cd305cd8dc7da5dc041
Kevin Normoyle <kevin@0xdata.com>
2013-10-19 13:40:37 -0700

    have them all move the logs between tests

--- py/testdir_release/runner_cdh3.sh
+++ py/testdir_release/runner_cdh3.sh
@@ -138,20 +138,29 @@ export H2O_REMOTE_BUCKETS_ROOT=/home/0xcustomer
 echo "If it exists, pytest_config-<username>.json in this dir will be used"
 echo "i.e. pytest_config-jenkins.json"
 echo "Used to run as 0xcust.., with multi-node targets (possibly)"
-DOIT=../testdir_single_jvm/n0.doit
+myPy() {
+    DOIT=../testdir_single_jvm/n0.doit
+    $DOIT $1/$2 || true
+    # try moving all the logs created by this test in sandbox to a subdir to isolate test failures
+    # think of h2o.check_sandbox_for_errors()
+    rm -f -r sandbox/$1
+    mkdir -p sandbox/$1
+    cp -f sandbox/*log sandbox/$1
+    rm -f sandbox/*log
+}
 
-# $DOIT c5/test_c5_KMeans_sphere15_180GB.py || true
+# myPy c5 test_c5_KMeans_sphere15_180GB.py
 
 # don't run this until we know whether 0xcustomer permissions also exist for the hadoop job
-# $DOIT c1/test_c1_rel.py || true
-$DOIT c2/test_c2_rel.py || true
-# $DOIT c3/test_c3_rel.py || true
-# $DOIT c4/test_c4_four_billion_rows.py || true
-$DOIT c6/test_c6_hdfs.py || true
+# myPy c1 test_c1_rel.py
+myPy c2 test_c2_rel.py
+# myPy c3 test_c3_rel.py
+# myPy c4 test_c4_four_billion_rows.py
+myPy c6 test_c6_hdfs.py
 
 # If this one fails, fail this script so the bash dies 
 # We don't want to hang waiting for the cloud to terminate.
-../testdir_single_jvm/n0.doit test_shutdown.py
+myPy shutdown test_shutdown.py
 
 echo "Maybe it takes some time for hadoop to shut it down? sleep 10"
 sleep 10
