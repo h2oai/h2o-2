e7085ef5de793864c37fe855505bdb7c2e2f56d5
SpencerA <spnrpa@gmail.com>
2013-10-20 01:29:09 -0700

    rename to loggers to avoid accidental kill (ensures logging stops)

---
+++ bench/startloggers.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#ssh into each of the machines from ${JSON} (passed as first argument)
+MACHINES=`cat BMscripts/$1 | python -c 'import sys, json; print json.load(sys.stdin)[sys.argv[1]]' ip | awk -F, 'OFS="," {gsub("[ u\x27\[\]]","", $0); print}'`
+IFS=","
+MACHINES=($MACHINES)
+
+function startBigLoggers {
+    for i in ${MACHINES[@]}
+    do
+        echo "Starting bigLogger on ${i}"
+        scp BMLogs/starttime spencer@$i:/home/spencer/h2o/bench/BMLogs
+        ssh spencer@$i "cd /home/spencer/h2o/bench/BMLogs; bash bigLogger.sh" &
+    done
+}
+
+function startLittleLoggers {
+    for i in ${MACHINES[@]}
+    do
+        echo "Starting littleLogger on ${i} on phase $1"
+        ssh spencer@$i "cd /home/spencer/h2o/bench/BMLogs; bash littleLogger.sh $1" &
+    done
+}
+
+function stopLittleLoggers {
+    for i in ${MACHINES[@]}
+    do
+        "Stopping little loggers on machine $i"
+        pids=`echo spencer@$i ps ax|grep bash|grep littleLogger|awk '{print $1}'| xargs`
+        ssh spencer@$i kill $pids
+    done
+}
+
+function stopAllLoggers {
+    for i in ${MACHINES[@]}
+    do
+        "Stopping all loggers on machine $i"
+        pids=`echo ssh spencer@$i ps ax|grep bash|grep Logger|awk '{print $1}'| xargs`
+        ssh spencer@$i kill $pids
+    done
+}
+
+function changePhase {
+  echo "Stopping little loggers"
+  stopLittleLoggers >/dev/null
+  newPhase=$1
+  startLittleLoggers $1 >/dev/null
+}
+
+if [ $2 = "big" ]
+then
+    startBigLoggers >/dev/null
+fi
+
+if [ $2 = "little" ]
+then
+    startLittleLoggers START >/dev/null
+fi
+
+if [ $2 = "changePhase" ]
+then
+    changePhase $3 >/dev/null
+fi
+
+if [ $2 = "stop_" ]
+then
+    stopAllLoggers
+fi
