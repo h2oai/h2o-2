51bd1b6929765a9a43c73d73b8522678bf2bfdc7
Tomas Nykodym <tomas.nykodym@gmail.com>
2013-10-21 12:00:15 -0700

    Updated GLMProgress2 to work with updated param names.

--- src/main/java/hex/glm/GLMProgressPage2.java
+++ src/main/java/hex/glm/GLMProgressPage2.java
@@ -4,21 +4,21 @@ package hex.glm;
 import water.*;
 import water.api.Progress2;
 import water.api.Request;
+import water.api.RequestBuilders.Response;
 
 import com.google.gson.JsonObject;
 
 public class GLMProgressPage2 extends Progress2 {
   /** Return {@link Response} for finished job. */
-  protected Response jobDone(final Job job, final String dst) {
+  @Override
+  protected Response jobDone(final Job job, final Key dst) {
     JsonObject args = new JsonObject();
     args.addProperty("model_key", job.dest().toString());
     return GLMModelView.redirect(this, job.dest());
   }
-
   public static Response redirect(Request req, Key jobkey, Key dest) {
-    return new Response(Response.Status.redirect, req, -1, -1, "GLMProgressPage2", "job", jobkey, "dst_key", dest );
+    return new Response(Response.Status.redirect, req, -1, -1, "GLMProgressPage2", "job_key", jobkey, "destination_key", dest );
   }
-
   @Override public boolean toHTML( StringBuilder sb ) {
     Job jjob = Job.findJob(job_key);
     Value v = DKV.get(jjob.dest());
@@ -29,5 +29,4 @@ public class GLMProgressPage2 extends Progress2 {
       sb.append("<b>No model yet.</b>");
     return true;
   }
-
 }
