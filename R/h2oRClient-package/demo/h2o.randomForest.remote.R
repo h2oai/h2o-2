6f095e96021ce15359526ff8b80ad160fa159a0a
anqi <anqi@0xdata.com>
2013-10-21 12:59:39 -0700

    Added R demos that can run on remote machines

--- R/h2oRClient-package/demo/h2o.randomForest.remote.R
+++ R/h2oRClient-package/demo/h2o.randomForest.remote.R
@@ -4,16 +4,18 @@
 # Note: This demo runs H2O on localhost:54321
 library(h2o)
 h2o.installDepPkgs()
-localH2O = h2o.init(ip = "localhost", port = 54321, startH2O = TRUE, silentUpgrade = TRUE, promptUpgrade = FALSE)
+myIP = readline("Enter IP address of H2O server: ")
+myPort = readline("Enter port number of H2O server: ")
+remoteH2O = h2o.init(ip = myIP, port = as.numeric(myPort), startH2O = FALSE, silentUpgrade = TRUE, promptUpgrade = FALSE)
 
-iris.hex = h2o.importFile(localH2O, path = system.file("extdata", "iris.csv", package="h2oRClient"), key = "iris.hex")
+iris.hex = h2o.uploadFile(remoteH2O, path = system.file("extdata", "iris.csv", package="h2oRClient"), key = "iris.hex")
 summary(iris.hex)
 iris.rf = h2o.randomForest(y = 5, x = c(1,2,3,4), data = iris.hex, ntree = 50, depth = 100, classwt = c("Iris-versicolor" = 20.0, "Iris-virginica" = 30.0))
 print(iris.rf)
 
 invisible(readline("Hit <Return> to continue: "))
 
-covtype.hex = h2o.importFile(localH2O, path = system.file("extdata", "covtype.csv", package="h2oRClient"), key = "covtype.hex")
+covtype.hex = h2o.uploadFile(remoteH2O, path = system.file("extdata", "covtype.csv", package="h2oRClient"), key = "covtype.hex")
 summary(covtype.hex)
 covtype.rf = h2o.randomForest(y = "Cover_Type", x = setdiff(colnames(covtype.hex), c("Cover_Type", "Aspect", "Hillshade_9am")), data = covtype.hex, ntree = 50, depth = 150)
 print(covtype.rf)
