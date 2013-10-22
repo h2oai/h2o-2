af6cbdf213a201292d25dfd13cc9b23908f8c8a1
SpencerA <spnrpa@gmail.com>
2013-10-22 11:33:51 -0700

    fixes for recent infrastructure change

--- bench/BMscripts/glmBench.py
+++ bench/BMscripts/glmBench.py
@@ -116,6 +116,7 @@ def doGLM(f, folderPath, family, link, lambda_, alpha, nfolds, y, x, testFilehex
         output.close()
 
 if __name__ == '__main__':
+    dat   = sys.argv.pop(-1)
     debug = sys.argv.pop(-1)
     build = sys.argv.pop(-1)
     h2o.parse_our_args()
