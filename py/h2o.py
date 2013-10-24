803844983971451344a742efb6fa4c3dfa75a773
Kevin Normoyle <kevin@0xdata.com>
2013-10-24 14:44:37 -0700

    trying KMeans2ModelView and KMeans2 moving h2o on cdh4 to 3 nodes of 20GB

--- py/h2o.py
+++ py/h2o.py
@@ -1134,6 +1134,27 @@ class H2O(object):
             time.sleep(5)
         return a
 
+    # this is only for 2 (fvec)
+    def kmeans_model_view(self, model, timeoutSecs=30, **kwargs):
+        # defaults
+        params_dict = {
+            'model': model,
+            }
+        browseAlso = kwargs.get('browseAlso', False)
+        # only lets these params thru
+        check_params_update_kwargs(params_dict, kwargs, 'kmeans_model_view', print_params=True)
+        print "\nKMeans2ModelView params list:", params_dict
+        a = self.__do_json_request('2/KMeans2ModelView.json', timeout=timeoutSecs, params=params_dict)
+
+        # kmeans_score doesn't need polling?
+        verboseprint("\nKMeans2Model View result:", dump_json(a))
+
+        if (browseAlso | browse_json):
+            print "Redoing the KMeans2ModelView through the browser, no results saved though"
+            h2b.browseJsonHistoryAsUrlLastMatch('KMeans2ModelView')
+            time.sleep(5)
+        return a
+
     # additional params include: cols=.
     # don't need to include in params_dict it doesn't need a default
     # FIX! cols should be renamed in test for fvec
