44bc407a87cd101a00ed5b2e8890b3b151370423
Tom Kraljevic <tomk@tomk.net>
2013-11-20 01:09:34 -0800

    Added RemoveAllKeysTask so make Spencer's RemoveAll operation work for the cluster.

--- src/main/java/water/api/RemoveAll.java
+++ src/main/java/water/api/RemoveAll.java
@@ -1,29 +1,16 @@
 package water.api;
 
 import com.google.gson.JsonObject;
-import water.H2O;
-import water.Key;
-import water.UKV;
 import water.util.Log;
+import water.util.RemoveAllKeysTask;
 
 public class RemoveAll extends JSONOnlyRequest {
   @Override
   protected Response serve() {
     try {
-      Log.info("Removing all keys!!!");
-      Log.info("Removing "+H2O.keySet().size()+" keys");
-      final int numKeys = H2O.keySet().size();
-      Key[] keys = new Key[numKeys];
-      int len = 0;
-      //Loop over keys
-      for( Key key : H2O.keySet() ) {
-        if( H2O.get(key) == null ) continue;
-        keys[len++] = key;
-        if( len == keys.length ) break;
-      }
-      //remove keys
-      UKV.removeAll(keys);
-      assert H2O.keySet().size() <= 1; //1 null key left over
+      Log.info("Removing all keys for the cluster");
+      RemoveAllKeysTask collector = new RemoveAllKeysTask();
+      collector.invokeOnAllNodes();
     } catch( Exception e ) {
       return Response.error(e.getMessage());
     }
