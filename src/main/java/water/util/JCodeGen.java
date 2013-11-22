17d67c93def5339762b1c6ff64dac5e3a406e6fd
mmalohlava <michal.malohlava@gmail.com>
2013-11-21 18:13:00 -0800

    Renamed jcodegen utility.

---
+++ src/main/java/water/util/JCodeGen.java
@@ -0,0 +1,35 @@
+package water.util;
+
+import water.fvec.Frame;
+import water.fvec.Vec;
+
+public class JCodeGen {
+
+  /**
+   * Outputs given frame as static variable with given name.
+   */
+  public static SB toStaticVar(SB sb, Frame f, String varname, int nrows) {
+    Vec[] vecs = f.vecs();
+    sb.indent().p("public static final double[][] DATA = new double[][] {").nl();
+    for( int row = 0; row < nrows; row++ ) {
+      sb.indent(1).p(row > 0 ? "," : "").p("new double[] {");
+      for( int v = 0; v < vecs.length; v++ )
+        sb.p(v > 0 ? "," : "").p(vecs[v].at(row));
+      sb.p("}").nl();
+    }
+    sb.indent().p("};").nl();
+    return sb;
+  }
+
+  public static SB toStaticVar(SB sb, String varname, int value) {
+    return sb.indent().p("public static final int ").p(varname).p(" = ").p(value).p(';').nl();
+  }
+
+  /**
+   * Transform given string to legal java Identifier (see Java grammar http://docs.oracle.com/javase/specs/jls/se7/html/jls-3.html#jls-3.8)
+   *
+   */
+  public static String toJavaId(String s) {
+    return s.replace('-', '_');
+  }
+}
