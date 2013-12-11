1677be00e6b13e6ec4cf14c3f064ca50b9d888a4
cliffclick <cliffc@acm.org>
2013-12-10 18:45:00 -0800

    Some alternative testing samples

--- src/test/java/hex/drf/DRFTest2.java
+++ src/test/java/hex/drf/DRFTest2.java
@@ -15,11 +15,14 @@ public class DRFTest2 extends TestUtil {
   static final long[][] a(long[] ...arr) { return arr; }
 
   // A bigger DRF test, useful for tracking memory issues.
-  /*@Test*/ public void testAirlines() throws Throwable {
+  @Test public void testAirlines() throws Throwable {
     new DRFTest().basicDRF(
+        //
+        //"../demo/c5/row10000.csv.gz", "c5.hex", null, null, 
         "../datasets/UCI/UCI-large/covtype/covtype.data", "covtype.hex", null, null, 
+        //"./smalldata/iris/iris_wheader.csv", "iris.hex", null, null, 
         new DRFTest.PrepData() { @Override int prep(Frame fr) { return fr.numCols()-1; } },
-        10,
+        10/*ntree*/,
         a( a(154871,  52304,    18,    0,   24,   15,   935),  
            a( 34958, 240871,  1423,   21,  243,  656,    56),  
            a(     4,   3836, 30040,  203,    1, 1049,     0),  
