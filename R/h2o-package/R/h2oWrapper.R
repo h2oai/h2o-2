691382d9bd698d8473c0b9d502ad1cf312d4db9e
earlh <github@earlh.com>
2013-12-13 12:13:21 -0800

    Revert "Minor demo changes"
    This reverts commit 6f45a35cfd21be601f34bbd8dc919b42cc98d47b.
    
    accidentally added startjar changes

--- R/h2o-package/R/h2oWrapper.R
+++ R/h2o-package/R/h2oWrapper.R
@@ -23,10 +23,10 @@ setMethod("h2o.init", signature(ip="character", port="numeric", startH2O="logica
       stop(paste("Cannot connect to H2O server. Please check that H2O is running at", myURL))
     else if(ip=="localhost" || ip=="127.0.0.1") {
       print("H2O is not running yet, starting it now.")
-      # h2oWrapper.startLauncher()
-      # invisible(readline("Start H2O, then hit <Return> to continue: "))
-      h2o.startJar()
-      count = 0; while(!url.exists(myURL) && count < 10) { Sys.sleep(1); count = count + 1 }
+      h2oWrapper.startLauncher()
+      invisible(readline("Start H2O, then hit <Return> to continue: "))
+      # h2o.startJar()
+      # count = 0; while(!url.exists(myURL) && count < 10) { Sys.sleep(1); count = count + 1 }
       if(!url.exists(myURL)) stop("H2O failed to start, stopping execution.")
     } else stop("Can only start H2O launcher if IP address is localhost")
   }
