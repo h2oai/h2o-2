54685a360e7d169437bc3116c70f30a0de332cc0
cliffclick <cliffc@acm.org>
2013-12-17 16:32:51 -0800

    Yet Another Boot Order /TypeMap Fix
    Make typemap IDs even more lazy: delay until an actual attempt t Ice
    something.  This way we can load a class without requiring a typeid for
    it... as long as it's only loading.  Fixs all those classes which load
    because the web server wants a shallow instance, but they never execute.
    Remove the giant static list of required-up-front types.

--- src/main/java/water/api/TypeaheadKeysRequest.java
+++ src/main/java/water/api/TypeaheadKeysRequest.java
@@ -17,7 +17,7 @@ public class TypeaheadKeysRequest extends TypeaheadRequest {
   final int _typeid;            // Also filter for Keys of this type
   public TypeaheadKeysRequest(String msg, String filter, Class C) {
     super(msg, filter);
-    _typeid = C==null ? 0 : TypeMap.onLoad(C.getName());
+    _typeid = C==null ? 0 : TypeMap.onIce(C.getName());
   }
 
   @Override
@@ -95,9 +95,6 @@ class TypeaheadPCAModelKeyRequest extends TypeaheadKeysRequest {
 }
 
 class TypeaheadHexKeyRequest extends TypeaheadKeysRequest {
-  final int _frame = TypeMap.onLoad(Frame.class.getName());
-  final int _va = TypeMap.onLoad(ValueArray.class.getName());
-
   public TypeaheadHexKeyRequest() {
     super("Provides a simple JSON array of filtered keys known to the "+
           "current node that are ValueArrays at the time of calling.",
@@ -105,8 +102,8 @@ class TypeaheadHexKeyRequest extends TypeaheadKeysRequest {
   }
 
   @Override protected boolean matchesType(Value val) {
-    if(val.type() == _va)
+    if( val.type() == TypeMap.VALUE_ARRAY ) 
       return val.isHex();
-    return val.type() == _frame;
+    return val.type() == TypeMap.FRAME;
   }
 }
