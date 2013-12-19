37f8d329fb7cb1ccdc690075972a9afbc8383f93
Tongxin Bai <waffle.bai@gmail.com>
2013-12-18 18:36:20 -0800

    runit for row slicing.

--- R/tests/testdir_munging/testdir_slice/runit_sliceRows_RIndex.R
+++ R/tests/testdir_munging/testdir_slice/runit_sliceRows_RIndex.R
@@ -8,20 +8,39 @@ test.slice.rows <- function(conn) {
   R <- as.data.frame(H)
   
   Log.info("Compare H[I,] and R[I,],  range of I is included in range of the data frame.")
+  I <- c(30, 1, 5)
+  Log.info("head(H[I,])")
+  print(head(H[I,2:6]))
+  Log.info("head(R[I,])")
+  print(head(R[I,2:6]))
+  DSlice <- as.data.frame(H[I, 2:6])
+  RSlice <- R[I,2:6]
+  expect_that(all(DSlice == RSlice), equals(T))
+  
+  Log.info("Compare H[I,] and R[I,],  range of I is included in range of the data frame.")
+  
   I <- sample(1:nrow(R), 1000, replace=T)
   Log.info("head(H[I,])")
   print(head(H[I,2:6]))
   Log.info("head(R[I,])")
   print(head(R[I,2:6]))
-  expect_that(as.data.frame(H[I,2:6]), equals(R[I,2:6]))
+  DSlice <- as.data.frame(H[I, 2:6])
+  RSlice <- R[I,2:6]
+  expect_that(all(DSlice[!is.na(DSlice)] == RSlice[!is.na(RSlice)]), equals(T))
 
+  
   Log.info("Compare H[I,] and R[I,],  range of I goes beyond the range of the data frame.")
   I <- c(nrow(R) + 1, nrow(R) + 2, I)
   Log.info("head(H[I,])")
   print(head(H[I,]))
   Log.info("head(R[I,])")
   print(head(R[I,]))  
-  expect_that(as.data.frame(H[I,2:6]), equals(R[I,2:6]))
+  DSlice <- as.data.frame(H[I, 2:6])
+  RSlice <- R[I,2:6]
+  expect_that(all(is.na(DSlice) == is.na(RSlice)), equals(T))
+  expect_that(all(DSlice[!is.na(DSlice)] == RSlice[!is.na(RSlice)]), equals(T))
+
+  Log.info("Compares H[null,]")
   
   testEnd()
 }
