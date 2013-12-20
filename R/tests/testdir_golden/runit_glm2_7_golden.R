3a2c17ce26762d1f3fe91cd65ace26bb64398e57
Irene L <irene@0xdata.com>
2013-12-20 15:51:06 -0800

    edits to golden tests

--- R/tests/testdir_golden/runit_glm2_7_golden.R
+++ R/tests/testdir_golden/runit_glm2_7_golden.R
@@ -15,17 +15,23 @@ fitH2O<- h2o.glm.FV(x=c("nofish","livebait","camper"), y="count", family="tweedi
 fitR<- glm(count ~ nofish + livebait+ camper, family=tweedie(var.power=1.5), data=zinabR)
 
 Log.info("Print model statistics for R and H2O... \n")
-Log.info(paste("H2O Deviance  : ", fitH2O@model$deviance,      "\t\t", "R Deviance   : ", fitR$deviance))
-Log.info(paste("H2O Null Dev  : ", fitH2O@model$null.deviance, "\t\t", "R Null Dev   : ", fitR$null.deviance))
-Log.info(paste("H2O residul df: ", fitH2O@model$df.residual,    "\t\t\t\t", "R residual df: ", fitR$df.residual))
-Log.info(paste("H2O null df   : ", fitH2O@model$df.null,       "\t\t\t\t", "R null df    : ", fitR$df.null))
+#Log.info(paste("H2O Deviance  : ", fitH2O@model$deviance,      "\t\t", "R Deviance   : ", fitR$deviance))
+#Log.info(paste("H2O Null Dev  : ", fitH2O@model$null.deviance, "\t\t", "R Null Dev   : ", fitR$null.deviance))
+#Log.info(paste("H2O residul df: ", fitH2O@model$df.residual,    "\t\t\t\t", "R residual df: ", fitR$df.residual))
+#Log.info(paste("H2O null df   : ", fitH2O@model$df.null,       "\t\t\t\t", "R null df    : ", fitR$df.null))
+Log.info(paste("H2O coefficients  : ", H2Ocoeffs,      "\t\t", "R coefficients   : ", Rcoeffs))
 
 
 Log.info("Compare model coefficients in R to model statistics in H2O")
-expect_equal(fitH2O@model$null.deviance, fitR$null.deviance, tolerance = 0.01)
-expect_equal(fitH2O@model$deviance, fitR$deviance, tolerance = 0.01)
-expect_equal(fitH2O@model$df.residual, fitR$df.residual, tolerance = 0.01)
-expect_equal(fitH2O@model$df.null, fitR$df.null, tolerance = 0.01)
+
+#expect_equal(fitH2O@model$null.deviance, fitR$null.deviance, tolerance = 0.01)
+#expect_equal(fitH2O@model$deviance, fitR$deviance, tolerance = 0.01)
+#expect_equal(fitH2O@model$df.residual, fitR$df.residual, tolerance = 0.01)
+#expect_equal(fitH2O@model$df.null, fitR$df.null, tolerance = 0.01)
+H2Ocoeffs<- sort(t(fitH2O@model$coefficients))
+Rcoeffs<- sort(t(fitR$coefficients))
+expect_equal(H2Ocoeffs, Rcoeffs, tolerance = 0.01)
+
 
   testEnd()
 }
