d0c8bcd8a600695e9f772ab7b336f70c61ace0bb
sris <srisatish.ambati@gmail.com>
2013-12-21 11:43:02 -0800

    RF1 test for VA

---
+++ R/tests/testdir_algos/testdir_rf/runit_RF.VA_covtype_all.R
@@ -0,0 +1,12 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"-f")))
+source('../../findNSourceUtils.R')
+
+test.RF.VA.covtype_class <- function(conn) {
+  covtype.hex = h2o.uploadFile.VA(conn, locate( "smalldata/covtype/covtype.20k.data"), "covtype.hex")
+  covtype.rf = h2o.randomForest.VA(y = 54, x = seq(1,53), data = covtype.hex, ntree = 50, depth = 100) 
+  print(covtype.rf)
+  testEnd()
+}
+
+doTest("RF1 VA test covtype all", test.RF.VA.covtype_class)
+
