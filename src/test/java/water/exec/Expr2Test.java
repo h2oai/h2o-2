1172260c168753d18ed0774666ef3dcbe9429a22
Tongxin Bai <waffle.bai@gmail.com>
2014-01-08 15:41:34 -0800

    New changes to exec2: Inf, min.na.rm, max.na.rm
    Add Inf as global constant
    add operators min.na.rm and max.na.rm, which act as R's min(..., na.rm=T)

--- src/test/java/water/exec/Expr2Test.java
+++ src/test/java/water/exec/Expr2Test.java
@@ -71,7 +71,14 @@ public class Expr2Test extends TestUtil {
       checkStr("(1.23+h.hex)-h.hex");
       checkStr("min(h.hex,1+2)");
       checkStr("max(h.hex,1+2)");
+      checkStr("min.na.rm(h.hex,NA)"); // 0
+      checkStr("max.na.rm(h.hex,NA)"); // 211.3375
+      checkStr("min.na.rm(c(NA, 1), -1)"); // -1
+      checkStr("max.na.rm(c(NA, 1), -1)"); // 1
+      checkStr("max(c(Inf,1),2)");     // Infinity
+      checkStr("min(c(Inf,1),-Inf)");  // -Infinity
       checkStr("is.na(h.hex)");
+      checkStr("sum(is.na(h.hex))");
       checkStr("nrow(h.hex)*3");
       checkStr("h.hex[nrow(h.hex)-1,ncol(h.hex)-1]");
       checkStr("1=2");
