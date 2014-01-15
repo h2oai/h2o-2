c8b3349922e06c24047385c6419bc884453ef8e2
anqi <anqi@0xdata.com>
2014-01-14 18:47:03 -0800

    Fix for 1459. Switch h2o.table to table

--- R/h2oRClient-package/R/Classes.R
+++ R/h2oRClient-package/R/Classes.R
@@ -427,11 +427,94 @@ setMethod("h2o.cut", signature(x="H2OParsedData", breaks="numeric"), function(x,
   new("H2OParsedData", h2o=x@h2o, key=res$dest_key)
 })
 
-h2o.table <- function(..., exclude = if (useNA == "no") c(NA, NaN), useNA = c("no", "ifany", "always"), dnn = list.names(...), deparse.level = 1) { 
-  myData = c(...); idx = sapply(myData, function(y) { class(y) == "H2OParsedData" })
-  if(any(!idx)) stop("input must be H2OParsedData objects")
-  if(length(myData) > 1 || ncol(myData[[1]]) > 2) stop("Unimplemented")
-  h2o.__unop2("table", myData[[1]]) 
+# TODO: H2O doesn't support any arguments beyond the single H2OParsedData object (with <= 2 cols)
+table.internal <- table
+table <- function(..., exclude = if (useNA == "no") c(NA, NaN), useNA = c("no", "ifany", "always"), dnn = list.names(...), deparse.level = 1) {
+  idx = sapply(c(...), function(x) { class(x) == "H2OParsedData" })
+  if(any(idx) && !all(idx))
+    stop("Can't mix H2OParsedData objects with R objects in table")
+  else if(any(idx)) {
+    myData = c(...)
+    if(length(myData) > 1 || ncol(myData[[1]]) > 2) stop("Unimplemented")
+    h2o.__unop2("table", myData[[1]]) 
+  } else {
+    list.names <- function(...) {
+      l <- as.list(substitute(list(...)))[-1L]
+      nm <- names(l)
+      fixup <- if (is.null(nm)) 
+        seq_along(l)
+      else nm == ""
+      dep <- vapply(l[fixup], function(x) switch(deparse.level + 
+                                                   1, "", if (is.symbol(x)) as.character(x) else "", 
+                                                 deparse(x, nlines = 1)[1L]), "")
+      if (is.null(nm)) 
+        dep
+      else {
+        nm[fixup] <- dep
+        nm
+      }
+    }
+    if (!missing(exclude) && is.null(exclude)) 
+      useNA <- "always"
+    useNA <- match.arg(useNA)
+    args <- list(...)
+    if (!length(args)) 
+      stop("nothing to tabulate")
+    if (length(args) == 1L && is.list(args[[1L]])) {
+      args <- args[[1L]]
+      if (length(dnn) != length(args)) 
+        dnn <- if (!is.null(argn <- names(args))) 
+          argn
+      else paste(dnn[1L], seq_along(args), sep = ".")
+    }
+    bin <- 0L
+    lens <- NULL
+    dims <- integer()
+    pd <- 1L
+    dn <- NULL
+    for (a in args) {
+      if (is.null(lens)) 
+        lens <- length(a)
+      else if (length(a) != lens) 
+        stop("all arguments must have the same length")
+      cat <- if (is.factor(a)) {
+        if (any(is.na(levels(a)))) 
+          a
+        else {
+          if (is.null(exclude) && useNA != "no") 
+            addNA(a, ifany = (useNA == "ifany"))
+          else {
+            if (useNA != "no") 
+              a <- addNA(a, ifany = (useNA == "ifany"))
+            ll <- levels(a)
+            a <- factor(a, levels = ll[!(ll %in% exclude)], 
+                        exclude = if (useNA == "no") 
+                          NA)
+          }
+        }
+      }
+      else {
+        a <- factor(a, exclude = exclude)
+        if (useNA != "no") 
+          addNA(a, ifany = (useNA == "ifany"))
+        else a
+      }
+      nl <- length(ll <- levels(cat))
+      dims <- c(dims, nl)
+      if (prod(dims) > .Machine$integer.max) 
+        stop("attempt to make a table with >= 2^31 elements")
+      dn <- c(dn, list(ll))
+      bin <- bin + pd * (as.integer(cat) - 1L)
+      pd <- pd * nl
+    }
+    names(dn) <- dnn
+    bin <- bin[!is.na(bin)]
+    if (length(bin)) 
+      bin <- bin + 1L
+    y <- array(tabulate(bin, pd), dims, dimnames = dn)
+    class(y) <- "table"
+    y
+  }   
 }
 
 h2o.runif <- function(x) { h2o.__unop2("runif", x) }
