b598e5eddcb9f1eb2a76c09625ce4f80f18ff447
Irene L <irene@0xdata.com>
2014-01-17 07:04:02 -0800

    runits for R docs

---
+++ R/tests/testdir_golden/runit_Rdoc_colnames.R
@@ -0,0 +1,16 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"-f")))
+source('../findNSourceUtils.R')
+
+test.rdoccolnames.golden <- function(H2Oserver) {
+	
+
+irisPath = system.file("extdata", "iris.csv", package="h2oRClient")
+iris.hex = h2o.importFile(H2Oserver, path = irisPath, key = "iris.hex")
+summary(iris.hex)
+colnames(iris.hex)
+
+testEnd()
+}
+
+doTest("R Doc K Means", test.rdoccolnames.golden)
+
