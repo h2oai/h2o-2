b398619a21f50a1fa14b860be7948d45cb5ea67a
mmalohlava <michal.malohlava@gmail.com>
2014-01-22 12:40:31 -0800

    Report proper NaN if mapping for a given enum-value is not found.

--- src/main/java/water/fvec/TransfVec.java
+++ src/main/java/water/fvec/TransfVec.java
@@ -39,7 +39,7 @@ public class TransfVec extends Vec {
     final int   _domMin;
     public TransfChunk(Chunk c, int[] domMap, int domMin, Vec vec) { _c  = c; _domMap = domMap; _domMin = domMin; _len = _c._len; _start = _c._start; _vec = vec; }
     @Override protected long at8_impl(int idx) { return _domMap[(int)_c.at8_impl(idx)-_domMin]; }
-    @Override protected double atd_impl(int idx) { return _c.isNA0(idx) ? Double.NaN : at8_impl(idx);  }
+    @Override protected double atd_impl(int idx) { double d = 0; return _c.isNA0(idx) ? Double.NaN : ( (d=at8_impl(idx)) == -1 ? Double.NaN : d ) ;  }
     @Override protected boolean isNA_impl(int idx) {
       if (_c.isNA_impl(idx)) return true;
       return at8_impl(idx) == -1; // this case covers situation when there is no mapping
