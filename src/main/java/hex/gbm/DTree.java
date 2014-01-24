2c46ba2314bef6159cca2c39e83f854712213254
mmalohlava <michal.malohlava@gmail.com>
2014-01-23 19:24:11 -0800

    On the way to fix PUB-65, PUB-66: wrong comparision in generated code.
    Generated code was comparing double with float, interpreter was
    comparing float with float. It led to different behaviour in a few
    cases.
    
    E.g. double d = 1.98f
     ( ( (float) d) < 1.98f ) != (d < 1.98f) )

--- src/main/java/hex/gbm/DTree.java
+++ src/main/java/hex/gbm/DTree.java
@@ -925,7 +925,7 @@ public class DTree extends Iced {
       JCodeGen.toStaticVar(sb, "NTREES_INTERNAL", numTrees()*nclasses(), "Number of internal trees in this model (= NTREES*NCLASSES).");
       JCodeGen.toStaticVar(sb, "DEFAULT_ITERATIONS", 10000, "Default number of iterations.");
       // Generate a data in separated class since we do not want to influence size of constant pool of model class
-      JCodeGen.toClass(fileContextSB, "//Sample of data used by benchmark\nclass DataSample", "DATA", ValueArray.asFrame(DKV.get(_dataKey)).subframe(_names), 10, "Sample test data.");
+      JCodeGen.toClass(fileContextSB, "// Sample of data used by benchmark\nclass DataSample", "DATA", ValueArray.asFrame(DKV.get(_dataKey)).subframe(_names), 10, "Sample test data.");
 
       return sb;
     }
@@ -1132,7 +1132,7 @@ public class DTree extends Iced {
         preamble(_sb, _subtrees);
         _subtrees++;
       }
-      _sb.p(" (data[").p(col).p("/* ").p(_tm._names[col]).p(" */").p("] ").p(equal?"!= ":"< ").pj(fcmp); // then left and then right (left is !=)
+      _sb.p(" ((float) data[").p(col).p(" /* ").p(_tm._names[col]).p(" */").p("] ").p(equal?"!= ":"< ").pj(fcmp); // then left and then right (left is !=)
       assert _bits[_depth]==0;
       _bits[_depth]=1;
     }
