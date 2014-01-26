27ef8deba7a5c637183e26d838b39a540d5a842b
Tom Kraljevic <tomk@tomk.net>
2014-01-26 15:04:20 -0800

    Added covtype example too.

--- R/tests/testdir_hdfs/runit_hdfs_kmeans.R
+++ R/tests/testdir_hdfs/runit_hdfs_kmeans.R
@@ -27,7 +27,7 @@ running_inside_hexdata = (rv == 0)
 if (running_inside_hexdata) {
     hdfs_name_node = "192.168.1.161"    
     hdfs_iris_file = "/datasets/runit/iris_wheader.csv"
-    hdfs_iris_dir  = "/datasets/runit/iris_test_train"
+    hdfs_covtype_file = "/datasets/covtype.data"
 } else {
     stop("Not running on 0xdata internal network.  No access to HDFS.")
 }
@@ -42,7 +42,7 @@ conn <- new("H2OClient", ip=myIP, port=myPort)
 # Single file cases.
 #----------------------------------------------------------------------
 
-heading("Running importHDFS")
+heading("Running iris importHDFS")
 url <- sprintf("hdfs://%s%s", hdfs_name_node, hdfs_iris_file)
 iris.hex <- h2o.importHDFS(conn, url)
 n <- nrow(iris.hex)
@@ -51,8 +51,24 @@ if (n != 150) {
     stop("nrows is wrong")
 }
 
-heading("Running KMeans")
+heading("Running iris KMeans")
 iris.km = h2o.kmeans(data = iris.hex, centers = 3, cols = c(1, 2, 3, 4), iter.max=10)
 iris.km
+
+
+
+heading("Running covtype importHDFS")
+url <- sprintf("hdfs://%s%s", hdfs_name_node, hdfs_covtype_file)
+covtype.hex <- h2o.importHDFS(conn, url)
+n <- nrow(covtype.hex)
+print(n)
+if (n != 581012) {
+    stop("nrows is wrong")
+}
+
+heading("Running covtype KMeans")
+covtype.km = h2o.kmeans(data = covtype.hex, centers = 8, cols = colnames(covtype.hex), iter.max=10)
+covtype.km
+
 
 PASS_BANNER()
