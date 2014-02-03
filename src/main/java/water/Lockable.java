94621c0e9131d6e2eeda02ff5226b00fb21b9144
Tomas Nykodym <tomas.nykodym@gmail.com>
2014-02-02 20:15:37 -0800

    Changed locking operations to preserve _persist flag.
    Default TAtomic behavior is to set _persist to ICED and not on disk, which is correct for most atomic operations which update the data. However, locking does not update the data itself (only changes transient members) and it was causing problems when locking lazy chunked(disk mapped) values backed by files on nfs/hdfs/s3. The lazy chunking would not work after locking.

--- src/main/java/water/Lockable.java
+++ src/main/java/water/Lockable.java
@@ -68,8 +68,17 @@ public abstract class Lockable<T extends Lockable<T>> extends Iced {
     return (T)this;
   }
 
+  private abstract static class Lock extends TAtomic<Lockable> {
+    @Override public Value atomic(Value val) {
+      Lockable old = val == null ? null : (Lockable)(val.get().clone());
+      Lockable nnn = atomic(old);
+      if(nnn == null)return null;
+      return  val == null ? new Value(_key,nnn):new Value(_key,nnn,val._persist);
+    }
+  }
+
   // Obtain the write-lock on _key, which may already exist, using the current 'this'.
-  private class PriorWriteLock extends TAtomic<Lockable> {
+  private class PriorWriteLock extends Lock {
     final Key _job_key;         // Job doing the locking
     Lockable _old;              // Return the old thing, for deleting later
     PriorWriteLock( Key job_key ) { _job_key = job_key; }
@@ -131,7 +140,7 @@ public abstract class Lockable<T extends Lockable<T>> extends Iced {
   }
 
   // Obtain read-lock
-  static private class ReadLock extends TAtomic<Lockable> {
+  static private class ReadLock extends Lock {
     final Key _job_key;         // Job doing the unlocking
     ReadLock( Key job_key ) { _job_key = job_key; }
     @Override public Lockable atomic(Lockable old) {
