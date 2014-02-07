73244ba7ca6ac9dac028389a53a31ad0ed55b1f6
Arno Candel <arno.candel@gmail.com>
2014-02-06 18:32:33 -0800

    Update Mnist test case for latest changes in NN.

--- h2o-samples/src/main/java/samples/NeuralNetMnist2.java
+++ h2o-samples/src/main/java/samples/NeuralNetMnist2.java
@@ -4,7 +4,6 @@ import hex.nn.NN;
 import water.Job;
 import water.Key;
 import water.TestUtil;
-import water.api.FrameSplit;
 import water.fvec.Frame;
 import water.util.Log;
 
@@ -14,13 +13,13 @@ import water.util.Log;
 public class NeuralNetMnist2 extends Job {
   public static void main(String[] args) throws Exception {
     Class job = NeuralNetMnist2.class;
-    samples.launchers.CloudLocal.launch(job, 1);
+//    samples.launchers.CloudLocal.launch(job, 1);
 //    samples.launchers.CloudProcess.launch(job, 4);
     //samples.launchers.CloudConnect.launch(job, "localhost:54321");
 //    samples.launchers.CloudRemote.launchIPs(job, "192.168.1.171", "192.168.1.172", "192.168.1.173", "192.168.1.174", "192.168.1.175");
 //    samples.launchers.CloudRemote.launchIPs(job, "192.168.1.161", "192.168.1.162", "192.168.1.163", "192.168.1.164");
 //    samples.launchers.CloudRemote.launchIPs(job, "192.168.1.161", "192.168.1.162");
-//    samples.launchers.CloudRemote.launchIPs(job, "192.168.1.161");
+    samples.launchers.CloudRemote.launchIPs(job, "192.168.1.161");
 //    samples.launchers.CloudRemote.launchEC2(job, 4);
   }
 
@@ -28,19 +27,13 @@ public class NeuralNetMnist2 extends Job {
     Log.info("Parsing data.");
     long seed = 0xC0FFEE;
     double fraction = 1.0;
-    Frame trainf = TestUtil.parseFromH2OFolder("smalldata/mnist/train.csv");
-    Frame testf = TestUtil.parseFromH2OFolder("smalldata/mnist/test.csv");
-    if (fraction < 1) {
-      System.out.println("Sampling " + fraction*100 + "% of data with random seed: " + seed + ".");
-      FrameSplit split = new FrameSplit();
-      final double[] ratios = {fraction, 1-fraction};
-      trainf = split.splitFrame(trainf, ratios, seed)[0];
-//      testf = split.splitFrame(testf, ratios, seed)[0];
-    }
+//    Frame trainf = NN.sampleFrame(TestUtil.parseFromH2OFolder("smalldata/mnist/train.csv"), (long)(60000*fraction), seed);
+    Frame trainf = NN.sampleFrame(TestUtil.parseFromH2OFolder("smalldata/mnist/train10x.csv"), (long)(600000*fraction), seed);
+    Frame testf = NN.sampleFrame(TestUtil.parseFromH2OFolder("smalldata/mnist/test.csv"), (long)(10000*fraction), seed+1);
     Log.info("Done.");
 
     NN p = new NN();
-    // Hinton parameters -> should lead to ~1 % test error after ~ 10M training points
+    // Hinton parameters -> should lead to ~1 % test error after a few dozen million samples
     p.seed = seed;
     p.hidden = new int[]{1024,1024,2048};
 //    p.hidden = new int[]{128,128,256};
@@ -59,14 +52,16 @@ public class NeuralNetMnist2 extends Job {
     p.initial_weight_distribution = NN.InitialWeightDistribution.UniformAdaptive;
 //    p.initial_weight_scale = 0.01
     p.classification = true;
-    p.diagnostics = true;
+    p.diagnostics = false;
     p.expert_mode = true;
-    p.score_training = 1000;
-    p.score_validation = 10000;
+    p.score_training_samples = 10000;
+    p.score_validation_samples = 10000;
     p.validation = testf;
     p.source = trainf;
     p.response = trainf.lastVec();
     p.ignored_cols = null;
+    p.sync_samples = 50000;
+    p.score_interval = 30;
     p.destination_key = Key.make("mnist.model");
     return p.exec();
   }
