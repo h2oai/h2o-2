63d40aacf465d1fda0f3075ca5eddc8e91a79691
Cliff Click <cliffc@acm.org>
2014-02-07 20:07:26 -0800

    Pass1 of ddply: find unique groups

--- src/main/java/water/exec/ASTRApply.java
+++ src/main/java/water/exec/ASTRApply.java
@@ -5,6 +5,7 @@ import java.util.*;
 import water.*;
 import water.fvec.*;
 import water.util.Utils;
+import water.nbhm.NonBlockingHashMap;
 
 /** Parse a generic R string and build an AST, in the context of an H2O Cloud
  *  @author cliffc@0xdata.com
@@ -154,28 +155,32 @@ class ASTddply extends ASTOp {
 
     // Either a single column, or a collection of columns to group on.
     int cols[];
-    if( !env.isAry(-2) )        // Single column?
-      cols = new int[]{(int)env.dbl(-2)};
-    else {                      // Else a collection of columns?
+    if( !env.isAry(-2) ) {      // Single column?
+      if( Double.isNaN(env.dbl(-2)) ) throw new IllegalArgumentException("NA not a valid column");
+      cols = new int[]{(int)env.dbl(-2)-1};
+    } else {                    // Else a collection of columns?
       Frame cs = env.ary(-2);
-      if( cs.numCols() != 1 ) throw new IllegalArgumentException("Only one column-of-columns for column selection");
+      if( cs.numCols() !=  1  ) throw new IllegalArgumentException("Only one column-of-columns for column selection");
       if( cs.numRows() > 1000 ) throw new IllegalArgumentException("Too many columns selected");
       cols = new int[(int)cs.numRows()];
       Vec vec = cs.vecs()[0];
       for( int i=0; i<cols.length; i++ ) 
         if( vec.isNA(i) ) throw new IllegalArgumentException("NA not a valid column");
-        else cols[i] = (int)vec.at8(i);
+        else cols[i] = (int)vec.at8(i)-1;
     }
     // Another check for sane columns
-    for( int c : cols ) 
-      if( c < 1 || c > fr.numCols() )
-        throw new IllegalArgumentException("Column "+c+" out of range for frame columns "+fr.numCols());
+    for( int c : cols )
+      if( c < 0 || c >= fr.numCols() )
+        throw new IllegalArgumentException("Column "+(c+1)+" out of range for frame columns "+fr.numCols());
 
     // Random implementation hacks/notes...
-    // - ddply inner fcn is side-effect-free.
-    // - ddply inner fcn runs once per group (apparently).  
-    // - ddply inner fcn can make multple refs to the whole group-frame, requiring (apparently) multiple passes
-    // 
+
+    // Was pondering a SIMD-like execution model, running the fcn "once" - but
+    // in parallel for all groups.  But this isn't going to work: each fcn
+    // execution will take different control paths.  Also the functions side-
+    // effects' must only happen once, and they will make multiple passes over
+    // the Frame passed in.  
+    //
     // GroupIDs' can vary from 1 group to 1-per-row.  Are formed by the cross-
     // product of the selection cols.  Will be hashed to find Group - NBHML
     // mapping row-contents to group.  Index is a sample row.  NBHML per-node,
@@ -191,10 +196,82 @@ class ASTddply extends ASTOp {
     // bogus Frame, with magical Vecs that use the Group(s) to define their
     // rows.  All the usual Chunk.at0 or Vec.at calls work, except the Chunk
     // does a row-indirection to find the actual Chunk data.
-
+    
+    // Pass-1: Find Groups.
+    // Build a NBHML mapping 1st row-# in group to double[] holding selection cols.
+    ddplyPass1 p1 = new ddplyPass1(cols).doAll(fr);
+    System.out.print(p1.toString());
     
     env.pop(4);
     // Push empty frame for debuggging
     env.push(new Frame(new String[0],new Vec[0]));
   }
+
+  // ---
+  // Pass1: Find unique groups, based on a subset of columns
+  private static class ddplyPass1 extends MRTask2<ddplyPass1> {
+    public int _cols[];         // Selection columns
+    public transient NonBlockingHashMap<Entry,Object> _uniques;
+    public static final Object V = "";
+    // A stupid class to give double[] sane equals & hashCode
+    private static class Entry extends Iced {
+      public double _ds[];
+      Entry( int len ) { _ds = new double[len]; }
+      @Override public boolean equals( Object o ) { 
+        return o instanceof Entry && Arrays.equals(_ds,((Entry)o)._ds); }
+      @Override public int hashCode() {
+        int sum=0;
+        for( double d : _ds ) sum += Double.doubleToRawLongBits(d);
+        return sum;
+      }
+      @Override public String toString() { return Arrays.toString(_ds); }
+    }
+    ddplyPass1( int cols[] ) { _cols = cols; }
+    @Override public void setupLocal() {
+      _uniques = new NonBlockingHashMap<Entry,Object>();
+    }
+    @Override public void map( Chunk chks[] ) {
+      Entry e = new Entry(_cols.length);
+      int len = chks[_cols[0]]._len;
+      for( int row=0; row<len; row++ ) {
+        // Temp array holding the column-selection data
+        for( int c=0; c<_cols.length; c++ )   // For all selection cols
+          e._ds[c] = chks[_cols[c]].at0(row); // Load into working array
+        if( _uniques.putIfAbsent(e,V)==null ) // Add group signature if not already present
+          e = new Entry(_cols.length);        // Was added; so 'e' in hashset; need a new 'e'
+      }
+    }
+    @Override public void reduce( ddplyPass1 p1 ) {
+      if( _uniques == p1._uniques ) return; // Trivally true
+      _uniques.putAll(p1._uniques);         // Smash hashmaps together
+    }
+    @Override public String toString() { return _uniques.toString(); }
+    // Custom serialization for NBHM.  Much nicer when these are auto-gen'd.
+    @Override public AutoBuffer write( AutoBuffer ab ) {
+      super.write(ab);
+      ab.putA4(_cols);
+      if( _uniques == null ) return ab.put4(0);
+      ab.put4(_uniques.size());
+      for( Entry e : _uniques.keySet() ) ab.put(e);
+      return ab;
+    }
+    
+    @Override public ddplyPass1 read( AutoBuffer ab ) {
+      super.read(ab);
+      assert _uniques == null;
+      _uniques = new NonBlockingHashMap<Entry,Object>();
+      _cols = ab.getA4();
+      int len = ab.get4();
+      for( int i=0; i<len; i++ )
+        _uniques.put((Entry)ab.get(),V);
+      return this;
+    }
+    public void copyOver(DTask that) {
+      ddplyPass1 p1 = (ddplyPass1)that;
+      super.copyOver(p1);
+      p1._cols = _cols;
+      p1._uniques = _uniques;
+    }
+  }
+
 }
