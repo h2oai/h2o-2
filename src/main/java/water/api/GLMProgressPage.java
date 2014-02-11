fa87f8c4d07a9dcb268a11f20a1fa4cfd85fe322
Spencer Aiello <saiello@dhcp-office-15.tagged.com>
2014-02-10 18:31:09 -0800

    add null check before ROCplot function call should not be after (loads axes but no plot other wise...)

--- src/main/java/water/api/GLMProgressPage.java
+++ src/main/java/water/api/GLMProgressPage.java
@@ -294,7 +294,8 @@ public class GLMProgressPage extends Request {
         R.replace("CM",R2);
       }
       sb.append(R);
-      ROCplot(val, sb);
+      if (val._cm != null && val._fprs != null && val._tprs != null)
+        ROCplot(val, sb);
       confusionHTML(val.bestCM(),sb);
       if(val.fold() > 1){
         int nclasses = 2;
@@ -400,16 +401,14 @@ public class GLMProgressPage extends Request {
           );
           sb.append("var dataset = [");
 
-          if(xval != null && xval._cm != null && xval._fprs != null && xval._tprs != null) {
-            for(int c = 0; c < xval._cm.length; c++) {
-                if (c == 0) {
-                    sb.append("["+String.valueOf(xval._fprs[c])+",").append(String.valueOf(xval._tprs[c])).append("]");
-                }
-                sb.append(", ["+String.valueOf(xval._fprs[c])+",").append(String.valueOf(xval._tprs[c])).append("]");
+          for(int c = 0; c < xval._cm.length; c++) {
+            if (c == 0) {
+              sb.append("["+String.valueOf(xval._fprs[c])+",").append(String.valueOf(xval._tprs[c])).append("]");
             }
-            for(int c = 0; c < 2*xval._cm.length; c++) {
-                sb.append(", ["+String.valueOf(c/(2.0*xval._cm.length))+",").append(String.valueOf(c/(2.0*xval._cm.length))).append("]");
-            }
+            sb.append(", ["+String.valueOf(xval._fprs[c])+",").append(String.valueOf(xval._tprs[c])).append("]");
+          }
+          for(int c = 0; c < 2*xval._cm.length; c++) {
+            sb.append(", ["+String.valueOf(c/(2.0*xval._cm.length))+",").append(String.valueOf(c/(2.0*xval._cm.length))).append("]");
           }
           sb.append("];\n");
 
