7981e6a6bd98096f36ec6e8d170fd344c6691871
Kevin Normoyle <kevin@0xdata.com>
2014-02-12 22:19:40 -0800

    for cleanup

---
+++ py/testdir_release/kill_0xdiag_0xcustomer_everywhere.sh
@@ -0,0 +1,38 @@
+
+# the 0xdiag ones may be shared with other people doing python tests, so this will whack them
+
+#****************************************************************************
+# got the private id from 180
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.161 pkill -u 0xcustomer
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.162 pkill -u 0xcustomer
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.163 pkill -u 0xcustomer
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.164 pkill -u 0xcustomer
+
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.171 pkill -u 0xcustomer
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.172 pkill -u 0xcustomer
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.173 pkill -u 0xcustomer
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.174 pkill -u 0xcustomer
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.175 pkill -u 0xcustomer
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.176 pkill -u 0xcustomer
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.177 pkill -u 0xcustomer
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.178 pkill -u 0xcustomer
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.179 pkill -u 0xcustomer
+ssh -i ~/.0xcustomer/0xcustomer_id_rsa 0xcustomer@192.168.1.180 pkill -u 0xcustomer
+
+#****************************************************************************
+# got the private id from 180
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.161 pkill -u 0xdiag
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.162 pkill -u 0xdiag
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.163 pkill -u 0xdiag
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.164 pkill -u 0xdiag
+
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.171 pkill -u 0xdiag
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.172 pkill -u 0xdiag
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.173 pkill -u 0xdiag
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.174 pkill -u 0xdiag
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.175 pkill -u 0xdiag
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.176 pkill -u 0xdiag
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.177 pkill -u 0xdiag
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.178 pkill -u 0xdiag
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.179 pkill -u 0xdiag
+ssh -i ~/.0xdiag/0xdiag_id_rsa 0xdiag@192.168.1.180 pkill -u 0xdiag
