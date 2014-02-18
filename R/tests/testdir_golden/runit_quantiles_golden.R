67fb37461664dfc117d00dd3a476ca43a0fc231e
Irene L <irene@0xdata.com>
2014-02-18 12:45:42 -0800

    summary tests

--- R/tests/testdir_golden/runit_quantiles_golden.R
+++ R/tests/testdir_golden/runit_quantiles_golden.R
@@ -41,34 +41,47 @@ makeH9<- as.data.frame(quantile(makeH2O[,9]))
 
 Log.info("Print summary for H2O and R... \n")
 
-Log.info(paste("H2O Make 2  : ", makeH2,      "\t\t", "R Make 2   : ", makeR2))
-Log.info(paste("H2O Make 2  : ", makeH2,      "\t\t", "R Make 2   : ", makeR2))
-Log.info(paste("H2O Make 3  : ", makeH3,      "\t\t", "R Make 3   : ", makeR3))
-Log.info(paste("H2O Make 4  : ", makeH2,      "\t\t", "R Make 4   : ", makeR4))
-Log.info(paste("H2O Make 5  : ", makeH2,      "\t\t", "R Make 5   : ", makeR5))
-Log.info(paste("H2O Make 6  : ", makeH2,      "\t\t", "R Make 6   : ", makeR2))
-Log.info(paste("H2O Make 7  : ", makeH2,      "\t\t", "R Make 7   : ", makeR2))
-Log.info(paste("H2O Make 8  : ", makeH2,      "\t\t", "R Make 8   : ", makeR2))
-Log.info(paste("H2O Make 9  : ", makeH2,      "\t\t", "R Make 9   : ", makeR2))
-Log.info(paste("H2O Runif 2  : ", qrH2,      "\t\t", "R Runif 2   : ", qrR2))
-Log.info(paste("H2O Runif 3  : ", qrH3,      "\t\t", "R Runif 3   : ", qrR3))
-Log.info(paste("H2O Runif 4  : ", qrH2,      "\t\t", "R Runif 4   : ", qrR4))
+Log.info(paste("H2O Make 2  : ", makeH2))      
+Log.info(paste("R Make 2  : ", makeR2))
+Log.info(paste("H2O Make 3  : ", makeH3))      
+Log.info(paste("R Make 3  : ", makeR3))
+Log.info(paste("H2O Make 4  : ", makeH4))      
+Log.info(paste("R Make 4  : ", makeR4))
+Log.info(paste("H2O Make 5  : ", makeH5))      
+Log.info(paste("R Make 5  : ", makeR5))
+Log.info(paste("H2O Make 6  : ", makeH6))      
+Log.info(paste("R Make 6  : ", makeR6))
+Log.info(paste("H2O Make 7  : ", makeH7))      
+Log.info(paste("R Make 7  : ", makeR7))
+Log.info(paste("H2O Make 8  : ", makeH8))      
+Log.info(paste("R Make 8  : ", makeR8))
+Log.info(paste("H2O Make 9  : ", makeH9))      
+Log.info(paste("R Make 9  : ", makeR9))
+Log.info(paste("H2O Runif 2  : ", qrH2))      
+Log.info(paste("R Runif 2  : ", qrR2))
+Log.info(paste("H2O Runif 3  : ", qrH3))      
+Log.info(paste("R Runif 3  : ", qrR3))
+Log.info(paste("H2O Runif 4  : ", qrH4))      
+Log.info(paste("R Runif 4  : ", qrR4))
 
 
-
-
-Log.info("Compare H2O summary to R summary... \n")
-expect_equal(makeH2[,1], makeR2[,1], tolerance=.01)
-#expect_equal(sumBH2O[,2], sumBR[,2], tolerance=.01)
-#expect_equal(sumCH2O[,2], sumCR[,2], tolerance=.01)
-#expect_equal(sumrunifH2O[,2], sumrunifR[,2], tolerance=.01)
-#expect_equal(sumrunifH2O[,3], sumrunifR[,3], tolerance=.01)
-#expect_equal(sumrunifH2O[,4], sumrunifR[,4], tolerance=.01)
+Log.info("Compare H2O quantiles to R quantiles... \n")
+expect_equal(makeH2[,1], makeR2[,1], tolerance=.03)
+expect_equal(makeH3[,1], makeR3[,1], tolerance=.03)
+expect_equal(makeH4[,1], makeR4[,1], tolerance=.03)
+expect_equal(makeH5[,1], makeR5[,1], tolerance=.03)
+expect_equal(makeH6[,1], makeR6[,1], tolerance=.03)
+expect_equal(makeH7[,1], makeR7[,1], tolerance=.03)
+expect_equal(makeH8[,1], makeR8[,1], tolerance=.03)
+expect_equal(makeH9[,1], makeR9[,1], tolerance=.03)
+expect_equal(qrH2[,1], qrR2[,1], tolerance=.03)
+expect_equal(qrH3[,1], qrR3[,1], tolerance=.03)
+expect_equal(qrH4[,1], qrR4[,1], tolerance=.03)
 
 testEnd()
 }
 
-doTest("Summary on Random Uniform Dist", test.quantiles.golden)
+doTest("Quantiles on Known Distributions", test.quantiles.golden)
 
 #NOTES ON MAKEDATA 
 #A: normal, mean: -100, sd = 50
@@ -79,3 +92,4 @@ doTest("Summary on Random Uniform Dist", test.quantiles.golden)
 #F: binom, size=100, prob=.02
 #G: binom, size=10, prob=.01
 #H: exponential: rate= .4
+#all cols in runif are random uniform distributions with different range definitions
