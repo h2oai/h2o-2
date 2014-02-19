a579b5bc681e2a43385e6d0cb0229a69ba090f7e
Arno Candel <arno.candel@gmail.com>
2014-02-18 20:35:18 -0800

    Update naming for NeuralNet2 Progress.

--- src/main/java/water/api/NNProgressPage.java
+++ src/main/java/water/api/NNProgressPage.java
@@ -19,7 +19,7 @@ public class NNProgressPage extends Progress2 {
     Job jjob = Job.findJob(job_key);
     if (jjob ==null) return true;
     NNModel m = UKV.get(jjob.dest());
-    if (m!=null) m.generateHTML("NeuralNet Model", sb);
+    if (m!=null) m.generateHTML("NeuralNet2 Model", sb);
     else DocGen.HTML.paragraph(sb, "Pending...");
     return true;
   }
