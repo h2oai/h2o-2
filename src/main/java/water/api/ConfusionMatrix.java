ab00b8a058c030c10479252b25370f201cfb37d5
Arno Candel <arno.candel@gmail.com>
2014-02-19 17:04:10 -0800

    Add support for regression scoring via MSE to the ConfusionMatrix class (for API simplicity).

--- src/main/java/water/api/ConfusionMatrix.java
+++ src/main/java/water/api/ConfusionMatrix.java
@@ -60,35 +60,48 @@ public class ConfusionMatrix extends Request2 {
   @API(help="Confusion Matrix (or co-occurrence matrix)")
   public long cm[][];
 
+  @API(help="Mean Squared Error")
+  public double mse = Double.NaN;
+
+  private boolean classification;
+
   @Override public Response serve() {
     Vec va = null,vp = null, avp = null;
+    classification = vactual.isInt() && vpredict.isInt();
+    final boolean regression = !vactual.isInt() && !vpredict.isInt();
     // Input handling
     if( vactual==null || vpredict==null )
       throw new IllegalArgumentException("Missing actual or predict!");
-    if ( !vactual.isInt() || !vpredict.isInt())
-      throw new IllegalArgumentException("Cannot provide confusion matrix for float vectors!");
-    if ( vactual.length() != vpredict.length())
+    if (!classification && !regression)
+      throw new IllegalArgumentException("Both arguments must either be floating point or integer.");
+    if (vactual.length() != vpredict.length())
       throw new IllegalArgumentException("Both arguments must have the same length!");
 
     try {
-      // Create a new vectors - it is cheap since vector are only adaptation vectors
-      va = vactual .toEnum(); // always returns TransfVec
-      actual_domain = va._domain;
-      vp = vpredict.toEnum(); // always returns TransfVec
-      predicted_domain = vp._domain;
-      if (!Arrays.equals(actual_domain, predicted_domain)) {
-        domain = Utils.union(actual_domain, predicted_domain);
-        int[][] vamap = Model.getDomainMapping(domain, actual_domain, true);
-        va = TransfVec.compose( (TransfVec) va, vamap, domain, false ); // delete original va
-        int[][] vpmap = Model.getDomainMapping(domain, predicted_domain, true);
-        vp = TransfVec.compose( (TransfVec) vp, vpmap, domain, false ); // delete original vp
-      } else domain = actual_domain;
-      // The vectors are from different groups => align them, but properly delete it after computation
-      if (!va.group().equals(vp.group())) {
-        avp = vp;
-        vp = va.align(vp);
+      if (classification) {
+        // Create a new vectors - it is cheap since vector are only adaptation vectors
+        va = vactual .toEnum(); // always returns TransfVec
+        actual_domain = va._domain;
+        vp = vpredict.toEnum(); // always returns TransfVec
+        predicted_domain = vp._domain;
+        if (!Arrays.equals(actual_domain, predicted_domain)) {
+          domain = Utils.union(actual_domain, predicted_domain);
+          int[][] vamap = Model.getDomainMapping(domain, actual_domain, true);
+          va = TransfVec.compose( (TransfVec) va, vamap, domain, false ); // delete original va
+          int[][] vpmap = Model.getDomainMapping(domain, predicted_domain, true);
+          vp = TransfVec.compose( (TransfVec) vp, vpmap, domain, false ); // delete original vp
+        } else domain = actual_domain;
+        // The vectors are from different groups => align them, but properly delete it after computation
+        if (!va.group().equals(vp.group())) {
+          avp = vp;
+          vp = va.align(vp);
+        }
+        cm = new CM(domain.length).doAll(va,vp)._cm;
+      } else {
+        assert(!vactual.isEnum());
+        assert(!vpredict.isEnum());
+        mse = new CM(1).doAll(vactual,vpredict).mse();
       }
-      cm = new CM(domain.length).doAll(va,vp)._cm;
       return Response.done(this);
     } catch( Throwable t ) {
       return Response.error(t);
@@ -102,24 +115,53 @@ public class ConfusionMatrix extends Request2 {
   // Compute the co-occurrence matrix
   private static class CM extends MRTask2<CM> {
     /* @IN */ final int _c_len;
-    /* @OUT */ long _cm[][];
+    /* @OUT Classification */ long _cm[][];
+    /* @OUT Regression */ public double mse() { return _count > 0 ? _mse/_count : Double.POSITIVE_INFINITY; }
+    /* @OUT Regression Helper */ private double _mse = Double.NaN;
+    /* @OUT Regression Helper */ private long _count;
     CM(int c_len) { _c_len = c_len;  }
     @Override public void map( Chunk ca, Chunk cp ) {
-      _cm = new long[_c_len+1][_c_len+1];
-      int len = Math.min(ca._len,cp._len); // handle different lenghts, but the vectors should have been rejected already
-      for( int i=0; i < len; i++ ) {
-        int a=ca.isNA0(i) ? _c_len : (int)ca.at80(i);
-        int p=cp.isNA0(i) ? _c_len : (int)cp.at80(i);
-        _cm[a][p]++;
+      //classification
+      if (_c_len > 1) {
+        _cm = new long[_c_len+1][_c_len+1];
+        int len = Math.min(ca._len,cp._len); // handle different lenghts, but the vectors should have been rejected already
+        for( int i=0; i < len; i++ ) {
+          int a=ca.isNA0(i) ? _c_len : (int)ca.at80(i);
+          int p=cp.isNA0(i) ? _c_len : (int)cp.at80(i);
+          _cm[a][p]++;
+        }
+        if( len < ca._len )
+          for( int i=len; i < ca._len; i++ )
+            _cm[ca.isNA0(i) ? _c_len : (int)ca.at80(i)][_c_len]++;
+        if( len < cp._len )
+          for( int i=len; i < cp._len; i++ )
+            _cm[_c_len][cp.isNA0(i) ? _c_len : (int)cp.at80(i)]++;
+      } else {
+        _cm = null;
+        _mse = 0;
+        assert(ca._len == cp._len);
+        int len = ca._len;
+        for( int i=0; i < len; i++ ) {
+          if (ca.isNA0(i) || cp.isNA0(i)) continue; //TODO: Improve
+          final float a=ca.at80(i);
+          final float p=cp.at80(i);
+          _mse += (p-a)*(p-a);
+          _count++;
+        }
       }
-      if( len < ca._len )
-        for( int i=len; i < ca._len; i++ )
-          _cm[ca.isNA0(i) ? _c_len : (int)ca.at80(i)][_c_len]++;
-      if( len < cp._len )
-        for( int i=len; i < cp._len; i++ )
-          _cm[_c_len][cp.isNA0(i) ? _c_len : (int)cp.at80(i)]++;
     }
-    @Override public void reduce( CM cm ) { Utils.add(_cm,cm._cm); }
+
+    @Override public void reduce( CM cm ) {
+      if (_cm != null && cm._cm != null) {
+        assert(_mse == Double.NaN && cm._mse == Double.NaN);
+        Utils.add(_cm,cm._cm);
+      } else {
+        assert(_mse != Double.NaN && cm._mse != Double.NaN);
+        assert(_cm == null && cm._cm == null);
+        _mse += cm._mse;
+        _count += cm._count;
+      }
+    }
   }
 
   public static String[] show( long xs[], String ds[] ) {
@@ -133,66 +175,82 @@ public class ConfusionMatrix extends Request2 {
   }
 
   @Override public boolean toHTML( StringBuilder sb ) {
-    DocGen.HTML.title(sb,"Confusion Matrix");
-    if( cm == null ) return true;
-    // Sum up predicted & actuals
-    long acts [] = new long[cm   .length];
-    long preds[] = new long[cm[0].length];
-    for( int a=0; a<cm.length; a++ ) {
-      long sum=0;
-      for( int p=0; p<cm[a].length; p++ ) { sum += cm[a][p]; preds[p] += cm[a][p]; }
-      acts[a] = sum;
+    if (classification) {
+      DocGen.HTML.title(sb,"Confusion Matrix");
+      if( cm == null ) return true;
     }
-
-    String adomain[] = show(acts , domain);
-    String pdomain[] = show(preds, domain);
-    assert adomain.length == pdomain.length : "The confusion matrix should have the same length for both directions.";
+    else {
+      DocGen.HTML.title(sb,"Mean Squared Error");
+      if( mse == Double.NaN ) return true;
+    }
 
     DocGen.HTML.arrayHead(sb);
-    // Top row of CM
-    sb.append("<tr class='warning'>");
-    sb.append("<th>Actual / Predicted</th>"); // Row header
-    for( int p=0; p<pdomain.length; p++ )
-      if( pdomain[p] != null )
-        sb.append("<th>").append(pdomain[p]).append("</th>");
-    sb.append("<th>Error</th>");
-    sb.append("</tr>");
+    if (classification) {
+      // Sum up predicted & actuals
+      long acts [] = new long[cm   .length];
+      long preds[] = new long[cm[0].length];
+      for( int a=0; a<cm.length; a++ ) {
+        long sum=0;
+        for( int p=0; p<cm[a].length; p++ ) { sum += cm[a][p]; preds[p] += cm[a][p]; }
+        acts[a] = sum;
+      }
 
-    // Main CM Body
-    long terr=0;
-    for( int a=0; a<cm.length; a++ ) { // Actual loop
-      if( adomain[a] == null ) continue;
+      String adomain[] = show(acts , domain);
+      String pdomain[] = show(preds, domain);
+      assert adomain.length == pdomain.length : "The confusion matrix should have the same length for both directions.";
+
+      // Top row of CM
+      sb.append("<tr class='warning'>");
+      sb.append("<th>Actual / Predicted</th>"); // Row header
+      for( int p=0; p<pdomain.length; p++ )
+        if( pdomain[p] != null )
+          sb.append("<th>").append(pdomain[p]).append("</th>");
+      sb.append("<th>Error</th>");
+      sb.append("</tr>");
+
+      // Main CM Body
+      long terr=0;
+      for( int a=0; a<cm.length; a++ ) { // Actual loop
+        if( adomain[a] == null ) continue;
+        sb.append("<tr>");
+        sb.append("<th>").append(adomain[a]).append("</th>");// Row header
+        long correct=0;
+        for( int p=0; p<pdomain.length; p++ ) { // Predicted loop
+          if( pdomain[p] == null ) continue;
+          boolean onDiag = adomain[a].equals(pdomain[p]);
+          if( onDiag ) correct = cm[a][p];
+          sb.append(onDiag ? "<td style='background-color:LightGreen'>":"<td>").append(cm[a][p]).append("</td>");
+        }
+        long err = acts[a]-correct;
+        terr += err;              // Bump totals
+        sb.append(String.format("<th>%5.3f = %d / %d</th>", (double)err/acts[a], err, acts[a]));
+        sb.append("</tr>");
+      }
+
+      // Last row of CM
       sb.append("<tr>");
-      sb.append("<th>").append(adomain[a]).append("</th>");// Row header
-      long correct=0;
+      sb.append("<th>Totals</th>");// Row header
       for( int p=0; p<pdomain.length; p++ ) { // Predicted loop
         if( pdomain[p] == null ) continue;
-        boolean onDiag = adomain[a].equals(pdomain[p]);
-        if( onDiag ) correct = cm[a][p];
-        sb.append(onDiag ? "<td style='background-color:LightGreen'>":"<td>").append(cm[a][p]).append("</td>");
+        sb.append("<td>").append(preds[p]).append("</td>");
       }
-      long err = acts[a]-correct;
-      terr += err;              // Bump totals
-      sb.append(String.format("<th>%5.3f = %d / %d</th>", (double)err/acts[a], err, acts[a]));
+      sb.append(String.format("<th>%5.3f = %d / %d</th>", (double)terr/vactual.length(), terr, vactual.length()));
       sb.append("</tr>");
+    } else{
+      // Regression
+      sb.append("<tr class='warning'><td>" + mse + "</td></tr>"); // Row header
     }
-
-    // Last row of CM
-    sb.append("<tr>");
-    sb.append("<th>Totals</th>");// Row header
-    for( int p=0; p<pdomain.length; p++ ) { // Predicted loop
-      if( pdomain[p] == null ) continue;
-      sb.append("<td>").append(preds[p]).append("</td>");
-    }
-    sb.append(String.format("<th>%5.3f = %d / %d</th>", (double)terr/vactual.length(), terr, vactual.length()));
-    sb.append("</tr>");
-
     DocGen.HTML.arrayTail(sb);
     return true;
   }
 
   public double toASCII( StringBuilder sb ) {
-    if( cm == null ) return 1.0;
+    if( cm == null && classification) return 1.0;
+    if( !classification) {
+      sb.append("MSE: " + mse);
+      return mse;
+    }
+
     // Sum up predicted & actuals
     long acts [] = new long[cm   .length];
     long preds[] = new long[cm[0].length];
