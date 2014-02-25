dff5a4bc1021f033991e2abd20d167c100b66816
Arno Candel <arno.candel@gmail.com>
2014-02-24 18:08:13 -0800

    Rename NeuralNet to NN.

--- src/main/java/water/api/NNModelView.java
+++ src/main/java/water/api/NNModelView.java
@@ -9,11 +9,11 @@ public class NNModelView extends Request2 {
   static final int API_WEAVER = 1; // This file has auto-gen'd doc & json fields
   static public DocGen.FieldDoc[] DOC_FIELDS; // Initialized from Auto-Gen code.
 
-  @API(help="NeuralNet Model Key", required=true, filter=NeuralNetModelKeyFilter.class)
+  @API(help="NN Model Key", required=true, filter=NeuralNetModelKeyFilter.class)
   Key _modelKey;
   class NeuralNetModelKeyFilter extends H2OKey { public NeuralNetModelKeyFilter() { super("model_key",true); } }
 
-  @API(help="NeuralNet Model")
+  @API(help="NN Model")
   NNModel neuralnet_model;
 
   public static String link(String txt, Key model) {
@@ -26,7 +26,7 @@ public class NNModelView extends Request2 {
 
   @Override public boolean toHTML(StringBuilder sb){
     if (neuralnet_model != null)
-      neuralnet_model.generateHTML("NeuralNet Model", sb);
+      neuralnet_model.generateHTML("NN Model", sb);
     return true;
   }
 
