e796b549797866712eef47ba2e932473e1a57aeb
SpencerA <spnrpa@gmail.com>
2014-02-27 16:13:29 -0800

    Modifications to tests to fit with new scheme (and to work on 0x1)

--- bench2/tests/glmVA_prostate/glmVA_prostate_Parse.R
+++ bench2/tests/glmVA_prostate/glmVA_prostate_Parse.R
@@ -2,9 +2,9 @@ source("../../R/h2oPerf/prologue.R")
 data_source <<- "smalldata"
 trainData   <<- "~/master/h2o/smalldata/logreg/prostate.csv"
 response <<- "CAPSULE"
-upload.VA("parsed.hex")
+upload.VA("parsed.hex", trainData)
 
 testData    <<- trainData
-upload.VA("test.hex")
+upload.VA("test.hex", testData)
 
 source("../../R/h2oPerf/epilogue.R")
--- bench2/tests/rfVA_mnist/rfVA_mnist_Parse.R
+++ bench2/tests/rfVA_mnist/rfVA_mnist_Parse.R
@@ -3,9 +3,9 @@ source("../../R/h2oPerf/prologue.R")
 data_source <<- "smalldata"
 trainData   <<- "~/master/h2o/smalldata/mnist/train.csv.gz"
 response <<- "C785"
-upload.VA("parsed.hex")
+upload.VA("parsed.hex", trainData)
 
 testData    <<- "~/master/h2o/smalldata/mnist/test.csv.gz"
-upload.VA("test.hex")
+upload.VA("test.hex", testData)
 
 source("../../R/h2oPerf/epilogue.R")
