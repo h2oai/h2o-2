1165da6baaf14227cdd8747eda3a2f75839917d7
anqi <anqi@0xdata.com>
2014-03-04 11:48:55 -0800

    Fix for PUB-443. Only need 'java' command in non-Windows OS.

--- R/h2o-package/R/Wrapper.R
+++ R/h2o-package/R/Wrapper.R
@@ -171,7 +171,10 @@ h2o.clusterStatus <- function(client) {
 
 .h2o.startJar <- function(memory = "1g", beta=F) {
   command <- .h2o.checkJava()
-  command <- paste('"', command, '"', sep = "")
+  if(.Platform$OS.type == "windows")
+    command <- paste('"', command, '"', sep = "")
+  else
+    command <- "java"
   
   #
   # TODO: tmp files should be user-independent
