176862193421c72ae4e233dc7fdbeea4dc8a078b
earlh <github@earlh.com>
2014-03-04 12:52:46 -0800

    Revert "Sorting of rows done according to first column of returned data frame."
    This reverts commit be6123bbec7be9a2dcd3c42ed557266616556ff9.
    
    overlapping work

--- R/tests/testdir_jira/runit_NOPASS_pub_180_ddply.R
+++ R/tests/testdir_jira/runit_NOPASS_pub_180_ddply.R
@@ -41,12 +41,12 @@ ddplytest <- function(conn){
   df.3 <- as.data.frame( df.h.3 )
   df.4 <- as.data.frame( df.h.4 )
   
-  Log.info('sorting (in ascending order) data by first column for comparisons')
-  df.1 <- df.1[order(df.1[,1]),]
-  df.2 <- df.2[order(df.2[,1]),]
-  df.3 <- df.3[order(df.3[,1]),]
-  df.4 <- df.4[order(df.4[,1]),]
-  
+  Log.info('sorting each column in ascending order for comparisons')
+  df.1[,1] <- sort(df.1[,1]); df.1[,2] <- sort(df.1[,2])
+  # df.2[,1] <- sort(df.2[,1]); df.2[,2] <- sort(df.2[,2]); df.2[,3] <- sort(df.2[,3])
+  df.3[,1] <- sort(df.3[,1]); df.3[,2] <- sort(df.3[,2])
+  # df.4[,1] <- sort(df.4[,1]); df.4[,2] <- sort(df.4[,2]); df.4[,3] <- sort(df.4[,3])
+
   Log.info('testing')
   expect_equal( dim(df.1), c(3, 2) )
   expect_equal(df.1[,1], factor(c('a', 'b', 'c')))
