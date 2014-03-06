432f4c4d6c0abbea686fd2ada0547dd1449562f2
SpencerA <spnrpa@gmail.com>
2014-03-05 23:57:23 -0800

    typos in num_cols && num_rows

--- h2o-perf/bench/tests/glmVA_airlines/glmVA_airlines_Parse.R
+++ h2o-perf/bench/tests/glmVA_airlines/glmVA_airlines_Parse.R
@@ -2,7 +2,7 @@ source("../../R/h2oPerf/prologue.R")
 data_source <<- "home-0xdiag-datasets"
 trainData   <<- "/home/0xdiag/datasets/standard/allyears.csv"
 num_train_rows <<- 123534970
-num_explan_cols <<- 11
+num_explan_cols <<- 31
 response    <<- "IsDepDelayed"
 import.VA("parsed.hex", trainData)
 
