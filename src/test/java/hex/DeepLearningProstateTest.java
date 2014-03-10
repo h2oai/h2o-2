86c2388f96d806f25a9a4b274ae6e4287d2b4db0
Arno Candel <arno.candel@gmail.com>
2014-03-10 16:59:21 -0700

    Update Prostate Test.  Add code to test AUC threshold confusion matrix, but ran into trouble with rollupstats not being computable, even for tiny data.

--- src/test/java/hex/DeepLearningProstateTest.java
+++ src/test/java/hex/DeepLearningProstateTest.java
@@ -9,10 +9,10 @@ import water.JUnitRunnerDebug;
 import water.Key;
 import water.TestUtil;
 import water.UKV;
+import water.api.AUC;
 import water.fvec.Frame;
 import water.fvec.NFSFileVec;
 import water.fvec.ParseDataset2;
-import water.util.Log;
 
 public class DeepLearningProstateTest extends TestUtil {
   @BeforeClass public static void stall() {
@@ -26,56 +26,82 @@ public class DeepLearningProstateTest extends TestUtil {
     Key dest = Key.make("prostate");
 
     // build the model, with all kinds of shuffling/rebalancing/sampling
-    {
-      DeepLearning p = new DeepLearning();
-      p.epochs = 1000;
-      p.source = frame;
-      p.response = frame.vecs()[1];
-      p.validation = frame;
-      p.destination_key = dest;
+    DeepLearning p = new DeepLearning();
+    p.epochs = 17;
+    p.source = frame;
+    p.response = frame.vecs()[1];
+    p.destination_key = dest;
+    p.seed = 0xC01DF337;
 
-      // Leaky
+    // Leaky
 //      p.force_load_balance = true;
 //      p.balance_classes = true;
 //      p.shuffle_training_data = true;
 //      p.score_training_samples = 100;
-//      p.score_validation_samples = 50;
 //      NNModel mymodel = p.initModel();
 //      p.trainModel(mymodel);
 //      p.trainModel(mymodel);
 //      p.delete();
 
-      // No leaks
-      p.force_load_balance = false;
-      p.balance_classes = false;
-      p.shuffle_training_data = false;
-      p.score_training_samples = 0;
-      p.score_validation_samples = 0;
-      p.execImpl();
-    }
+    // No leaks
+    p.force_load_balance = false;
+    p.balance_classes = false;
+    p.shuffle_training_data = false;
+    p.score_training_samples = 0;
+    p.execImpl();
 
     // score and check result
     {
       DeepLearningModel mymodel = UKV.get(dest); //this actually *requires* frame to also still be in UKV (because of DataInfo...)
       Frame pred = mymodel.score(frame);
+      StringBuilder sb = new StringBuilder();
+      // Score binary classification
+      AUC auc = new AUC();
+      auc.actual = frame;
+      auc.vactual = frame.vecs()[1];
+      auc.predict = pred;
+      auc.vpredict = pred.vecs()[2];
+      auc.threshold_criterion = AUC.ThresholdCriterion.maximum_F1;
+      auc.serve();
+      auc.toASCII(sb);
+      final double threshold = auc.threshold();
+      final double error = auc.err();
+
+      // check absolute value for default operation
+      if (!p.force_load_balance && !p.shuffle_training_data && !p.balance_classes && p.score_training_samples == 0)
+        Assert.assertEquals(0.25263157894736843, error, 1e-15);
+
+      // check that auc.cm() is the right CM
+      Assert.assertEquals(new ConfusionMatrix(auc.cm()).err(), error, 1e-15);
+
+      // check that calcError() is consistent as well (for CM=null, AUC!=null)
+      Assert.assertEquals(mymodel.calcError(frame, pred, "training", true, null, auc), error, 1e-15);
+
+      // Now create labels using the same threshold as the AUC object and compute the CM
+
+      /*
+      // Version 1: Exec2
+//      new Frame(Key.make("pred"), pred.names(), pred.vecs()).delete_and_lock(Key.make("job")); //put prediction into KV store
+//      Frame labels = Exec2.exec("pred[,0]=pred[,2]>=" + threshold).popAry(); //apply threshold criterion
+
+      // Version 2: Without mapreduce (OK since tiny data)
+      for (int i=0; i<pred.numRows(); ++i)
+        pred.vecs()[0].set(i, pred.vecs()[2].at(i) >= threshold ? 1 : 0);
+
       water.api.ConfusionMatrix CM = new water.api.ConfusionMatrix();
-      final double trainErr = mymodel.calcError(frame, pred, "training", true, CM, null);
-      // test ConfusionMatrix accuracy computation
-      CM = new water.api.ConfusionMatrix();
       CM.actual = frame;
       CM.vactual = frame.vecs()[1];
       CM.predict = pred;
       CM.vpredict = pred.vecs()[0];
       CM.serve();
-      StringBuilder sb = new StringBuilder();
+      sb.append("\n");
       CM.toASCII(sb);
-      double error = new ConfusionMatrix(CM.cm).err();
-      assert(error == mymodel.last_scored().train_err);
       Log.info(sb);
-      Assert.assertEquals(trainErr, error, 1e-10);
-      if (error != 0) {
-        Assert.fail("Classification error is not 0, but " + error + ".");
-      }
+
+      // check that threshold-specific CM matches the AUC-reported CM
+      Assert.assertEquals(new ConfusionMatrix(CM.cm).err(), error, 1e-15);
+      */
+
       pred.delete();
       mymodel.delete();
     }
