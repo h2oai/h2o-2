b974d4bdf8c5884638cc12dcb5f1061c711a41b4
cliffclick <cliffc@acm.org>
2014-03-11 12:49:36 -0700

    stupid test bug

--- src/test/java/water/exec/Expr2Test.java
+++ src/test/java/water/exec/Expr2Test.java
@@ -187,7 +187,7 @@ public class Expr2Test extends TestUtil {
       checkStr("apply(h.hex,2,function(x){h.hex})","apply requires that ary fun(ary x) return 1 column");
       checkStr("apply(h.hex,2,function(x){sum(x)/nrow(x)})");
       checkStr("mean=function(x){apply(x,2,sum)/nrow(x)};mean(h.hex)");
-      checkStr("sum(apply(h.hex[,c(4,5)],1,mean))",184.96); // Row-wise apply on mean
+      checkStr("sum(apply(h.hex[,c(4,5)],1,mean))",183.96); // Row-wise apply on mean
 
       // Conditional selection; 
       checkStr("ifelse(0,1,2)",2);
