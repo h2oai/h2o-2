67f17a77112353c7b19dce7c7dbb1aa2185b3afe
anqi <anqi@0xdata.com>
2014-03-12 12:25:00 -0700

    Sigh. R banned overloading of && and || in recent versions.

--- R/h2o-package/R/Classes.R
+++ R/h2o-package/R/Classes.R
@@ -600,8 +600,6 @@ setMethod(">=", c("H2OParsedData", "H2OParsedData"), function(e1, e2) { .h2o.__b
 setMethod("<=", c("H2OParsedData", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("<=", e1, e2) })
 setMethod("&", c("H2OParsedData", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("&", e1, e2) })
 setMethod("|", c("H2OParsedData", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("|", e1, e2) })
-setMethod("&&", c("H2OParsedData", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("&&", e1, e2) })
-setMethod("||", c("H2OParsedData", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("||", e1, e2) })
 setMethod("%*%", c("H2OParsedData", "H2OParsedData"), function(x, y) { .h2o.__binop2("%*%", x, y) })
 
 setMethod("+", c("numeric", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("+", e1, e2) })
@@ -617,8 +615,6 @@ setMethod(">=", c("numeric", "H2OParsedData"), function(e1, e2) { .h2o.__binop2(
 setMethod("<=", c("numeric", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("<=", e1, e2) })
 setMethod("&", c("numeric", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("&", e1, e2) })
 setMethod("|", c("numeric", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("|", e1, e2) })
-setMethod("&&", c("numeric", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("&&", e1, e2) })
-setMethod("||", c("numeric", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("||", e1, e2) })
 
 setMethod("+", c("H2OParsedData", "numeric"), function(e1, e2) { .h2o.__binop2("+", e1, e2) })
 setMethod("-", c("H2OParsedData", "numeric"), function(e1, e2) { .h2o.__binop2("-", e1, e2) })
@@ -633,18 +629,11 @@ setMethod(">=", c("H2OParsedData", "numeric"), function(e1, e2) { .h2o.__binop2(
 setMethod("<=", c("H2OParsedData", "numeric"), function(e1, e2) { .h2o.__binop2("<=", e1, e2) })
 setMethod("&", c("H2OParsedData", "numeric"), function(e1, e2) { .h2o.__binop2("&", e1, e2) })
 setMethod("|", c("H2OParsedData", "numeric"), function(e1, e2) { .h2o.__binop2("|", e1, e2) })
-setMethod("&&", c("H2OParsedData", "numeric"), function(e1, e2) { .h2o.__binop2("&&", e1, e2) })
-setMethod("||", c("H2OParsedData", "numeric"), function(e1, e2) { .h2o.__binop2("||", e1, e2) })
 
 setMethod("&", c("logical", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("&", as.numeric(e1), e2) })
 setMethod("|", c("logical", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("|", as.numeric(e1), e2) })
-setMethod("&&", c("logical", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("&&", as.numeric(e1), e2) })
-setMethod("||", c("logical", "H2OParsedData"), function(e1, e2) { .h2o.__binop2("||", as.numeric(e1), e2) })
 setMethod("&", c("H2OParsedData", "logical"), function(e1, e2) { .h2o.__binop2("&", e1, as.numeric(e2)) })
 setMethod("|", c("H2OParsedData", "logical"), function(e1, e2) { .h2o.__binop2("|", e1, as.numeric(e2)) })
-setMethod("&&", c("H2OParsedData", "logical"), function(e1, e2) { .h2o.__binop2("&&", e1, as.numeric(e2)) })
-setMethod("||", c("H2OParsedData", "logical"), function(e1, e2) { .h2o.__binop2("||", e1, as.numeric(e2)) })
-
 
 setMethod("!", "H2OParsedData", function(x) { .h2o.__unop2("!", x) })
 setMethod("abs", "H2OParsedData", function(x) { .h2o.__unop2("abs", x) })
