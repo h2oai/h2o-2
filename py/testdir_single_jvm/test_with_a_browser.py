396d7d163d4c33cad20b0ca94403358fed46f9ad
Kevin Normoyle <kevin@0xdata.com>
2014-03-20 16:52:07 -0700

    force base_port 54321 always

--- py/testdir_single_jvm/test_with_a_browser.py
+++ py/testdir_single_jvm/test_with_a_browser.py
@@ -15,9 +15,9 @@ class Basic(unittest.TestCase):
         if (localhost):
             h2o.build_cloud(1, use_hdfs=True, 
             hdfs_name_node='192.168.1.176', hdfs_version='cdh3',
-            java_heap_GB=14)
+            java_heap_GB=14, base_port=54321)
         else:
-            h2o_hosts.build_cloud_with_hosts(1)
+            h2o_hosts.build_cloud_with_hosts(1, base_port=54321)
 
     @classmethod
     def tearDownClass(cls):
