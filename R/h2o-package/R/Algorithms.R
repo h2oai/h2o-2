23acb568e9098044122a752bf41778b5ecb24e54
anqi <anqi@0xdata.com>
2014-03-24 12:23:08 -0700

    Fix for PUB-504. Return correctly oriented confusion matrix.

--- R/h2o-package/R/Algorithms.R
+++ R/h2o-package/R/Algorithms.R
@@ -937,7 +937,7 @@ h2o.confusionMatrix <- function(data, reference) {
   
   res = .h2o.__remoteSend(data@h2o, .h2o.__PAGE_CONFUSION, actual = reference@key, vactual = 0, predict = data@key, vpredict = 0)
   cm = lapply(res$cm[-length(res$cm)], function(x) { x[-length(x)] })
-  .build_cm(cm, res$actual_domain, res$predicted_domain, transpose = FALSE)
+  .build_cm(cm, res$actual_domain, res$predicted_domain, transpose = TRUE)
 }
 
 h2o.performance <- function(data, reference, measure = "accuracy", thresholds) {
