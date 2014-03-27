8783fa4483035f03080678a2d97b384d4a9362c1
SpencerA <spnrpa@gmail.com>
2014-03-26 18:42:46 -0700

    add labels

--- h2o-perf/web/prototype/js/perf.js
+++ h2o-perf/web/prototype/js/perf.js
@@ -56,17 +56,17 @@ $('*').on('change', '#question', function() {
 
 function makeGraph(json, svg) {
     var margin = {top: 20, right: 80, bottom: 30, left: 50},
-        width = 960 - margin.left - margin.right,
+        width = 1200 - margin.left - margin.right,
         height = 500 -margin.top -margin.bottom;
 
-    var x = d3.scale.linear().range([0, width]);
+    var x = d3.scale.linear().range([0, width - 100]);
     var y = d3.scale.linear().range([height, 0]);
     var color = d3.scale.category10();
 
     var xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(d3.format("d"));
     var yAxis = d3.svg.axis().scale(y).orient("left");
     
-    var lineFunction = d3.svg.line().interpolate("basis")
+    var lineFunction = d3.svg.line().interpolate("ordinal")
                 .x(function(d) {return x(d[0]); })
                 .y(function(d) { return y(d[1]); });
 
@@ -76,17 +76,16 @@ function makeGraph(json, svg) {
               .append("g")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var datas = new Object();
     var test_names = new Array();
     for(i = 0; i < json.data.length; i++) {
       bv = d3.values(json.data[i])[6].split('.');
       bv = bv[bv.length - 1];
       if (bv == "99999") { continue; }
       if (bv == 99999) { continue; }
-      test_names[i] = d3.values(json.data[i])[1];
-      datas[i] = d3.values(json.data[i]);
+      test_names.push(d3.values(json.data[i])[1]);
     }
     test_names = d3.set(test_names).values();
+    console.log(test_names)
 
     var datas2 = new Array();
     var domains = new Array();
@@ -102,7 +101,6 @@ function makeGraph(json, svg) {
     for(i = 0; i < json.data.length; i++) {
       build = d3.values(json.data[i])[6].split('.');
       build = build[build.length - 1]
-      console.log(build)
       if (build == "99999") { continue; }
       if (build == 99999) { continue; }
       test  = d3.values(json.data[i])[1];
@@ -121,12 +119,12 @@ function makeGraph(json, svg) {
     ]);
 
     y.domain([
-      0,//d3.min(datas2, function(c) { return d3.min(c.data, function(v) {  return +v[1]; }); }),
+      0,
       d3.max(datas2, function(c) { return d3.max(c.data, function(v) { return +v[1]; }); })
     ]);
 
     var linesGroup = svg.append("g").attr("class", "line");
-  
+
     for (var i in datas2) {
         linedata = datas2[i]
         linesGroup.append("path")
@@ -136,30 +134,12 @@ function makeGraph(json, svg) {
                   .attr("stroke", function(d, i) {
                       return linedata.color;
                   });
-//        linesGroup.append("text")
-//              .attr("x", 750)
-//              .attr("dy", ".35em")
-//              .text(linedata.name);
+        linesGroup.append("text")
+              .attr("x", x(linedata.data[linedata.data.length - 1][0]))
+              .attr("y", y(linedata.data[linedata.data.length - 1][1]))
+              .text(linedata.name);
      };
 
-//    var tests = color.domain().map(function(name) {
-//        return {
-//            name: name,
-//            values: datas.filter(function(d) { return d.test_name !== name })
-//                        .map(function(d) {
-//                          build_version = d.build_version.split('.');
-//                          build_version = build_version[build_version.length - 1];
-//                          return {build: build_version, time: d.time_s};
-//                         })
-//         };
-//    });
-//
-//    x.domain(d3.extent(datas, function(d) { bv = d.build_version.split('.'); return bv[bv.length - 1]; }));
-//    y.domain([
-//        d3.min(tests, function(t) { return d3.min(t.values, function(v) {return v.time; }); }),
-//        d3.max(tests, function(t) { return d3.max(t.values, function(v) {return v.time; }); })
-//    ]);
-//
     svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
@@ -174,24 +154,4 @@ function makeGraph(json, svg) {
       .attr("dy", ".71em")
       .style("text-anchor", "end")
       .text("Time (s)");
-//    
-//    
-//    var test = svg.selectAll(".test")
-//      .data(tests)
-//    .enter().append("g")
-//      .attr("class", "test");
-//
-//
-//    test.append("path")
-//      .attr("class", "line")
-//      .attr("d", function(d) { return line(d.values); })
-//      .style("stroke", function(d) { return color(d.name); });
-//
-//  test.append("text")
-//      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-//      .attr("transform", function(d) { return "translate(" + x(d.value.build) + "," + y(d.value.time) + ")"; })
-//      .attr("x", 3)
-//      .attr("dy", ".35em")
-//      .text(function(d) { return d.name; });
-
 }
