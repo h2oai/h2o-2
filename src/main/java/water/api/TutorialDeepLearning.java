52472991cebd372b5f622eaff478c3bfb0bf6246
Arno Candel <arno.candel@gmail.com>
2014-04-04 18:41:59 -0700

    Fill Deep Learning tutorial with content (prostate classification).

--- src/main/java/water/api/TutorialDeepLearning.java
+++ src/main/java/water/api/TutorialDeepLearning.java
@@ -10,10 +10,13 @@ public class TutorialDeepLearning extends TutorialWorkflow {
 
   private final transient TutorWorkflow _wf;
   private final static String[][] TUTORIAL_STEPS = new String[][]{
-    /*               Title     Short Summary     File containing step description */
-    new String[] { "Step 1", "Introduction",    "/tutorials/deeplearning/step1.html" },
-    new String[] { "Step 2", "Dataset inhale",  "/tutorials/deeplearning/step2.html" },
-    new String[] { "Step 3", "Parsing dataset", "/tutorials/deeplearning/step3.html" },
+         /*               Title     Short Summary         File containing step description */
+          new String[] { "Step 1", "Introduction"  ,     "/tutorials/deeplearning/step1.html" },
+          new String[] { "Step 2", "Dataset inhale",     "/tutorials/deeplearning/step2.html" },
+          new String[] { "Step 3", "Parsing dataset",    "/tutorials/deeplearning/step3.html" },
+          new String[] { "Step 4", "Inspecting dataset", "/tutorials/deeplearning/step4.html" },
+          new String[] { "Step 5", "Building model",     "/tutorials/deeplearning/step5.html" },
+          new String[] { "Step 6", "Inspecting model",   "/tutorials/deeplearning/step6.html" },
   };
 
   public TutorialDeepLearning() {
