6d99ea6fd6ba4346c38bac419ce5b055ec1d442c
Raymond Peck <rpeck@rpeck.com>
2014-04-21 18:51:41 -0700

    PP-20 + PP-21: more tests for the two new endpoints.

--- py/testdir_model_management/test_model_management.py
+++ py/testdir_model_management/test_model_management.py
@@ -29,6 +29,29 @@ class Basic(unittest.TestCase):
         if h2o.clone_cloud_json is None:
             h2o.tear_down_cloud()
 
+    def assertKeysExist(self, d, path, keys):
+        path_elems = path.split("/")
+
+        for path_elem in path_elems:
+            if "" != path_elem:
+                assert path_elem in d, "Failed to find key: " + path + " in dict: " + repr(d)
+                d = d[path]
+        
+        for key in keys:
+            assert key in d, "Failed to find key: " + key + " in dict: " + repr(d)
+
+    def assertKeysDontExist(self, d, path, keys):
+        path_elems = path.split("/")
+
+        for path_elem in path_elems:
+            if "" != path_elem:
+                assert path_elem in d, "Unexpectedly failed to find key: " + path + " in dict: " + repr(d)
+                d = d[path]
+        
+        for key in keys:
+            assert key not in d, "Unexpectedly found key: " + key + " in dict: " + repr(d)
+
+
     prostate_hex = None
     airlines_train_hex = None
     airlines_test_hex = None
@@ -78,11 +101,11 @@ class Basic(unittest.TestCase):
         timeoutSecs = 20
         retryDelaySecs = 2
 
-        ################################################
-        print "Generating AirlinesTrain GLM2 model. . ."
+        print "##############################################################"
+        print "Generating AirlinesTrain GLM2 binary classification model. . ."
         # h2o.glm.FV(y = "IsDepDelayed", x = c("Origin", "Dest", "fDayofMonth", "fYear", "UniqueCarrier", "fDayOfWeek", "fMonth", "DepTime", "ArrTime", "Distance"), data = airlines_train.hex, family = "binomial", alpha=0.05, lambda=1.0e-2, standardize=FALSE, nfolds=0)
         glm_AirlinesTrain_1_params = {
-            'destination_key': 'glm_AirlinesTrain_1',
+            'destination_key': 'glm_AirlinesTrain_binary_1',
             'response': 'IsDepDelayed', 
             'ignored_cols': 'IsDepDelayed_REC', 
             'family': 'binomial', 
@@ -95,11 +118,11 @@ class Basic(unittest.TestCase):
         h2o_glm.simpleCheckGLM(self, glm_AirlinesTrain_1, None, **glm_AirlinesTrain_1_params)
 
 
-        ######################################################
-        print "Generating AirlinesTrain simple GBM model. . ."
+        print "####################################################################"
+        print "Generating AirlinesTrain simple GBM binary classification model. . ."
         # h2o.gbm(y = "IsDepDelayed", x = c("Origin", "Dest", "fDayofMonth", "fYear", "UniqueCarrier", "fDayOfWeek", "fMonth", "DepTime", "ArrTime", "Distance"), data = airlines_train.hex, n.trees=3, interaction.depth=1, distribution="multinomial", n.minobsinnode=2, shrinkage=.1)
         gbm_AirlinesTrain_1_params = {
-            'destination_key': 'gbm_AirlinesTrain_1',
+            'destination_key': 'gbm_AirlinesTrain_binary_1',
             'response': 'IsDepDelayed', 
             'ignored_cols_by_name': 'IsDepDelayed_REC', 
             'ntrees': 3,
@@ -110,11 +133,11 @@ class Basic(unittest.TestCase):
         gbm_AirlinesTrain_1 = node.gbm(Basic.airlines_train_hex, timeoutSecs, retryDelaySecs, **gbm_AirlinesTrain_1_params)
 
 
-        ######################################################
-        print "Generating AirlinesTrain complex GBM model. . ."
+        print "#####################################################################"
+        print "Generating AirlinesTrain complex GBM binary classification model. . ."
         # h2o.gbm(y = "IsDepDelayed", x = c("Origin", "Dest", "fDayofMonth", "fYear", "UniqueCarrier", "fDayOfWeek", "fMonth", "DepTime", "ArrTime", "Distance"), data = airlines_train.hex, n.trees=50, interaction.depth=5, distribution="multinomial", n.minobsinnode=2, shrinkage=.1)
         gbm_AirlinesTrain_2_params = {
-            'destination_key': 'gbm_AirlinesTrain_2',
+            'destination_key': 'gbm_AirlinesTrain_binary_2',
             'response': 'IsDepDelayed', 
             'ignored_cols_by_name': 'IsDepDelayed_REC', 
             'ntrees': 50,
@@ -125,11 +148,11 @@ class Basic(unittest.TestCase):
         gbm_AirlinesTrain_2 = node.gbm(Basic.airlines_train_hex, timeoutSecs, retryDelaySecs, **gbm_AirlinesTrain_2_params)
 
 
-        ######################################################
-        print "Generating AirlinesTrain simple DRF model. . ."
+        print "####################################################################"
+        print "Generating AirlinesTrain simple DRF binary classification model. . ."
         # h2o.randomForest.FV(y = "IsDepDelayed", x = c("Origin", "Dest", "fDayofMonth", "fYear", "UniqueCarrier", "fDayOfWeek", "fMonth", "DepTime", "ArrTime", "Distance"), data = airlines_train.hex, ntree=5, depth=2)
         rf_AirlinesTrain_1_params = {
-            'destination_key': 'rf_AirlinesTrain_1',
+            'destination_key': 'rf_AirlinesTrain_binary_1',
             'response': 'IsDepDelayed', 
             'ignored_cols_by_name': 'IsDepDelayed_REC', 
             'ntrees': 5,
@@ -139,11 +162,11 @@ class Basic(unittest.TestCase):
         rf_AirlinesTrain_1 = node.random_forest(Basic.airlines_train_hex, timeoutSecs, retryDelaySecs, **rf_AirlinesTrain_1_params)
 
 
-        #######################################################
-        print "Generating AirlinesTrain complex DRF model. . ."
+        print "#####################################################################"
+        print "Generating AirlinesTrain complex DRF binary classification model. . ."
         # h2o.randomForest.FV(y = "IsDepDelayed", x = c("Origin", "Dest", "fDayofMonth", "fYear", "UniqueCarrier", "fDayOfWeek", "fMonth", "DepTime", "ArrTime", "Distance"), data = airlines_train.hex, ntree=50, depth=10)
         rf_AirlinesTrain_2_params = {
-            'destination_key': 'rf_AirlinesTrain_2',
+            'destination_key': 'rf_AirlinesTrain_binary_2',
             'response': 'IsDepDelayed', 
             'ignored_cols_by_name': 'IsDepDelayed_REC', 
             'ntrees': 50,
@@ -153,16 +176,24 @@ class Basic(unittest.TestCase):
         rf_AirlinesTrain_2 = node.random_forest(Basic.airlines_train_hex, timeoutSecs, retryDelaySecs, **rf_AirlinesTrain_2_params)
 
 
-        # print("Generating AirlinesTrain DeepLearning model. . .")
+        print "######################################################################"
+        print "Generating AirlinesTrain DeepLearning binary classification model. . ."
         # h2o.deeplearning(y = "IsDepDelayed", x = c("Origin", "Dest", "fDayofMonth", "fYear", "UniqueCarrier", "fDayOfWeek", "fMonth", "DepTime", "ArrTime", "Distance"), data = airlines_train.hex, classification=TRUE, hidden=c(10, 10))
+        dl_AirlinesTrain_1_params = {
+            'destination_key': 'dl_AirlinesTrain_binary_1',
+            'response': 'IsDepDelayed', 
+            'ignored_cols': 'IsDepDelayed_REC', 
+            'hidden': [10, 10],
+            'classification': 1
+        }
+        dl_AirlinesTrain_1 = node.deep_learning(Basic.airlines_train_hex, timeoutSecs, retryDelaySecs, **dl_AirlinesTrain_1_params)
 
 
-
-        #################################################################################
-        # print("Generating AirlinesTrain GLM2 model with different response column. . .")
+        print "##############################################################################################"
+        print "Generating AirlinesTrain GLM2 binary classification model with different response column. . ."
         # h2o.glm.FV(y = "IsDepDelayed_REC", x = c("Origin", "Dest", "fDayofMonth", "fYear", "UniqueCarrier", "fDayOfWeek", "fMonth", "DepTime", "ArrTime", "Distance"), data = airlines_train.hex, family = "binomial", alpha=0.05, lambda=1.0e-2, standardize=FALSE, nfolds=0)
         glm_AirlinesTrain_A_params = {
-            'destination_key': 'glm_AirlinesTrain_A',
+            'destination_key': 'glm_AirlinesTrain_binary_A',
             'response': 'IsDepDelayed_REC', 
             'ignored_cols': 'IsDepDelayed', 
             'family': 'binomial', 
@@ -175,9 +206,149 @@ class Basic(unittest.TestCase):
         h2o_glm.simpleCheckGLM(self, glm_AirlinesTrain_A, None, **glm_AirlinesTrain_A_params)
 
 
+        print "#########################################################"
+        print "Generating Prostate GLM2 binary classification model. . ."
+        # h2o.glm.FV(y = "CAPSULE", x = c("AGE","RACE","PSA","DCAPS"), data = prostate.hex, family = "binomial", nfolds = 0, alpha = 0.5)
+        glm_Prostate_1_params = {
+            'destination_key': 'glm_Prostate_binary_1',
+            'response': 'CAPSULE', 
+            'ignored_cols': None, 
+            'family': 'binomial', 
+            'alpha': 0.5, 
+            'n_folds': 0
+        }
+        glm_Prostate_1 = node.GLM(Basic.prostate_hex, timeoutSecs, retryDelaySecs, **glm_Prostate_1_params)
+        h2o_glm.simpleCheckGLM(self, glm_Prostate_1, None, **glm_Prostate_1_params)
 
 
-        # h2o_cmd.runRF(parseResult=parseResult, trees=50, timeoutSecs=10)
+        print "###############################################################"
+        print "Generating Prostate simple DRF binary classification model. . ."
+        # h2o.randomForest.FV(y = "CAPSULE", x = c("AGE","RACE","DCAPS"), data = prostate.hex, ntree=10, depth=5)
+        rf_Prostate_1_params = {
+            'destination_key': 'rf_Prostate_binary_1',
+            'response': 'CAPSULE', 
+            'ignored_cols_by_name': None, 
+            'ntrees': 10,
+            'max_depth': 5,
+            'classification': 1
+        }
+        rf_Prostate_1 = node.random_forest(Basic.prostate_hex, timeoutSecs, retryDelaySecs, **rf_Prostate_1_params)
+
+
+        print "##############################################"
+        print "Generating Prostate GLM2 regression model. . ."
+        # h2o.glm.FV(y = "AGE", x = c("CAPSULE","RACE","PSA","DCAPS"), data = prostate.hex, family = "gaussian", nfolds = 0, alpha = 0.5)
+        glm_Prostate_regression_1_params = {
+            'destination_key': 'glm_Prostate_regression_1',
+            'response': 'AGE', 
+            'ignored_cols': None, 
+            'family': 'gaussian', 
+            'alpha': 0.5, 
+            'n_folds': 0
+        }
+        glm_Prostate_regression_1 = node.GLM(Basic.prostate_hex, timeoutSecs, retryDelaySecs, **glm_Prostate_regression_1_params)
+        h2o_glm.simpleCheckGLM(self, glm_Prostate_regression_1, None, **glm_Prostate_regression_1_params)
+
+
+        print "##############################################"
+        print "Testing /2/Frames with various options. . ."
+        print "##############################################"
+        print ""
+
+
+        print "##############################################"
+        print "Testing /2/Frames list. . ."
+        frames = node.frames()
+        self.assertKeysExist(frames, 'frames', ['airlines_train.hex', 'airlines_test.hex', 'prostate.hex'])
+        self.assertKeysDontExist(frames, 'frames', ['glm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_2', 'rf_AirlinesTrain_binary_1', 'rf_AirlinesTrain_binary_2', 'dl_AirlinesTrain_binary_1', 'glm_AirlinesTrain_binary_A', 'glm_Prostate_binary_1', 'rf_Prostate_binary_1', 'glm_Prostate_regression_1'])
+        self.assertKeysDontExist(frames, '', ['models'])
+
+
+        print "##############################################"
+        print "Testing /2/Frames?key=airlines_test.hex. . ."
+        frames = node.frames(key='airlines_test.hex')
+        self.assertKeysExist(frames, 'frames', ['airlines_test.hex'])
+        self.assertKeysDontExist(frames, 'frames', ['glm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_2', 'rf_AirlinesTrain_binary_1', 'rf_AirlinesTrain_binary_2', 'dl_AirlinesTrain_binary_1', 'glm_AirlinesTrain_binary_A', 'glm_Prostate_binary_1', 'rf_Prostate_binary_1', 'glm_Prostate_regression_1', 'airlines_train.hex', 'prostate.hex'])
+        self.assertKeysDontExist(frames, '', ['models'])
+
+
+        print "##############################################"
+        print "Testing /2/Frames?key=airlines_test.hex&find_compatible_models=true. . ."
+        frames = node.frames(key='airlines_test.hex', find_compatible_models=1)
+        self.assertKeysExist(frames, 'frames', ['airlines_test.hex'])
+        self.assertKeysDontExist(frames, 'frames', ['glm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_2', 'rf_AirlinesTrain_binary_1', 'rf_AirlinesTrain_binary_2', 'dl_AirlinesTrain_binary_1', 'glm_AirlinesTrain_binary_A', 'glm_Prostate_binary_1', 'rf_Prostate_binary_1', 'glm_Prostate_regression_1', 'airlines_train.hex', 'prostate.hex'])
+
+        self.assertKeysExist(frames, '', ['models'])
+        self.assertKeysExist(frames, 'models', ['glm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_2', 'rf_AirlinesTrain_binary_1', 'rf_AirlinesTrain_binary_2', 'dl_AirlinesTrain_binary_1', 'glm_AirlinesTrain_binary_A'])
+        self.assertKeysDontExist(frames, 'models', ['glm_Prostate_binary_1', 'rf_Prostate_binary_1', 'glm_Prostate_regression_1', 'airlines_train.hex', 'airlines_train.hex', 'airlines_test.hex', 'prostate.hex'])
+
+
+        print "##############################################"
+        print "Testing /2/Frames with various options. . ."
+        print "##############################################"
+        print ""
+
+
+        print "##############################################"
+        print "Testing /2/Models list. . ."
+        models = node.models()
+        self.assertKeysExist(models, 'models', ['glm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_2', 'rf_AirlinesTrain_binary_1', 'rf_AirlinesTrain_binary_2', 'dl_AirlinesTrain_binary_1', 'glm_AirlinesTrain_binary_A', 'glm_Prostate_binary_1', 'rf_Prostate_binary_1', 'glm_Prostate_regression_1'])
+        self.assertKeysDontExist(models, 'models', ['airlines_train.hex', 'airlines_test.hex', 'prostate.hex'])
+        self.assertKeysDontExist(models, '', ['frames'])
+
+
+        print "##############################################"
+        print "Testing /2/Models?key=rf_Prostate_binary_1. . ."
+        models = node.models(key='rf_Prostate_binary_1')
+        self.assertKeysExist(models, 'models', ['rf_Prostate_binary_1'])
+        self.assertKeysDontExist(models, 'models', ['airlines_train.hex', 'airlines_test.hex', 'prostate.hex', 'glm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_2', 'rf_AirlinesTrain_binary_1', 'rf_AirlinesTrain_binary_2', 'dl_AirlinesTrain_binary_1', 'glm_AirlinesTrain_binary_A', 'glm_Prostate_binary_1', 'glm_Prostate_regression_1'])
+        self.assertKeysDontExist(models, '', ['frames'])
+
+
+        print "##############################################"
+        print "Testing /2/Models?key=rf_Prostate_binary_1&find_compatible_frames=true. . ."
+        models = node.models(key='rf_Prostate_binary_1', find_compatible_frames=1)
+        self.assertKeysExist(models, 'models', ['rf_Prostate_binary_1'])
+        self.assertKeysDontExist(models, 'models', ['airlines_train.hex', 'airlines_test.hex', 'prostate.hex', 'glm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_1', 'gbm_AirlinesTrain_binary_2', 'rf_AirlinesTrain_binary_1', 'rf_AirlinesTrain_binary_2', 'dl_AirlinesTrain_binary_1', 'glm_AirlinesTrain_binary_A', 'glm_Prostate_binary_1', 'glm_Prostate_regression_1'])
+
+        self.assertKeysExist(models, '', ['frames'])
+        self.assertKeysExist(models, 'frames', ['prostate.hex'])
+        self.assertKeysDontExist(models, 'frames', ['airlines_train.hex', 'airlines_test.hex'])
+
+
+        print "##############################################"
+        print "Testing /2/Frames with scoring. . ."
+        print "##############################################"
+        print ""
+
+
+        print "##############################################"
+        print "Scoring compatible models for /2/Frames?key=airlines_test.hex&find_compatible_models=true. . ."
+        frames = node.frames(key='airlines_test.hex', find_compatible_models=1)
+        compatible_models = frames['frames']['airlines_test.hex']['compatible_models']
+
+        for model_key in compatible_models:
+            scoring_result = node.frames(key='airlines_test.hex', score_model=model_key)
+            self.assertKeysExist(scoring_result, '', ['metrics'])
+            self.assertKeysExist(scoring_result, 'metrics', ['model', 'frame', 'duration_in_ms', 'error', 'cm', 'auc']) # TODO: HitRatio
+            self.assertEqual(scoring_result['metrics']['model'], model_key, "Expected model key: " + model_key + " but got: " + scoring_result['metrics']['model'])
+            self.assertEqual(scoring_result['metrics']['frame'], 'airlines_test.hex', "Expected frame key: " + 'airlines_test.hex' + " but got: " + scoring_result['metrics']['frame'])
+            # TODO: look inside the auc and cm elements
+
+
+        print "##############################################"
+        print "Scoring compatible models for /2/Frames?key=prostate.hex&find_compatible_models=true. . ."
+        frames = node.frames(key='prostate.hex', find_compatible_models=1)
+        compatible_models = frames['frames']['prostate.hex']['compatible_models']
+
+        for model_key in compatible_models:
+            scoring_result = node.frames(key='prostate.hex', score_model=model_key)
+            self.assertKeysExist(scoring_result, '', ['metrics'])
+            self.assertKeysExist(scoring_result, 'metrics', ['model', 'frame', 'duration_in_ms', 'error', 'cm', 'auc']) # TODO: HitRatio
+            self.assertEqual(scoring_result['metrics']['model'], model_key, "Expected model key: " + model_key + " but got: " + scoring_result['metrics']['model'])
+            self.assertEqual(scoring_result['metrics']['frame'], 'prostate.hex', "Expected frame key: " + 'prostate.hex' + " but got: " + scoring_result['metrics']['frame'])
+            # TODO: look inside the auc and cm elements
+
 
 if __name__ == '__main__':
     h2o.unit_main()
