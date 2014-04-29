3fb91bbf362cbb6164adac4beef23807b8280d56
mmalohlava <michal@0xdata.com>
2014-04-28 18:02:11 -0700

    Experimental support for Tachyon FS
    Changes:
     - added tachyon 0.4.1
     - Persist layer for tachyon
     - url for tachyon is tachyon://<address of server/<filename> - e.g.,
       tachyon://localhost:19998/covtype/covtype.data

---
+++ src/main/java/water/persist/PersistTachyon.java
@@ -0,0 +1,136 @@
+package water.persist;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import com.google.common.io.ByteStreams;
+
+import tachyon.client.ReadType;
+import tachyon.client.TachyonFS;
+import water.*;
+import water.Job.ProgressMonitor;
+import water.fvec.Vec;
+import water.util.*;
+
+public class PersistTachyon extends Persist<TachyonFS> {
+
+  public static final String PREFIX = "tachyon://";
+  public static final String DEFAULT_CLIENT_URI = "tachyon://localhost:19998";
+
+  private final String _defaultUri;
+
+  PersistTachyon() { this(DEFAULT_CLIENT_URI); }
+  PersistTachyon(String uri) { _defaultUri = uri; }
+
+  public static InputStream openStream(Key k, ProgressMonitor pmon) {
+    return new H2OTachyonInputStream(k, pmon);
+  }
+
+  @Override public byte[] load(Value v) {
+    long start_io_ms = System.currentTimeMillis();
+    byte[] b = MemoryManager.malloc1(v._max);
+    Key k = v._key;
+    String[] keyComp = decodeKey(k);
+    String clientUri = keyComp[0];
+    String fpath = keyComp[1];
+    TachyonFS tfs = null;
+    InputStream is = null;
+    try {
+      tfs = (TachyonFS) (Persist.I[Value.TACHYON].createClient(clientUri));
+      long start_ns = System.nanoTime(); // Blocking i/o call timing - without counting repeats
+      is = tfs.getFile(fpath).getInStream(ReadType.NO_CACHE);
+      ByteStreams.readFully(is, b);
+      TimeLine.record_IOclose(start_ns, start_io_ms, 1/* read */, v._max, Value.TACHYON);
+      return b;
+    } catch (IOException e) {
+      throw new RuntimeException(Log.err("File load failed: ", e));
+    } finally {
+      if (is!=null) Utils.close(is);
+    }
+  }
+
+  public static final class H2OTachyonInputStream extends RIStream {
+    final private Key key;
+    final private String clientURI;
+    final private String fpath;
+    protected H2OTachyonInputStream(Key k, long from, ProgressMonitor pmon) {
+      super(from, pmon);
+      key = k;
+      String[] c = decodeKey(k);
+      clientURI = c[0];
+      fpath = c[1];
+    }
+    public H2OTachyonInputStream(Key k, ProgressMonitor pmon) {
+      this(k, 0, pmon);
+    }
+    @Override protected InputStream open(long offset) throws IOException {
+      TachyonFS tfs = (TachyonFS) (Persist.I[Value.TACHYON].createClient(clientURI));
+      InputStream is = tfs.getFile(fpath).getInStream(ReadType.NO_CACHE);
+      is.skip(offset);
+      return is;
+    }
+  }
+
+  /** Split key name composed of tachyon://<client-uri>/filename into two parts:
+   *  - client-uri without tachyon:// prefix
+   *  - filename
+   *  And returns both components.
+   */
+  private static String[] decodeKey(Key k) {
+    String s = new String((k.isDVec()) ? Arrays.copyOfRange(k._kb, Vec.KEY_PREFIX_LEN, k._kb.length):k._kb);
+    return decode(s);
+  }
+
+  public static String[] decode(String s) {
+    assert s.startsWith(PREFIX) : "Unsupported key name for tachyon: " + s;
+    s = s.substring(PREFIX.length());
+    int nextSlash = s.indexOf('/');
+    String clientUri, filename;
+    if (nextSlash!=-1) {
+      clientUri = s.substring(0, nextSlash);
+      filename  = s.substring(nextSlash);
+    } else {
+      clientUri = s;
+      filename = "/";
+    }
+    return new String[] { clientUri, filename };
+  }
+
+  //
+  // Un-implemented methods
+  //
+  @Override public Value lazyArrayChunk(Key key) {
+    throw H2O.unimpl();
+  }
+  @Override public String getPath() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override public void clear() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override public void loadExisting() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override public void store(Value v) {
+    throw new RuntimeException("TODO Auto-generated method stub");
+  }
+
+  @Override public void delete(Value v) {
+    throw new UnsupportedOperationException();
+  }
+
+  //
+  // Methods providing low-level client implementation
+  //
+  @Override public String getDefaultURI() {
+    return _defaultUri;
+  }
+  @Override public TachyonFS createClient(String uri) throws IOException {
+    if (!uri.startsWith(PREFIX)) uri = PREFIX + uri;
+    return TachyonFS.get(uri);
+  }
+}
