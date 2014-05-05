8b9497a54577d366a6e0acc7c6725fcab792debc
SpencerA <spnrpa@gmail.com>
2014-05-04 22:59:27 -0700

    remove debug prints

--- R/h2o-package/R/Algorithms.R
+++ R/h2o-package/R/Algorithms.R
@@ -1171,11 +1171,6 @@ h2o.SpeeDRF <- function(x, y, data, classification=TRUE, validation,
 
   # NB: externally, 1 based indexing; internally, 0 based
   cols <- paste(args$x_i - 1, collapse=',')
-  print("DEBUG:")
-  print("X COLUMNS PASSING IN:")
-  print(cols)
-  print("")
-  print("")
   res = .h2o.__remoteSend(data@h2o, .h2o.__PAGE_SpeeDRF, source=data@key, response=args$y, ignored_cols=args$x_ignore, num_trees=ntree, max_depth=depth, validation=validation@key,
                           sample=sample.rate, bin_limit=nbins, seed=seed, stat_type = stat.type, oobee=as.numeric(oobee), sampling_strategy=sampling_strategy, strata_samples=strata_samples, class_weights=classwt)
 
