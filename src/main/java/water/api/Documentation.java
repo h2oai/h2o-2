70180f7e298ab4aa6346a3cc47cde1240f5f6c8a
Tom Kraljevic <tomk@tomk.net>
2014-05-07 19:41:20 -0700

    Refactor buildNumber() into AbstractBuildVersion.java.

--- src/main/java/water/api/Documentation.java
+++ src/main/java/water/api/Documentation.java
@@ -8,24 +8,10 @@ import water.H2O;
  */
 public class Documentation extends HTMLOnlyRequest {
   protected String build(Response response) {
-    String branchName = null;
-    String buildNumber = null;
-    try {
-      AbstractBuildVersion abv = H2O.getBuildVersion();
-      branchName = abv.branchName();
-      String projectVersion = abv.projectVersion();
-      buildNumber = projectVersion.split("\\.")[3];
-    }
-    catch (Exception xe) {}
-
-    String url;
-    if (branchName == null || buildNumber == null) {
-      url = "http://docs.0xdata.com/";
-    }
-    else {
-      url = "http://s3.amazonaws.com/h2o-release/h2o/" + branchName + "/" + buildNumber + "/docs-website/index.html";
-    }
-
+    AbstractBuildVersion abv = H2O.getBuildVersion();
+    String branchName = abv.branchName();
+    String buildNumber = abv.buildNumber();
+    String url = "http://s3.amazonaws.com/h2o-release/h2o/" + branchName + "/" + buildNumber + "/docs-website/index.html";
     return "<meta http-equiv=\"refresh\" content=\"0; url=" + url + "\">";
   }
 }
