eb4104bf0fbc33d28d9cdb19f872a7842bc29178
Kevin Normoyle <kevin@0xdata.com>
2014-05-13 14:28:56 -0700

    named keys in cbind seems to fail in exec

---
+++ py/testdir_single_jvm_fvec/test_exec2_cbind_fail.py
@@ -0,0 +1,40 @@
+import unittest, random, sys, time
+sys.path.extend(['.','..','py'])
+import h2o, h2o_exec as h2e, h2o_hosts, h2o_import as h2i
+
+class Basic(unittest.TestCase):
+    def tearDown(self):
+        h2o.check_sandbox_for_errors()
+
+    @classmethod
+    def setUpClass(cls):
+        global SEED, localhost
+        SEED = h2o.setup_random_seed()
+        localhost = h2o.decide_if_localhost()
+        if (localhost):
+            h2o.build_cloud(1)
+        else:
+            h2o_hosts.build_cloud_with_hosts(1)
+
+    @classmethod
+    def tearDownClass(cls):
+        h2o.tear_down_cloud()
+
+    def test_exec2_cbind_fail(self):
+
+        for i in range(5):
+            execExpr = "h <- cbind(c(0,0,0), c(1,1,1))"
+            h2e.exec_expr(execExpr=execExpr, timeoutSecs=30)
+            # have to make sure they're created as keys for reuse between execs
+            execExpr = "a=c(0,0,0); b=c(0,0,0); d=c(0,0,0); e=c(0,0,0); f=c(0,0,0); g= c(0,0,0);"
+            h2e.exec_expr(execExpr=execExpr, timeoutSecs=30)
+            execExpr = "b=a; d=a; f=a; g=a;"
+            h2e.exec_expr(execExpr=execExpr, timeoutSecs=30)
+            execExpr = "h <- cbind(a, b, d, f, g)"
+            h2e.exec_expr(execExpr=execExpr, timeoutSecs=30)
+
+        h2o.check_sandbox_for_errors()
+
+
+if __name__ == '__main__':
+    h2o.unit_main()
