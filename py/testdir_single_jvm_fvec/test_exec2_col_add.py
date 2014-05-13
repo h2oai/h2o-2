4486004ffc5da1d292e6170aa22494dd19c020bf
Kevin Normoyle <kevin@0xdata.com>
2014-05-13 09:43:39 -0700

    update

--- py/testdir_single_jvm_fvec/test_exec2_col_add.py
+++ py/testdir_single_jvm_fvec/test_exec2_col_add.py
@@ -7,49 +7,12 @@ DO_PLOT = getpass.getuser()=='kevin'
 DO_PLOT = True
 DO_ORIG = True
 
-# new ...ability to reference cols
-# src[ src$age<17 && src$zip=95120 && ... , ]
-# can specify values for enums ..values are 0 thru n-1 for n enums
-# the inspect info looks like this for the 8MB temps created..just 4 chunks
-# Row C1
-# Change Type 
-# Type    Real
-# Min 4.221149880967445E-6
-# Max 1.999996683238248
-# Mean    0.999
-# Size    7.6 MB
-# /192.168.1.41:54321, 0  C8D
-# /192.168.1.41:54321, 228060 C8D
-# /192.168.1.41:54321, 456150 C8D
-# /192.168.1.41:54321, 684255 C8D
-
-# Do Execs not do compression?
-
 initList = [
         'r2 = c(1); r2 = r1[,c(1)]',
         ]
-if DO_ORIG:
-    # update to write back to the original dataset at the same time as the last temp
-    exprList = [
-        'Last.value.0 = r1[,c(1)]',
-        'Last.value.1 = any.factor(Last.value.0)',
-        'Last.value.2 = Last.value.0 + 1',
-        'r1[,1] = Last.value.3 = log(Last.value.2)',
-
-        'Last.value.4 = r2',
-        'Last.value.5 = any.factor(Last.value.4)',
-        'Last.value.6 = Last.value.4 + 1',
-        'r1[,1] = Last.value.7 = log(Last.value.6)',
-
-        'Last.value.8 = r2',
-        'Last.value.9 = any.factor(Last.value.8)',
-        'Last.value.10 = Last.value.8 + 1',
-        'r1[,1] = Last.value.11 = log(Last.value.10)',
-
-        ]
-else:
-    exprList = [
-        'Last.value.3 = r2+1',
+exprList = [
+        'r1[,1] = Last.value = r2',
+        'apply(r1,2,sum)',
         ]
 
 class Basic(unittest.TestCase):
@@ -62,30 +25,29 @@ class Basic(unittest.TestCase):
         SEED = h2o.setup_random_seed()
         localhost = h2o.decide_if_localhost()
         if (localhost):
-            h2o.build_cloud(1, java_heap_GB=14, java_extra_args='-XX:+PrintGCDetails', base_port=54321)
+            h2o.build_cloud(1, java_heap_GB=12, java_extra_args='-XX:+PrintGCDetails', base_port=54321)
         else:
             h2o_hosts.build_cloud_with_hosts(1, java_heap_GB=28, java_extra_args='-XX:+PrintGCDetails', base_port=54321)
 
 
     @classmethod
     def tearDownClass(cls):
-        time.sleep(3600)
         h2o.tear_down_cloud()
 
-    def test_exec2_sum(self):
+    def test_exec2_col_add(self):
         h2o.beta_features = True
         bucket = 'home-0xdiag-datasets'
         # csvPathname = 'airlines/year2013.csv'
         if localhost:
-            csvPathname = '1B/reals_100000x1000_15f.data'
+            # csvPathname = '1B/reals_100000x1000_15f.data'
             # csvPathname = '1B/reals_1000000x1000_15f.data'
-            # csvPathname = '1B/reals_1000000x1_15f.data'
+            csvPathname = '1B/reals_1000000x1_15f.data'
             # csvPathname = '1B/reals_1B_15f.data'
             # csvPathname = '1B/reals_100M_15f.data'
         else:
-            csvPathname = '1B/reals_100000x1000_15f.data'
+            # csvPathname = '1B/reals_100000x1000_15f.data'
             # csvPathname = '1B/reals_1000000x1000_15f.data'
-            # csvPathname = '1B/reals_1000000x1_15f.data'
+            csvPathname = '1B/reals_1000000x1_15f.data'
             # csvPathname = '1B/reals_1B_15f.data'
             # csvPathname = '1B/reals_100M_15f.data'
 
@@ -103,10 +65,11 @@ class Basic(unittest.TestCase):
         fList = []
         for execExpr in initList:
             execResult, result = h2e.exec_expr(h2o.nodes[0], execExpr, resultKey=None, timeoutSecs=300)
-        for trial in range(300):
+        for trial in range(1000):
             for execExpr in exprList:
                 # put the trial number into the temp for uniqueness
                 execExpr = re.sub('Last.value', 'Last.value%s' % trial, execExpr)
+                execExpr = re.sub(',1', ',%s' % trial, execExpr)
                 start = time.time()
                 execResult, result = h2e.exec_expr(h2o.nodes[0], execExpr, resultKey=None, timeoutSecs=300)
                 execTime = time.time() - start
@@ -123,27 +86,18 @@ class Basic(unittest.TestCase):
                 print "value_size_bytes: %s" % " ".join(map(str,v))
 
                 # print "result:", result
-                if DO_ORIG:
-                    if 'r1' in execExpr:
-                        xList.append(trial)
-                        eList.append(execTime)
-                    if 'log' in execExpr:
-                        fList.append(execTime)
-                else:
+                if ('r1' in execExpr) and (not 'apply' in execExpr):
                     xList.append(trial)
                     eList.append(execTime)
+                if ('apply' in execExpr):
                     fList.append(execTime)
 
         h2o.check_sandbox_for_errors()
         # PLOTS. look for eplot.jpg and fplot.jpg in local dir?
         if DO_PLOT:
             xLabel = 'trial'
-            if DO_ORIG:
-                eLabel = 'time: Last.value<trial>.4 = r1[,c(1)]'
-                fLabel = 'time: Last.value<trial>.7 = log(Last.value<trial>.6)'
-            else:
-                eLabel = 'time: Last.value.3 = r2+1'
-                fLabel = 'time: Last.value.3 = r2+1'
+            eLabel = 'time: r1[,1] = Last.value = r2',
+            fLabel = 'time: apply(r1, 2, sum)',
             eListTitle = ""
             fListTitle = ""
             h2o_gbm.plotLists(xList, xLabel, eListTitle, eList, eLabel, fListTitle, fList, fLabel, server=True)
