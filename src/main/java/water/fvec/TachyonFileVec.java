8e085a774b2bb2a067df5b381597a6e7ee945b6e
mmalohlava <michal@0xdata.com>
2014-05-21 15:41:32 -0700

    Mimic Tomas fix for platform vectors.

--- src/main/java/water/fvec/TachyonFileVec.java
+++ src/main/java/water/fvec/TachyonFileVec.java
@@ -13,13 +13,16 @@ public class TachyonFileVec extends FileVec {
   }
   public static Key make(String serverUri, ClientFileInfo tf, Futures fs) {
     String fname = tf.getPath(); // Always return absolute path /dir/filename
-    Key k = Key.make(PersistTachyon.PREFIX + serverUri + fname);
     long size = tf.getLength();
+    Key k = Key.make(PersistTachyon.PREFIX + serverUri + fname);
     Key k2 = Vec.newKey(k);
+    new Frame(k2).delete_and_lock(null);
     // Insert the top-level FileVec key into the store
     Vec v = new TachyonFileVec(k2,size);
     DKV.put(k2, v, fs);
-    new Frame(k,new String[]{fname},new Vec[]{v}).delete_and_lock(null).unlock(null);
+    Frame fr = new Frame(k,new String[] {fname}, new Vec[] {v});
+    fr.update(null);
+    fr.unlock(null);
     return k;
   }
   private TachyonFileVec(Key key, long len) {super(key,len,Value.TACHYON);}
