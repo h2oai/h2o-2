38d293425330d14da6defa884a95b8641c16d83e
Arno Candel <arno.candel@gmail.com>
2014-05-30 16:25:57 -0700

    Avoid NPE.

--- src/main/java/hex/gbm/GBM.java
+++ src/main/java/hex/gbm/GBM.java
@@ -155,7 +155,7 @@ public class GBM extends SharedTreeModelBuilder<GBM.GBMModel> {
       }
     }
     @Override protected void setCrossValidationError(ValidatedJob job, double cv_error, water.api.ConfusionMatrix cm, water.api.AUC auc, water.api.HitRatio hr) {
-      GBMModel gbmm = ((GBM)job).makeModel(this, cv_error, new ConfusionMatrix(cm.cm, cms[0].nclasses()), this.varimp, auc);
+      GBMModel gbmm = ((GBM)job).makeModel(this, cv_error, cm.cm == null ? null : new ConfusionMatrix(cm.cm, cms[0].nclasses()), this.varimp, auc);
       gbmm._have_cv_results = true;
       DKV.put(this._key, gbmm); //overwrite this model
     }
