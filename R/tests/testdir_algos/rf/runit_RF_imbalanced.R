5f112721dafb61f5ef8caccec5a1e793adfb3eb6
Arno Candel <arno.candel@gmail.com>
2014-06-18 15:15:56 -0700

    HEX-1695: Add RUnit test for DRF balance_classes option.

---
+++ R/tests/testdir_algos/rf/runit_RF_imbalanced.R
@@ -0,0 +1,17 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source('../../findNSourceUtils.R')
+
+test.rf.imbalanced <- function(conn) {
+  prostate = h2o.uploadFile(conn, locate("smalldata/logreg/prostate.csv"))
+
+  hh_imbalanced=h2o.randomForest(x=c(1,2,3,5),y=4,ntree=10,data=prostate,validation=prostate,balance.classes=F)
+  print(hh_imbalanced)
+  hh_balanced=h2o.randomForest(x=c(1,2,3,5),y=4,ntree=10,data=prostate,validation=prostate,balance.classes=T)
+  print(hh_balanced)
+  # compare overal classification error
+  checkTrue(hh_imbalanced@model$confusion[4,4] > hh_balanced@model$confusion[4,4], "balance_classes makes it worse!")
+
+  testEnd()
+}
+
+doTest("rf imbalanced", test.rf.imbalanced)
