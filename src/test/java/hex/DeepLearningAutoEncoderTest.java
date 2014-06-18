1a712c74ab4c81a24f1ab303d0373b63cfad921f
Arno Candel <arno.candel@gmail.com>
2014-06-17 18:29:19 -0700

    PUB-603: Adapt AutoEncoder test to ECG data.

--- src/test/java/hex/DeepLearningAutoEncoderTest.java
+++ src/test/java/hex/DeepLearningAutoEncoderTest.java
@@ -1,6 +1,7 @@
 package hex;
 
 import hex.deeplearning.*;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import water.*;
@@ -13,7 +14,7 @@ import water.util.Log;
 import water.util.MRUtils;
 
 public class DeepLearningAutoEncoderTest extends TestUtil {
-  static final String PATH = "smalldata/logreg/prostate.csv";
+  static final String PATH = "smalldata/anomaly/ecg_discord.csv";
 
   @BeforeClass public static void stall() {
     stall_till_cloudsize(JUnitRunnerDebug.NODES);
@@ -29,36 +30,56 @@ public class DeepLearningAutoEncoderTest extends TestUtil {
     DeepLearning p = new DeepLearning();
     p.source = frame;
     p.autoencoder = true;
-    p.response = frame.vecs()[0]; //easy way to ignore ID (col 0)
+    p.response = frame.vecs()[0]; //ignored anyway
+    p.classification = false;
 
     p.seed = seed;
-    p.hidden = new int[]{10,5};
+    p.hidden = new int[]{20};
     p.adaptive_rate = true;
     p.l1 = 1e-4;
-    p.l2 = 1e-4;
+//    p.l2 = 1e-4;
 //    p.rate = 1e-5;
     p.activation = DeepLearning.Activation.Tanh;
     p.loss = DeepLearning.Loss.MeanSquare;
 //    p.initial_weight_distribution = DeepLearning.InitialWeightDistribution.Normal;
 //    p.initial_weight_scale = 1e-3;
-    p.epochs = 1000;
+    p.epochs = 100;
 //    p.shuffle_training_data = true;
-    p.force_load_balance = true;
+    p.force_load_balance = false;
     p.invoke();
 
     DeepLearningModel mymodel = UKV.get(p.dest());
 
-    // Reconstruct data
-    double quantile = 0.99;
-//    final Vec l2 = frame.lastVec();
-    final Vec l2 = mymodel.scoreAutoEncoder(frame);
-    double thres = mymodel.calcOutlierThreshold(l2, quantile);
+    // Verification of results
 
-    // print stats and outliers
-    Log.info("L2 norm of reconstruction error (in normalized space):");
     StringBuilder sb = new StringBuilder();
+    sb.append("Verifying results.");
+
+    // Reconstruct data using the same helper functions and verify that self-reported MSE agrees
+    double quantile = 0.95;
+    final Frame l2_frame = mymodel.scoreAutoEncoder(frame);
+    final Vec l2 = l2_frame.anyVec();
+    double thres = mymodel.calcOutlierThreshold(l2, quantile);
     sb.append("Mean reconstruction error: " + l2.mean() + "\n");
-    sb.append("The following points are reconstructed with an error above the " + quantile*100 + "-th percentile - outliers?\n");
+    Assert.assertEquals(mymodel.mse(), l2.mean(), 1e-6);
+
+    // manually compute L2
+    Frame reconstr = mymodel.score(frame);
+    double mean_l2 = 0;
+    for (int r=0; r<reconstr.numRows(); ++r) {
+      double my_l2 = 0;
+      for (int c = 0; c < reconstr.numCols(); ++c) {
+        my_l2 += Math.pow((reconstr.vec(c).at(r) - frame.vec(c).at(r)) * mymodel.model_info().data_info()._normMul[c], 2);
+      }
+      mean_l2 += my_l2;
+    }
+    mean_l2 /= reconstr.numRows();
+    reconstr.delete();
+    sb.append("Mean reconstruction error (check): " + l2.mean() + "\n");
+    Assert.assertEquals(mymodel.mse(), mean_l2, 1e-6);
+
+    // print stats and potential outliers
+    sb.append("The following points are reconstructed with an error above the " + quantile*100 + "-th percentile - candidates for outliers.\n");
     for( long i=0; i<l2.length(); i++ ) {
       if (l2.at(i) > thres) {
         sb.append(String.format("row %d : l2 error = %5f\n", i, l2.at(i)));
@@ -66,18 +87,11 @@ public class DeepLearningAutoEncoderTest extends TestUtil {
     }
     Log.info(sb);
 
-    assert(DKV.get(frame._key) != null);
     // cleanup
-    Log.info("before l2: " + H2O.store_size());
-    Futures fs = new Futures();
-    l2.remove(fs);
-    fs.blockForPending();
-    Log.info("after l2: " + H2O.store_size());
-    mymodel.delete();
-    Log.info("before frame: " + H2O.store_size());
-    frame.delete();
-    Log.info("after frame: " + H2O.store_size());
     p.delete();
+    mymodel.delete();
+    frame.delete();
+    l2_frame.delete();
   }
 }
 
