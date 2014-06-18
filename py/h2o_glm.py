ad6d9efbfaa6cf52f0d27f53873dda5a357f7fd2
Tomas Nykodym <tomas.nykodym@gmail.com>
2014-06-17 19:40:55 -0700

    Fixed my previous update of h2o_glm.py (missing colon)

--- py/h2o_glm.py
+++ py/h2o_glm.py
@@ -161,7 +161,7 @@ def simpleCheckGLM(self, glm, colX, allowFailWarning=False, allowZeroCoeff=False
         print "lambda_max:", lambda_max
 
         # currently lambda_max is not set by tomas. ..i.e.not valid
-        if 1==0 and lambda_max <= submodels[best_lambda_idx].lambda_value
+        if 1==0 and (lambda_max <= submodels[best_lambda_idx].lambda_value):
             raise Exception("lambda_max %s should always be > the lambda result %s we're checking" % (lambda_max, submodels[best_lambda_idx].lambda_value))
 
         # submodels0 = submodels[0]
