7c2f69a3f1d748e9145fa1cdda892a9d74e92c7f
Arno Candel <arno.candel@gmail.com>
2014-06-18 15:29:07 -0700

    Fix typo.

--- R/tests/testdir_algos/rf/runit_RF_imbalanced.R
+++ R/tests/testdir_algos/rf/runit_RF_imbalanced.R
@@ -8,7 +8,8 @@ test.rf.imbalanced <- function(conn) {
   print(hh_imbalanced)
   hh_balanced=h2o.randomForest(x=c(1,2,3,5),y=4,ntree=10,data=prostate,validation=prostate,balance.classes=T)
   print(hh_balanced)
-  # compare overal classification error
+
+  # compare overall classification error
   checkTrue(hh_imbalanced@model$confusion[4,4] > hh_balanced@model$confusion[4,4], "balance_classes makes it worse!")
 
   testEnd()
