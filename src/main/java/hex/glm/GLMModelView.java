44bfcd425f52f150add25008d4f68109739926fc
Tomas Nykodym <tomas.nykodym@gmail.com>
2014-06-19 13:10:07 -0700

    Fix of glm2 display. More lambda_value -> lambda renamings.

--- src/main/java/hex/glm/GLMModelView.java
+++ src/main/java/hex/glm/GLMModelView.java
@@ -33,7 +33,7 @@ public class GLMModelView extends Request2 {
 
   public static String link(String txt, Key model) {return link(txt,model,Double.NaN);}
   public static String link(String txt, Key model, double lambda) {
-    return "<a href='GLMModelView.html?_modelKey=" + model + "&lambda_value=" + lambda + "'>" + txt + "</a>";
+    return "<a href='GLMModelView.html?_modelKey=" + model + "&lambda=" + lambda + "'>" + txt + "</a>";
   }
   public static Response redirect(Request req, Key modelKey) {
     return Response.redirect(req, "/2/GLMModelView", "_modelKey", modelKey);
@@ -73,8 +73,8 @@ public class GLMModelView extends Request2 {
     parm(sb,"&epsilon;<sub>&beta;</sub>",glm_model.beta_eps);
     parm(sb,"&alpha;",glm_model.alpha);
     if(!Double.isNaN(glm_model.lambda_max))
-      parm(sb,"&lambda_value;<sub>max</sub>",DFORMAT2.format(glm_model.lambda_max));
-    parm(sb,"&lambda_value;",DFORMAT2.format(lambda));
+      parm(sb,"&lambda;<sub>max</sub>",DFORMAT2.format(glm_model.lambda_max));
+    parm(sb,"&lambda;",DFORMAT2.format(lambda));
 
     if(glm_model.submodels.length > 1){
       sb.append("\n<table class='table table-bordered table-condensed'>\n");
@@ -110,7 +110,7 @@ public class GLMModelView extends Request2 {
       sm = glm_model.submodels[0];
       while(glm_model.submodels[ii].lambda_value != lambda && ++ii < glm_model.submodels.length)
         sm = glm_model.submodels[ii];
-      if(ii == glm_model.submodels.length)throw new IllegalArgumentException("Unexpected value of lambda_value '" + lambda + "'");
+      if(ii == glm_model.submodels.length)throw new IllegalArgumentException("Unexpected value of lambda '" + lambda + "'");
     }
     if(glm_model.beta() != null)
       coefs2html(sm,sb);
