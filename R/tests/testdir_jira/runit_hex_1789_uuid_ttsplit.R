026dfaaa24c380660326f43ffd29cfe5e5eb5184
Tom Kraljevic <tomk@tomk.net>
2014-06-20 21:03:25 -0700

    Add NAs to the test.

--- R/tests/testdir_jira/runit_hex_1789_uuid_ttsplit.R
+++ R/tests/testdir_jira/runit_hex_1789_uuid_ttsplit.R
@@ -1,6 +1,6 @@
 ######################################################################
-# Test for PUB-169
-# apply should work across columns
+# Test for HEX-1789
+# UUID
 ######################################################################
 
 # setwd("/Users/tomk/0xdata/ws/h2o/R/tests/testdir_jira")
@@ -9,15 +9,22 @@ setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 options(echo=TRUE)
 source('../findNSourceUtils.R')
 
-test.colapply <- function(conn) {
+test.uuid <- function(conn) {
   Log.info('Importing test_uuid.csv to H2O...')
   df <- h2o.importFile(conn, normalizePath(locate('smalldata/test/test_uuid.csv')))
   colnames(df) = c("AA", "UUID", "CC")
   
-  Log.info("Slice a subset of columns")
+  Log.info("Slice a subset of columns 1")
   df.train = df[df$CC == 1,]
   
+  df2 <- h2o.importFile(conn, normalizePath(locate('smalldata/test/test_uuid_na.csv')))
+  colnames(df2) = c("AA", "UUID", "CC")
+
+  Log.info("Slice a subset of columns 2")
+  df2.train = df2[df2$CC == 1,]
+  df2.test = df2[df2$CC == 0,]
+
   testEnd()
 }
 
-doTest("PUB-169 Test: Apply scale over columns", test.colapply)
+doTest("HEX-1789 Test: UUID", test.uuid)
