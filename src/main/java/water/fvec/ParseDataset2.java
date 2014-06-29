e2f12f68b12f2a88b184c65e6f1d3bccb170dd57
Tom Kraljevic <tomk@tomk.net>
2014-06-28 18:37:45 -0700

    Revert "Comment out logParseResults."
    This reverts commit f7d4b208665036b529b045ddbfe972013403ace7.

--- src/main/java/water/fvec/ParseDataset2.java
+++ src/main/java/water/fvec/ParseDataset2.java
@@ -373,7 +373,7 @@ public final class ParseDataset2 extends Job {
       eut.doAll(evecs);
     }
 
-//    logParseResults(job, fr);
+    logParseResults(job, fr);
 
     // Release the frame for overwriting
     fr.unlock(job.self());
