1d6312726649fb5425aebb672c7a3512f4314988
mmalohlava <michal@0xdata.com>
2014-07-02 17:57:59 -0700

    PUB-768 SpeeDRF tests for imbalance data with cross validation

---
+++ src/test/java/hex/singlenoderf/SpeeDRFTest2.java
@@ -0,0 +1,42 @@
+package hex.singlenoderf;
+
+import junit.framework.Assert;
+import water.*;
+import water.fvec.Frame;
+import water.fvec.Vec;
+import hex.trees.TreeTestWithBalanceAndCrossVal;
+
+public class SpeeDRFTest2 extends TreeTestWithBalanceAndCrossVal {
+
+  @Override protected void testBalanceWithCrossValidation(String dataset, int response, int[] ignored_cols, int ntrees, int nfolds) {
+    Frame f = parseFrame(dataset);
+    SpeeDRFModel model = null;
+    SpeeDRF spdrf = new SpeeDRF();
+    try {
+      Vec respVec = f.vec(response);
+      // Build a model
+      spdrf.source = f;
+      spdrf.response = respVec;
+      spdrf.ignored_cols = ignored_cols;
+      spdrf.classification = true;
+      spdrf.num_trees = ntrees;
+      spdrf.balance_classes = true;
+      spdrf.n_folds = nfolds;
+      spdrf.keep_cross_validation_splits = false;
+      spdrf.invoke();
+      Assert.assertEquals("Number of cross validation model is wrond!", nfolds, spdrf.xval_models.length);
+      model = UKV.get(spdrf.dest());
+    } finally {
+      if (f!=null) f.delete();
+      if (model!=null) {
+        if (spdrf.xval_models!=null) {
+          for (Key k : spdrf.xval_models) {
+            Model m = UKV.get(k);
+            m.delete();
+          }
+        }
+        model.delete();
+      }
+    }
+  }
+}
