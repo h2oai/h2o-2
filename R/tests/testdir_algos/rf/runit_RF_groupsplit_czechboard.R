96cf6236d6f5c246f4dfb6546d8e7afd8a031d46
anqi <anqi@0xdata.com>
2014-07-08 16:12:54 -0700

    Add a javapredict test with czechboard data

--- R/tests/testdir_algos/rf/runit_RF_groupsplit_czechboard.R
+++ R/tests/testdir_algos/rf/runit_RF_groupsplit_czechboard.R
@@ -4,14 +4,14 @@ source('../../findNSourceUtils.R')
 test.DRF.Czechboard <- function(conn) {
   # Training set has checkerboard pattern
   Log.info("Importing czechboard_300x300.csv data...\n")
-  board.hex <- h2o.uploadFile(conn, locate("smalldata/histogram_test/czechboard_300x300.csv"), key = "board.hex", header = FALSE)
+  board.hex <- h2o.uploadFile(conn, locate("smalldata/histogram_test/czechboard_300x300.csv"), key = "board.hex")
   board.hex[,3] <- as.factor(board.hex[,3])
   Log.info("Summary of czechboard_300x300.csv from H2O:\n")
   print(summary(board.hex))
   
   # Train H2O DRF Model:
   Log.info("H2O DRF with parameters:\nclassification = TRUE, ntree = 50, depth = 20, nbins = 500\n")
-  drfmodel.h2o <- h2o.randomForest(x = c("C1", "C2"), y = "C3", data = board.hex, classification = TRUE, ntree = 50, depth = 20, nbins = 100)
+  drfmodel.h2o <- h2o.randomForest(x = c("C1", "C2"), y = "C3", data = board.hex, classification = TRUE, ntree = 50, depth = 20, nbins = 500)
   print(drfmodel.h2o)
   
   testEnd()
