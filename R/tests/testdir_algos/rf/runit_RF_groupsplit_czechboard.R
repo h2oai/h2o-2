39d3a4c646f5986131f45fb5a191107089b3e327
anqi <anqi@0xdata.com>
2014-07-14 18:44:54 -0700

    Modify czechboard test to check that AUC is higher and overall error lower in group split compared to naive approach.

--- R/tests/testdir_algos/rf/runit_RF_groupsplit_czechboard.R
+++ R/tests/testdir_algos/rf/runit_RF_groupsplit_czechboard.R
@@ -10,9 +10,16 @@ test.DRF.Czechboard <- function(conn) {
   print(summary(board.hex))
   
   # Train H2O DRF Model:
-  Log.info("H2O DRF with parameters:\nclassification = TRUE, ntree = 50, depth = 20, nbins = 500\n")
-  drfmodel.h2o <- h2o.randomForest(x = c("C1", "C2"), y = "C3", data = board.hex, classification = TRUE, ntree = 50, depth = 20, nbins = 500)
-  print(drfmodel.h2o)
+  Log.info("H2O DRF (Naive Split) with parameters:\nclassification = TRUE, ntree = 50, depth = 20, nbins = 500\n")
+  drfmodel.nogrp <- h2o.randomForest(x = c("C1", "C2"), y = "C3", data = board.hex, classification = TRUE, ntree = 50, depth = 20, nbins = 500, doGrpSplit = FALSE)
+  print(drfmodel.nogrp)
+  
+  Log.info("H2O DRF (Group Split) with parameters:\nclassification = TRUE, ntree = 50, depth = 20, nbins = 500\n")
+  drfmodel.grpsplit <- h2o.randomForest(x = c("C1", "C2"), y = "C3", data = board.hex, classification = TRUE, ntree = 50, depth = 20, nbins = 500, doGrpSplit = TRUE)
+  print(drfmodel.grpsplit)
+  
+  expect_true(drfmodel.grpsplit@model$auc >= drfmodel.nogrp@model$auc)
+  expect_true(drfmodel.grpsplit@model$confusion[3,3] <= drfmodel.nogrp@model$confusion[3,3])
   
   testEnd()
 }
