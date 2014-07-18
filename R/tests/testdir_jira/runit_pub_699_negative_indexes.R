d0d32e56d407d0e8de849ff803f7c3f8e57dd046
SpencerA <spnrpa@gmail.com>
2014-07-17 23:51:15 -0700

    PUB-699 Negative indexing exclusion fix *Also print out a better warning if select is too big (was just unimpl before)

--- R/tests/testdir_jira/runit_pub_699_negative_indexes.R
+++ R/tests/testdir_jira/runit_pub_699_negative_indexes.R
@@ -16,7 +16,7 @@ expect_equal(9, dim(prostate.local)[2])
 expect_equal(100, dim(prostate.local[-101:-380,])[1])
 expect_equal(100, dim(prostate.hex[-101:-380,])[1])
 
-simple column exclusion
+#simple column exclusion
 expect_equal(7, dim(prostate.local[,-8:-9,])[2])
 expect_equal(7, dim(prostate.hex[,-8:-9,])[2])
 
@@ -27,6 +27,33 @@ expect_equal(378, dim(prostate.hex[c(-101, -110),])[1])
 # list column exclusion
 expect_equal(6, dim(prostate.local[,c(-1, -3, -5),])[2])
 expect_equal(6, dim(prostate.hex[,c(-1, -3, -5),])[2])
+
+Log.info("Check that OOB indexes are ignored...")
+expect_equal(380, dim(prostate.hex[-c(1000:9230),])[1])
+
+  Log.info("Now trying with a multi-chunk data set")
+  
+  covtype <- h2o.importFile(H2Oserver, normalizePath(locate("smalldata/covtype/covtype.altered.gz")), "cov")
+  
+  Log.info("Number of columns & rows in covtype")
+  print(dim(covtype))
+  
+  print(covtype)
+
+  slice_range_across_chunks <- c(-1:-50, -400000:-450000)
+
+  Log.info("Number of rows to slice out")
+  print(length(slice_range_across_chunks))
+
+  sliced_cov <- covtype[slice_range_across_chunks,]
+  
+  Log.info("sliced covtype dimensions:")
+  print(dim(sliced_cov))
+
+  Log.info("Check that we have the difference in rows equal to the length of the slice_range_across_chunks variable")  
+  print(nrow(covtype) - nrow(sliced_cov))
+  
+  expect_equal(length(slice_range_across_chunks), nrow(covtype) - nrow(sliced_cov))
 
 testEnd()
 
