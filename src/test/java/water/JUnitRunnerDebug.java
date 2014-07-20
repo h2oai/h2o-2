31ab7cb8a0614db7f72ac54b8ecd0bdb74efa228
Arno Candel <arno.candel@gmail.com>
2014-07-19 18:03:49 -0700

    PUB-841: Add test for C16Chunk and cleanup its inflate_impl.

--- src/test/java/water/JUnitRunnerDebug.java
+++ src/test/java/water/JUnitRunnerDebug.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JUnitRunnerDebug {
-  public static final int NODES = 3;
+  public static final int NODES = 1;
 
   public static void main(String[] args) throws Exception {
     int[] ports = new int[NODES];
@@ -54,16 +54,16 @@ public class JUnitRunnerDebug {
 
       // Deep Learning tests
 //      tests.add(DeepLearningVsNeuralNet.class); //only passes for NODES=1, not clear why
-      tests.add(DeepLearningAutoEncoderTest.class); //test Deep Learning convergence
-      tests.add(DeepLearningAutoEncoderCategoricalTest.class); //test Deep Learning convergence
-      tests.add(DeepLearningSpiralsTest.class); //test Deep Learning convergence
-      tests.add(DeepLearningIrisTest.Short.class); //compare Deep Learning vs reference
-//      tests.add(DeepLearningIrisTest.Long.class); //compare Deep Learning vs reference
-      tests.add(DeepLearningProstateTest.Short.class); //test Deep Learning
-//      tests.add(DeepLearningProstateTest.Long.class); //test Deep Learning
-      tests.add(NeuronsTest.class); //test Deep Learning
-      tests.add(MRUtilsTest.class); //test MR sampling/rebalancing
-      tests.add(DropoutTest.class); //test NN Dropput
+//      tests.add(DeepLearningAutoEncoderTest.class); //test Deep Learning convergence
+//      tests.add(DeepLearningAutoEncoderCategoricalTest.class); //test Deep Learning convergence
+//      tests.add(DeepLearningSpiralsTest.class); //test Deep Learning convergence
+//      tests.add(DeepLearningIrisTest.Short.class); //compare Deep Learning vs reference
+////      tests.add(DeepLearningIrisTest.Long.class); //compare Deep Learning vs reference
+//      tests.add(DeepLearningProstateTest.Short.class); //test Deep Learning
+////      tests.add(DeepLearningProstateTest.Long.class); //test Deep Learning
+//      tests.add(NeuronsTest.class); //test Deep Learning
+//      tests.add(MRUtilsTest.class); //test MR sampling/rebalancing
+//      tests.add(DropoutTest.class); //test NN Dropput
 
       // Chunk tests
       tests.add(C0LChunkTest.class);
@@ -77,17 +77,18 @@ public class JUnitRunnerDebug {
       tests.add(C4SChunkTest.class);
       tests.add(C8ChunkTest.class);
       tests.add(C8DChunkTest.class);
+      tests.add(C16ChunkTest.class);
       tests.add(CBSChunkTest.class);
       tests.add(CX0ChunkTest.class);
       tests.add(CXIChunkTest.class);
       tests.add(CXDChunkTest.class);
 
-      tests.add(GLMTest2.class);
-      tests.add(DRFTest.class);
-      tests.add(DRFTest2.class);
-      tests.add(GBMTest.class);
-      tests.add(KMeans2Test.class);
-      tests.add(PCATest.class);
+//      tests.add(GLMTest2.class);
+//      tests.add(DRFTest.class);
+//      tests.add(DRFTest2.class);
+//      tests.add(GBMTest.class);
+//      tests.add(KMeans2Test.class);
+//      tests.add(PCATest.class);
 
       // Uncomment this to sleep here and use the browser.
       // try { Thread.sleep(10000000); } catch (Exception _) {}
---
+++ src/test/java/water/fvec/C16ChunkTest.java
@@ -0,0 +1,54 @@
+package water.fvec;
+
+import org.junit.Assert;
+import org.junit.Test;
+import water.TestUtil;
+
+import java.util.Arrays;
+
+public class C16ChunkTest extends TestUtil {
+  @Test
+  public void test_inflate_impl() {
+    for (int l=0; l<2; ++l) {
+      NewChunk nc = new NewChunk(null, 0);
+
+      long[] vals = new long[]{Long.MIN_VALUE, Long.MAX_VALUE, 0l, 12312421425l, 23523523423l, Long.MIN_VALUE+1, Long.MAX_VALUE-1, -823048234l, -123123l};
+      if (l==1) nc.addNA();
+      for (long v : vals) nc.addUUID(v,v);
+      nc.addNA();
+
+      Chunk cc = nc.compress();
+      Assert.assertEquals(vals.length + 1 + l, cc.len());
+      Assert.assertTrue(cc instanceof C16Chunk);
+      for (int i = 0; i < vals.length; ++i) Assert.assertEquals(vals[i], cc.at16l0(l+i));
+      for (int i = 0; i < vals.length; ++i) Assert.assertEquals(vals[i], cc.at16l(l+i));
+      for (int i = 0; i < vals.length; ++i) Assert.assertEquals(vals[i], cc.at16h0(l+i));
+      for (int i = 0; i < vals.length; ++i) Assert.assertEquals(vals[i], cc.at16h(l+i));
+      Assert.assertTrue(cc.isNA0(vals.length+l));
+      Assert.assertTrue(cc.isNA(vals.length+l));
+
+      nc = cc.inflate_impl(new NewChunk(null, 0));
+      Assert.assertEquals(vals.length + 1 + l, nc.len());
+
+      // NewChunk has no support for UUID (yet?)
+//      for (int i = 0; i < vals.length; ++i) Assert.assertEquals(vals[i], nc.at16l0(l+i));
+//      for (int i = 0; i < vals.length; ++i) Assert.assertEquals(vals[i], nc.at16l(l+i));
+//      for (int i = 0; i < vals.length; ++i) Assert.assertEquals(vals[i], nc.at16h0(l+i));
+//      for (int i = 0; i < vals.length; ++i) Assert.assertEquals(vals[i], nc.at16h(l+i));
+//      Assert.assertTrue(nc.isNA0(vals.length+l));
+//      Assert.assertTrue(nc.isNA(vals.length+l));
+
+      Chunk cc2 = nc.compress();
+      Assert.assertEquals(vals.length + 1 + l, cc.len());
+      Assert.assertTrue(cc2 instanceof C16Chunk);
+      for (int i = 0; i < vals.length; ++i) Assert.assertEquals(vals[i], cc2.at16l0(l+i));
+      for (int i = 0; i < vals.length; ++i) Assert.assertEquals(vals[i], cc2.at16l(l+i));
+      for (int i = 0; i < vals.length; ++i) Assert.assertEquals(vals[i], cc2.at16h0(l+i));
+      for (int i = 0; i < vals.length; ++i) Assert.assertEquals(vals[i], cc2.at16h(l+i));
+      Assert.assertTrue(cc2.isNA0(vals.length+l));
+      Assert.assertTrue(cc2.isNA(vals.length+l));
+
+      Assert.assertTrue(Arrays.equals(cc._mem, cc2._mem));
+    }
+  }
+}
