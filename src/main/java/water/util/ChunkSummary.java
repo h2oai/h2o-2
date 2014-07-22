9ef65b97f8fe53a5545a6c76b5de7c9392bcd398
Arno Candel <arno.candel@gmail.com>
2014-07-22 16:57:10 -0700

    PUB-866: Forgot to add CBSChunk to ChunkSummary.

--- src/main/java/water/util/ChunkSummary.java
+++ src/main/java/water/util/ChunkSummary.java
@@ -13,6 +13,7 @@ public class ChunkSummary extends MRTask2<ChunkSummary> {
   final transient static String[] chunkTypes = new String[]{
           "C0LChunk",
           "C0DChunk",
+          "CBSChunk",
           "C1Chunk",
           "C1NChunk",
           "C1SChunk",
@@ -59,9 +60,7 @@ public class ChunkSummary extends MRTask2<ChunkSummary> {
           byte_size_per_node[H2O.SELF.index()] += c.byteSize();
         }
       }
-      if (!found) {
-        throw H2O.unimpl();
-      }
+      assert(found);
     }
   }
 
@@ -88,13 +87,12 @@ public class ChunkSummary extends MRTask2<ChunkSummary> {
       total_chunk_count += chunk_counts[j];
     }
 
-    // TODO: Why is this slightly different for some data?
-//    long check = 0;
-//    for (int i=0; i<_fr.numCols(); ++i) {
-//      check += _fr.vecs()[i].nChunks();
-//    }
-//    assert(total_chunk_count == check);
-//    assert(total_chunk_byte_size == _fr.byteSize());
+    long check = 0;
+    for (int i=0; i<_fr.numCols(); ++i) {
+      check += _fr.vecs()[i].nChunks();
+    }
+    assert(total_chunk_count == check);
+    assert(total_chunk_byte_size == _fr.byteSize());
 
     // compute min, max, mean
     byte_size_per_node_min = Float.MAX_VALUE;
