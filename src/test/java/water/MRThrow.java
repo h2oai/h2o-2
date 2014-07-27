e26964aaca98930adac5629a73750e703203e01b
Cliff Click <cliffc@acm.org>
2014-07-26 23:11:31 -0700

    Turn on the rest of the MRThrow tests

--- src/test/java/water/MRThrow.java
+++ src/test/java/water/MRThrow.java
@@ -5,14 +5,12 @@ import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.util.concurrent.ExecutionException;
-
 import jsr166y.CountedCompleter;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
-import water.fvec.C1NChunk;
 import water.fvec.Chunk;
 import water.fvec.NFSFileVec;
+import water.util.Utils;
 
 public class MRThrow extends TestUtil {
 
@@ -22,7 +20,7 @@ public class MRThrow extends TestUtil {
   // Map in h2o.jar - a multi-megabyte file - into Arraylets.
   // Run a distributed byte histogram.  Throw an exception in *some* map call,
   // and make sure it's forwarded to the invoke.
-  //@Test
+  @Test
   public void testInvokeThrow() {
     File file = find_test_file("target/h2o.jar");
     Key h2okey = load_test_file(file);
@@ -46,7 +44,8 @@ public class MRThrow extends TestUtil {
     }
   }
 
-  @Test public void testGetThrow() {
+  @Test 
+  public void testGetThrow() {
     File file = find_test_file("target/h2o.jar");
     Key h2okey = load_test_file(file);
     NFSFileVec nfs = DKV.get(h2okey).get();
@@ -69,7 +68,7 @@ public class MRThrow extends TestUtil {
     }
   }
 
-  //@Test
+  @Test
   public void testContinuationThrow() throws InterruptedException, ExecutionException {
     File file = find_test_file("target/h2o.jar");
     Key h2okey = load_test_file(file);
@@ -78,7 +77,6 @@ public class MRThrow extends TestUtil {
       for(int i = 0; i < H2O.CLOUD._memary.length; ++i){
         ByteHistoThrow bh = new ByteHistoThrow();
         bh._throwAt = H2O.CLOUD._memary[i].toString();
-        System.out.println("RUNNING NODE:" + bh._throwAt);
         final boolean [] ok = new boolean[]{false};
         try {
           bh.setCompleter(new CountedCompleter() {
@@ -127,18 +125,13 @@ public class MRThrow extends TestUtil {
     @SuppressWarnings("divzero")
     @Override public void map( Chunk chk ) {
       _x = new int[256];        // One-time set histogram array
-      C1NChunk c1n = (C1NChunk)chk;
-      byte[] bits = c1n._mem;  // Compute local histogram
+      byte[] bits = chk._mem;   // Compute local histogram
       for( byte b : bits )
         _x[b&0xFF]++;
       if(H2O.SELF.toString().equals(_throwAt))
         throw new RuntimeException("test");
     }
     // ADD together all results
-    @Override public void reduce( ByteHistoThrow bh ) {
-      if( _x == null ) { _x = bh._x; return; }
-      for( int i=0; i<_x.length; i++ )
-        _x[i] += bh._x[i];
-    }
+    @Override public void reduce( ByteHistoThrow bh ) { Utils.add(_x,bh._x); }
   }
 }
