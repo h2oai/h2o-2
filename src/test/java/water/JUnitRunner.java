698bfc7efc5edc8c0a3d5ca33a013107638f4ed7
Arno Candel <arno.candel@gmail.com>
2014-07-27 17:01:47 -0700

    PUB-880: Move ParseAllSmalldata from ParserTest to ParserTest2.

--- src/test/java/water/JUnitRunner.java
+++ src/test/java/water/JUnitRunner.java
@@ -11,8 +11,8 @@ import org.junit.runner.notification.Failure;
 
 import water.deploy.Node;
 import water.deploy.NodeVM;
+import water.fvec.ParserTest2;
 import water.parser.ParseFolderTestBig;
-import water.parser.ParserTest;
 import water.util.Log;
 import water.util.Utils;
 
@@ -43,7 +43,7 @@ public class JUnitRunner {
     tests.remove(DeepLearningProstateTest.Long.class);
     tests.remove(GLMRandomTest.Short.class);
     tests.remove(GLMRandomTest.Long.class);
-    tests.remove(ParserTest.ParseAllSmalldata.class);
+    tests.remove(ParserTest2.ParseAllSmalldata.class);
     // Skip abstract test
     tests.remove(TreeTestWithBalanceAndCrossVal.class);
 
