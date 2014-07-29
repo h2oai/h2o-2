e60853de88c417d9f8eb50040b07d46685e4e322
Raymond Peck <rpeck@rpeck.com>
2014-07-29 15:33:49 -0700

    PUB-882: move test_model_management to test_multi_jvm, and override the broken default timeout so it has a chance of running.

--- py/testdir_multi_jvm/test_model_management.py
+++ py/testdir_multi_jvm/test_model_management.py
@@ -22,10 +22,10 @@ class ModelManagementTestCase(unittest.TestCase):
         localhost = h2o.decide_if_localhost()
         if (localhost):
             print "Calling h2o.build_cloud(" + str(cloud_size) + "). . ."
-            h2o.build_cloud(cloud_size)
+            h2o.build_cloud(cloud_size, java_heap_GB=2, timeoutSecs=30)
         else:
-            h2o_hosts.build_cloud_with_hosts(1)
             print "Calling h2o_hosts.build_cloud_with_hosts(1). . ."
+            h2o_hosts.build_cloud_with_hosts(1, java_heap_GB=2, timeoutSecs=30)
         
         # USE FVec!
         h2o.beta_features = True
