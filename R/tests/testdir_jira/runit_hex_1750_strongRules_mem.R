9a209d9a4611bc4f3330b2fe65d69154480428bf
arielrao <raoariel@users.noreply.github.com>
2014-07-29 08:42:46 -0700

    Temp shortening of SR test runtime

--- R/tests/testdir_jira/runit_hex_1750_strongRules_mem.R
+++ R/tests/testdir_jira/runit_hex_1750_strongRules_mem.R
@@ -19,11 +19,11 @@ test <- function(conn) {
   print("Dimension of arcene data: ")
     dim(arcene.train.full)
   
-  print("Set memory benchmark with strong rules off.")
-  print("Model successfully created for arcene data with 5000 columns")
-    h2o.model.noSR.pass <- h2o.glm(x=c(1:5000), y="arcene.train.label", data=arcene.train.full, family="binomial", lambda_search=FALSE,alpha=1, nfolds=0, use_all_factor_levels=1)
-  print("Model fails to be created for arcene data with 7000 columns due to memory error, without strong rules.")
-    assertError(h2o.model.noSR.fail <- h2o.glm(x=c(1:7000), y="arcene.train.label", data=arcene.train.full, family="binomial", lambda_search=FALSE,alpha=1, nfolds=0, use_all_factor_levels=1))
+  #print("Set memory benchmark with strong rules off.")
+  #print("Model successfully created for arcene data with 5000 columns")
+    #h2o.model.noSR.pass <- h2o.glm(x=c(1:5000), y="arcene.train.label", data=arcene.train.full, family="binomial", lambda_search=FALSE,alpha=1, nfolds=0, use_all_factor_levels=1)
+  #print("Model fails to be created for arcene data with 7000 columns due to memory error, without strong rules.")
+   # assertError(h2o.model.noSR.fail <- h2o.glm(x=c(1:7000), y="arcene.train.label", data=arcene.train.full, family="binomial", lambda_search=FALSE,alpha=1, nfolds=0, use_all_factor_levels=1))
   
   print("Model successfully created for arcene data with 7000 columns with strong rules on.")
     h2o.model.SR.pass <- h2o.glm(x=c(1:7000), y="arcene.train.label", data=arcene.train.full, family="binomial", lambda_search=T,alpha=1, nfolds=0, use_all_factor_levels=1)
