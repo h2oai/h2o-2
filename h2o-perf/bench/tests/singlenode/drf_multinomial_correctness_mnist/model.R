f2ba6a823761c4fe1f1e92ffbca6510b8f317646
SpencerA <spnrpa@gmail.com>
2014-08-05 00:10:49 -0700

    update rf benchmark tests for correctness

--- h2o-perf/bench/tests/singlenode/drf_multinomial_correctness_mnist/model.R
+++ h2o-perf/bench/tests/singlenode/drf_multinomial_correctness_mnist/model.R
@@ -1,3 +1,3 @@
 source("../../../R/h2oPerf/prologue.R")
-runRF(x = 2:785, y = 1, depth = 20, ntree = 50)
+runRF(x = 2:785, y = 1, depth = 20, ntree = 50, type = "BigData")
 source("../../../R/h2oPerf/epilogue.R")
