073fc02996cc1dbec400f7de5afe3f41d66469dc
Arno Candel <arno.candel@gmail.com>
2014-08-07 11:35:01 -0700

    Add back commented-out code for reproducibilty for single-node operation of DL.

--- src/main/java/hex/deeplearning/DeepLearningTask.java
+++ src/main/java/hex/deeplearning/DeepLearningTask.java
@@ -44,7 +44,12 @@ public class DeepLearningTask extends FrameTask<DeepLearningTask> {
 
   @Override public final void processRow(long seed, final double [] nums, final int numcats, final int [] cats, double [] responses){
     if(_output.get_params().self() != null && !Job.isRunning(_output.get_params().self())) throw new Job.JobCancelledException();
-    seed = new Random().nextLong(); //no point in being reproducible - better to be "good" at being random
+//    if (H2O.CLOUD.size()==1) {
+//      seed += model_info().get_processed_global(); //avoid periodicity
+//    } else {
+//      seed = new Random().nextLong(); //multi-node: no point in being reproducible - better to be "good" at being random
+//    }
+    seed = new Random().nextLong();
     ((Neurons.Input)_neurons[0]).setInput(seed, nums, numcats, cats);
     step(seed, _neurons, _output, _training, responses);
   }
