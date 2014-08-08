678c79d42ebea61411a19a00eb4039c3d2cf1f7f
Kevin Normoyle <kevin@0xdata.com>
2014-08-08 12:38:20 -0700

    fix build_cloud() if/else

--- py/testdir_single_jvm/test_GBMScore.py
+++ py/testdir_single_jvm/test_GBMScore.py
@@ -9,8 +9,11 @@ class Basic(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         global SEED, localhost
-        h2o.build_cloud(1,java_heap_GB=10)
-        #h2o_hosts.build_cloud_with_hosts()
+        localhost = h2o.decide_if_localhost()
+        if (localhost):
+            h2o.build_cloud(java_heap_GB=10)
+        else:
+            h2o_hosts.build_cloud_with_hosts()
 
     @classmethod
     def tearDownClass(cls):
