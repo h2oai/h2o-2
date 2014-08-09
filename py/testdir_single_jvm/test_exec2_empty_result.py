e3d7f0f7391ffb03d9a670f2d19f329b57ca9120
Kevin Normoyle <kevin@0xdata.com>
2014-08-09 14:08:16 -0700

    this fails with NPE in summary2 prepass. on empty data frame from exec (has reals and enums, not sure about ints (iris))

--- py/testdir_single_jvm/test_exec2_empty_result.py
+++ py/testdir_single_jvm/test_exec2_empty_result.py
@@ -1,29 +1,15 @@
 import unittest, random, sys, time
 sys.path.extend(['.','..','py'])
-import h2o, h2o_browse as h2b, h2o_exec as h2e, h2o_hosts, h2o_import as h2i
+import h2o, h2o_browse as h2b, h2o_exec as h2e, h2o_hosts, h2o_import as h2i, h2o_cmd
 
 initList = [
         ('r', 'r=i.hex'),
         ]
 
-exprListFull = [
-    "r[r[,1]>2,1]",
-    "r[r[,1]>10,1]",
-    "a=c(1,2,3); a[a[,1]>2,1]",
-    "a=c(1,2,3); a[a[,1]>10,1]",
+exprList = [
+    "a=c(1,2,3); a = r[r[,1]>2,]",
+    "a=c(1,2,3); a = r[r[,1]>10,]",
     ]
-
-# concatenate a lot of random choices to make life harder
-if 1==0:
-    exprList = []
-    for i in range(10):
-        expr = ""
-        for j in range(1):
-            expr += "z.hex=" + random.choice(exprListFull) + ";"
-            # expr += random.choice(exprListFull) + ";"
-        exprList.append(expr)
-
-exprList = exprListFull
 
 class Basic(unittest.TestCase):
     def tearDown(self):
@@ -35,7 +21,7 @@ class Basic(unittest.TestCase):
         SEED = h2o.setup_random_seed()
         localhost = h2o.decide_if_localhost()
         if (localhost):
-            h2o.build_cloud(1, java_heap_GB=14)
+            h2o.build_cloud()
         else:
             h2o_hosts.build_cloud_with_hosts(1)
 
@@ -45,19 +31,21 @@ class Basic(unittest.TestCase):
 
     def test_exec2_empty_result(self):
         h2o.beta_features = True
-        bucket = 'home-0xdiag-datasets'
-        # csvPathname = 'airlines/year2013.csv'
-        csvPathname = 'standard/covtype.data'
+        bucket = 'smalldata'
+        csvPathname = 'iris/iris2.csv'
         hexKey = 'i.hex'
         parseResult = h2i.import_parse(bucket=bucket, path=csvPathname, schema='put', hex_key=hexKey)
 
         for resultKey, execExpr in initList:
             h2e.exec_expr(h2o.nodes[0], execExpr, resultKey=None, timeoutSecs=10)
+
         start = time.time()
-        # h2e.exec_expr_list_rand(len(h2o.nodes), exprList, 'r1.hex', maxTrials=200, timeoutSecs=10)
         for execExpr in exprList:
             h2e.exec_expr(h2o.nodes[0], execExpr, resultKey=None, timeoutSecs=10)
+            rSummary = h2o_cmd.runSummary(key="a")
+            h2o_cmd.infoFromSummary(rSummary)
 
+            
         h2o.check_sandbox_for_errors()
         print "exec end on ", "operators" , 'took', time.time() - start, 'seconds'
 
