e60d2e3156ef0fb3a967cee6752afb40606542d1
Arno Candel <arno.candel@gmail.com>
2014-08-09 12:14:11 -0700

    Remove invalid value for response_factors (cannot be 0 - see the comment!).

--- py/testdir_single_jvm/test_create_frame_rand1.py
+++ py/testdir_single_jvm/test_create_frame_rand1.py
@@ -17,7 +17,7 @@ paramDict = {
     'integer_fraction': [None, 0.1, 1.0], # Fraction of integer columns (for randomize=true)
     'integer_range': [None, 0, 1, 1234567890], # -range to range
     'missing_fraction': [None, 0.1, 1.0],
-    'response_factors': [None, 0, 1, 2, 10], # Number of factor levels of the first column (1=real, 2=binomial, N=multinomial)
+    'response_factors': [None, 1, 2, 10], # Number of factor levels of the first column (1=real, 2=binomial, N=multinomial)
 }
 
 class Basic(unittest.TestCase):
