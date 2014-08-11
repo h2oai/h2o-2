e23c0721ff4f74c799c97c5007d5ccc97b58b2d9
Kevin Normoyle <kevin@0xdata.com>
2014-08-11 14:01:39 -0700

    does predict and auc after bayes on covtype with col 54 turned to factor by exec (didn't have any bayes tests) not sure if the auc makes sense, but it gets        java.lang.RuntimeException: org.apache.commons.math3.exception.NotStrictlyPositiveException: standard deviation (0) + on the predict using the same data for predict as training

---
+++ py/testdir_single_jvm/test_bayes_rand2.py
@@ -0,0 +1,85 @@
+import unittest, random, sys, time
+sys.path.extend(['.','..','py'])
+import h2o, h2o_cmd, h2o_glm, h2o_hosts, h2o_import as h2i, h2o_util, h2o_exec as h2e
+
+def define_params():
+    paramDict = {
+        'ignored_cols': [None, 0, 1],
+        # response col must be categorical?
+        # laplace smoothing parameter
+        # between 0 and 100000
+        'laplace': [None, 0, 1, 10, 1e5],
+        'drop_na_cols': [None, 0, 1],
+        }
+    return paramDict
+
+class Basic(unittest.TestCase):
+    def tearDown(self):
+        h2o.check_sandbox_for_errors()
+
+    @classmethod
+    def setUpClass(cls):
+        global SEED, localhost
+        SEED = h2o.setup_random_seed()
+        localhost = h2o.decide_if_localhost()
+        if (localhost):
+            h2o.build_cloud()
+        else:
+            h2o_hosts.build_cloud_with_hosts()
+
+    @classmethod
+    def tearDownClass(cls):
+        h2o.tear_down_cloud()
+
+    def test_bayes_and2(self):
+        h2o.beta_features = True
+        csvPathname = 'standard/covtype.data'
+        parseResult = h2i.import_parse(bucket='home-0xdiag-datasets', path=csvPathname, schema='put')
+        paramDict = define_params()
+        for trial in range(1):
+            params = {
+                'response': 54, 
+                }
+
+            colX = h2o_util.pickRandParams(paramDict, params)
+            kwargs = params.copy()
+
+            timeoutSecs = 120
+            # chagne response to factor
+            execExpr = 'covtype.hex[,54+1] = factor(covtype.hex[,54+1])'
+            resultExec, ncols = h2e.exec_expr(execExpr=execExpr)
+
+            start = time.time()
+            bayesResult = h2o.nodes[0].naive_bayes(timeoutSecs=timeoutSecs, source='covtype.hex', **kwargs)
+            print "bayes end on ", csvPathname, 'took', time.time() - start, 'seconds'
+
+            print "bayes result:", h2o.dump_json(bayesResult)
+
+            nb_model = bayesResult['nb_model']
+            ncats = nb_model['ncats']
+            nnums = nb_model['nnums']
+            pcond = nb_model['pcond']
+            pprior = nb_model['pprior']
+            rescnt = nb_model['rescnt']
+            modelClassDist = nb_model['_modelClassDist']
+            names = nb_model['_names']
+            domains = nb_model['_domains']
+            priorClassDist = nb_model['_priorClassDist']
+            model_key = nb_model['_key']
+
+
+            # is it an error to get std dev of 0 after predicting?
+            print "Doing predict with same dataset, and the bayes model"
+            h2o.nodes[0].generate_predictions(model_key=model_key, data_key='covtype.hex', prediction='Predict.hex')
+
+            # just get a predict and AUC on the same data. has to be binomial result
+            resultAUC = h2o.nodes[0].generate_auc(thresholds=None, actual=hexKey, predict='Predict.hex',
+                vactual=y, vpredict=1)
+            print "AUC result:", h2o.dump_json(resultAUC)
+
+
+            print "Trial #", trial, "completed\n"
+
+
+if __name__ == '__main__':
+    h2o.unit_main()
