fc9017d57ebd84a1b184aad83c722207fcd2fbe7
Kevin Normoyle <kevin@0xdata.com>
2014-08-22 20:58:46 -0700

    looks good (no error in log) now

--- py/testdir_multi_jvm/test_network.py
+++ py/testdir_multi_jvm/test_network.py
@@ -2,7 +2,7 @@ import unittest, sys, time
 sys.path.extend(['.','..','py'])
 import h2o, h2o_cmd, h2o_hosts, h2o_browse as h2b, h2o_import as h2i
 
-IGNORE_ERRORS = True
+IGNORE_ERRORS = False
 class Basic(unittest.TestCase):
     def tearDown(self):
         h2o.check_sandbox_for_errors()
