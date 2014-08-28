dd262f94c2ca65cf5df72475f079aea146ea7b8b
Tom Kraljevic <tomk@tomk.net>
2014-08-27 19:05:42 -0700

    Initial. Trivial API method to check if H2O is up.

---
+++ src/main/java/water/api/Up.java
@@ -0,0 +1,14 @@
+package water.api;
+
+import com.google.gson.JsonObject;
+import water.util.Log;
+
+public class Up extends Request {
+  @Override public RequestServer.API_VERSION[] supportedVersions() { return SUPPORTS_V1_V2; }
+
+  @Override
+  protected Response serve() {
+    JsonObject response = new JsonObject();
+    return Response.done(response);
+  }
+}
