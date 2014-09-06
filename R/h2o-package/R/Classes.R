dbbf5653de2a80deebf46f1cd2e377211c57b933
SpencerA <spnrpa@gmail.com>
2014-09-06 00:53:51 -0700

    model show for gbm/drf to show overall mse

--- R/h2o-package/R/Classes.R
+++ R/h2o-package/R/Classes.R
@@ -270,7 +270,7 @@ setMethod("show", "H2ODRFModel", function(object) {
   if(!is.null(model$varimp)) {
     cat("\nVariable importance:\n"); print(model$varimp)
   }
-  cat("\nMean-squared Error by tree:\n"); print(model$mse)
+  cat("\nOverall Mean-squared Error: ", model$err[[length(model$err)]], "\n")
   if(length(object@xval) > 0) {
     cat("\nCross-Validation Models:\n")
     print(sapply(object@xval, function(x) x@key))
@@ -363,7 +363,7 @@ setMethod("show", "H2OGBMModel", function(object) {
   if(!is.null(model$varimp)) {
     cat("\nVariable importance:\n"); print(model$varimp)
   }
-  cat("\nMean-squared Error by tree:\n"); print(model$err)
+  cat("\nOverall Mean-squared Error: ", model$err[[length(model$err)]], "\n")
   if(length(object@xval) > 0) {
     cat("\nCross-Validation Models:\n")
     print(sapply(object@xval, function(x) x@key))
