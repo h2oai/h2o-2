88e084abb051c13750a660ffc93d2150cd6881a2
Kevin Normoyle <kevin@0xdata.com>
2014-09-15 17:49:04 -0700

    update for new ips

--- py/testdir_0xdata_only/test_hdfs_cdh4_fvec.py
+++ py/testdir_0xdata_only/test_hdfs_cdh4_fvec.py
@@ -12,9 +12,9 @@ class Basic(unittest.TestCase):
         global localhost
         localhost = h2o.decide_if_localhost()
         if (localhost):
-            h2o.build_cloud(1, use_hdfs=True, hdfs_version='cdh4', hdfs_name_node='192.168.1.161')
+            h2o.build_cloud(1, use_hdfs=True, hdfs_version='cdh4', hdfs_name_node='172.16.2.176')
         else:
-            h2o_hosts.build_cloud_with_hosts(1, use_hdfs=True, hdfs_version='cdh4', hdfs_name_node='192.168.1.161')
+            h2o_hosts.build_cloud_with_hosts(1, use_hdfs=True, hdfs_version='cdh4', hdfs_name_node='172.16.2.176')
 
     @classmethod
     def tearDownClass(cls):
