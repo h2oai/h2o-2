454a5b15d9b7827d5a61f516908d984c476cdb97
cliffclick <cliffc@acm.org>
2014-09-25 09:39:17 +0200

    Downgrade multicast fails to warns
    We survive nearly all multicast fails with retries

--- src/main/java/water/H2O.java
+++ src/main/java/water/H2O.java
@@ -1197,7 +1197,7 @@ public final class H2O {
         try {
           H2O.CLOUD_DGRAM.send(bb, h2o._key);
         } catch( IOException e ) {
-          Log.err("Multicast Error to "+h2o, e);
+          Log.warn("Multicast Error to "+h2o+e);
         }
       }
     }
