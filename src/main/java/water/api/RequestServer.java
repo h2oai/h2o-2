d0e0dfaed251eaeb9e614c8429b871f8b0c3b4ac
Tomas Nykodym <tomas.nykodym@gmail.com>
2014-10-14 15:39:24 -0700

    Ported TaskStatus page over from tomas-parse branch.

--- src/main/java/water/api/RequestServer.java
+++ src/main/java/water/api/RequestServer.java
@@ -141,6 +141,7 @@ public class RequestServer extends NanoHTTPD {
     Request.addToNavbar(registerRequest(new JStack()),      "Stack Dump",               "Admin");
     Request.addToNavbar(registerRequest(new LogView()),     "Inspect Log",              "Admin");
     Request.addToNavbar(registerRequest(new NetworkTest()), "Network Test",             "Admin");
+    Request.addToNavbar(registerRequest(new TaskStatus()),  "Task status",              "Admin");
     Request.addToNavbar(registerRequest(new WaterMeterPerfbar()),  "Water Meter (Perfbar)",    "Admin");
     Request.addToNavbar(registerRequest(new Shutdown()),    "Shutdown",                 "Admin");
 
