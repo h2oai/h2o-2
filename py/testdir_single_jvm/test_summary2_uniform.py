6829b55fc27f37f863f0174879e7ccc0b8554ff6
Kevin Normoyle <kevin@0xdata.com>
2014-10-20 03:35:57 -0700

    adjust max err bounds

--- py/testdir_single_jvm/test_summary2_uniform.py
+++ py/testdir_single_jvm/test_summary2_uniform.py
@@ -4,23 +4,30 @@ import h2o, h2o_cmd, h2o_hosts, h2o_import as h2i, h2o_util, h2o_print as h2p, h
 
 DO_MEDIAN = False
 MAX_QBINS = 1000
-ROWS = 100000
+ROWS = 500000
 
 def write_syn_dataset(csvPathname, rowCount, colCount, expectedMin, expectedMax, SEED):
     r1 = random.Random(SEED)
     dsf = open(csvPathname, "w+")
 
     expectedRange = (expectedMax - expectedMin)
+    actualMin = None
+    actualMax = None
     for i in range(rowCount):
         rowData = []
         ri = expectedMin + (random.uniform(0,1) * expectedRange)
         for j in range(colCount):
             rowData.append(ri)
+            if not actualMin or ri < actualMin:
+                actualMin = ri
+            if not actualMax or ri > actualMax:
+                actualMax = ri
 
         rowDataCsv = ",".join(map(str,rowData))
         dsf.write(rowDataCsv + "\n")
 
     dsf.close()
+    return (actualMax, actualMin)
 
 class Basic(unittest.TestCase):
     def tearDown(self):
@@ -41,23 +48,22 @@ class Basic(unittest.TestCase):
         h2o.tear_down_cloud()
 
     def test_summary2_uniform(self):
-        h2o.beta_features = True
         SYNDATASETS_DIR = h2o.make_syn_dir()
         tryList = [
             # colname, (min, 25th, 50th, 75th, max)
-            (ROWS, 1, 'x.hex', 1, 20000,        ('C1',  1.10, 5000.0, 10000.0, 15000.0, 20000.00)),
-            (ROWS, 1, 'x.hex', -5000, 0,        ('C1', -5001.00, -3750.0, -2445, -1200.0, 99)),
-            (ROWS, 1, 'x.hex', -100000, 100000, ('C1',  -100001.0, -50000.0, 1613.0, 50000.0, 100000.0)),
-            (ROWS, 1, 'x.hex', -1, 1,           ('C1',  -1.05, -0.48, 0.0087, 0.50, 1.00)),
+            (ROWS, 1, 'x.hex', 0.0, 20000.0,        ['C1',  0, 5000.0, 10000.0, 15000.0, 20000.0]),
+            (ROWS, 1, 'x.hex', -5000.0, 0.0,        ['C1',  -5000.0, -3750.0, -2550.0, -1250.0, 0.0]),
+            (ROWS, 1, 'x.hex', -100000.0, 100000.0, ['C1',  -100000.0, -50000.0, 0.0, 50000.0, 100000.0]),
+            (ROWS, 1, 'x.hex', -1.0, 1.0,           ['C1',  -1.0, -0.50, 0.0, 0.50, 1.0]),
 
-            (ROWS, 1, 'A.hex', 1, 100,          ('C1',   1.05, 26.00, 51.00, 76.00, 100.0)),
-            (ROWS, 1, 'A.hex', -99, 99,         ('C1',  -99, -50.0, 0, 50.00, 99)),
+            (ROWS, 1, 'A.hex', 1.0, 100.0,          ['C1',   1.0, 26.0, 51.0, 76.0, 100.0]),
+            (ROWS, 1, 'A.hex', -99.0, 99.0,         ['C1',  -99.0, -50.0, 0.0, 50.0, 99.0]),
 
-            (ROWS, 1, 'B.hex', 1, 10000,        ('C1',   1.05, 2501.00, 5001.00, 7501.00, 10000.00)),
-            (ROWS, 1, 'B.hex', -100, 100,       ('C1',  -100.10, -50.0, 0.85, 51.7, 100,00)),
+            (ROWS, 1, 'B.hex', 1.0, 10000.0,        ['C1',   1.0, 2501.0, 5001.0, 7501.0, 10000.0]),
+            (ROWS, 1, 'B.hex', -100.0, 100.0,       ['C1',  -100.0, -50.0, 0.0, 50.0, 100.0]),
 
-            (ROWS, 1, 'C.hex', 1, 100000,       ('C1',   1.05, 25002.00, 50002.00, 75002.00, 100000.00)),
-            (ROWS, 1, 'C.hex', -101, 101,       ('C1',  -100.10, -50.45, -1.18, 49.28, 100.00)),
+            (ROWS, 1, 'C.hex', 1.0, 100000.0,       ['C1',   1.0, 25001.0, 50001.0, 75001.0, 100000.0]),
+            (ROWS, 1, 'C.hex', -100.0, 100.0,       ['C1',  -100.0, -50.0, 0.0, 50.0, 100.0]),
         ]
 
         timeoutSecs = 10
@@ -68,11 +74,6 @@ class Basic(unittest.TestCase):
         x = 0
         timeoutSecs = 60
         for (rowCount, colCount, hex_key, expectedMin, expectedMax, expected) in tryList:
-            # max error = half the bin size?
-        
-            maxDelta = ((expectedMax - expectedMin)/20.0) / 2.0
-            # add 5% for fp errors?
-            maxDelta = 1.05 * maxDelta
 
             SEEDPERFILE = random.randint(0, sys.maxint)
             x += 1
@@ -81,7 +82,22 @@ class Basic(unittest.TestCase):
             csvPathname = SYNDATASETS_DIR + '/' + csvFilename
 
             print "Creating random", csvPathname
-            write_syn_dataset(csvPathname, rowCount, colCount, expectedMin, expectedMax, SEEDPERFILE)
+            (actualMax, actualMin) = write_syn_dataset(csvPathname, rowCount, colCount, expectedMin, expectedMax, SEEDPERFILE)
+            # adjust the min/max depending on what the min/max actually was!
+            # the expected 25%/50%/75% will still be off
+            expected[1] = actualMin
+            expected[5] = actualMax
+
+            # max error = half the bin size?
+            # use this for comparing to sklearn/sort
+            expectedRange = expectedMax - expectedMin
+            # because of floor and ceil effects due we potentially lose 2 bins (worst case)
+            # the extra bin for the max value, is an extra bin..ignore
+            expectedBin = expectedRange/(MAX_QBINS-2)
+            # maxDelta = 0.5 * expectedBin # should we have some fuzz for fp?
+            maxDelta = expectedBin
+            maxDelta = 1.2 * maxDelta # fuzz?
+
             csvPathnameFull = h2i.find_folder_and_filename(None, csvPathname, returnFullPath=True)
             parseResult = h2i.import_parse(path=csvPathname, schema='put', hex_key=hex_key, timeoutSecs=30, doSummary=False)
             print "Parse result['destination_key']:", parseResult['destination_key']
@@ -124,9 +140,9 @@ class Basic(unittest.TestCase):
 
             zeros = stats['zeros']
             mins = stats['mins']
-            h2o_util.assertApproxEqual(mins[0], expected[1], tol=maxDelta, msg='min is not approx. expected')
+            h2o_util.assertApproxEqual(mins[0], expected[1], rel=.00001, msg='min is not approx. expected')
             maxs = stats['maxs']
-            h2o_util.assertApproxEqual(maxs[0], expected[5], tol=maxDelta, msg='max is not approx. expected')
+            h2o_util.assertApproxEqual(maxs[0], expected[5], rel=.00001, msg='max is not approx. expected')
 
             pct = stats['pct']
             # the thresholds h2o used, should match what we expected
@@ -170,6 +186,7 @@ class Basic(unittest.TestCase):
 
             trial += 1
 
+
             # don't check if colname is empty..means it's a string and scipy doesn't parse right?
             if colname!='':
                 # don't do for enums
@@ -182,6 +199,7 @@ class Basic(unittest.TestCase):
                     h2oSummary2=pctile[5 if DO_MEDIAN else 10], 
                     h2oQuantilesApprox=qresult_single,
                     h2oQuantilesExact=qresult,
+                    h2oSummary2MaxErr=maxDelta,
                     )
 
             h2o.nodes[0].remove_all_keys()
