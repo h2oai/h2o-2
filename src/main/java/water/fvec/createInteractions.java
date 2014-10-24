9c9e3378180e17c05e561051f197d72f694cbe2e
Arno Candel <arno.candel@gmail.com>
2014-10-24 00:17:19 -0700

    WIP

--- src/main/java/water/fvec/createInteractions.java
+++ src/main/java/water/fvec/createInteractions.java
@@ -2,13 +2,9 @@ package water.fvec;
 
 import hex.Interaction;
 import jsr166y.CountedCompleter;
-import water.H2O;
-import water.Key;
-import water.MRTask2;
+import water.*;
 import water.util.Log;
-import static water.util.Utils.domainUnion;
-
-import java.util.HashMap;
+import water.util.Utils;
 import java.util.Random;
 
 /**
@@ -17,86 +13,73 @@ import java.util.Random;
 public class createInteractions extends H2O.H2OCountedCompleter {
 
   public createInteractions(Interaction ci) { this(ci, null); }
+  public createInteractions(Interaction ci, Key job) { super(null); _job = job; _ci = ci; }
 
-  public createInteractions(Interaction ci, Key job) {
-    super(null);
-    _job=job;
-    _ci = ci;
-  }
   final private Interaction _ci;
 
   private Frame _out;
   final private Key _job;
 
-  @Override public void compute2() {
-
+  @Override
+  public void compute2() {
     // base frame - same as source
     _out = new Frame(Key.make(_ci.target), _ci.source.names(), _ci.source.vecs());
 
-    String[] union = null;
-    int A = _ci.factors[0];
-    String[] dA = _out.vecs()[A]._domain;
-    int len = dA.length;
-    String name = _out._names[A];
+    // Pass 1: compute unique domains of all interaction features
+//    for () {
+    String[] domain = new createInteractionDomain(_ci, 0, 1).doAll(_out)._domain;
+    Vec interaction = _out.anyVec().makeZero(domain);
+    _out.add("this_interaction", interaction);
 
-    for( int i=1; i< _ci.factors.length; i++ ) {
-      int B = _ci.factors[i];
-      String[] dB = _out.vecs()[B]._domain;
-      name += _out._names[B];
+      // Pass 2: create new vector based on the domain
+    new createInteractionDomain(_ci, 0, 1).doAll(_out); //FIXME: call for pass 2
+//    }
 
-      len *= dB.length;
-      if (len > water.parser.Enum.MAX_ENUM_SIZE) {
-        Log.info("Skipping interaction term " + name + ": too many factors.");
-        continue;
-      }
 
-        int count = 0;
-        for (String sa : dA)
-          for (String sb : dB)
-            union[count++] = sa + "." + sb;
-        Vec nv = _out.anyVec().makeZero(union);
-        _out.add(name, nv);
-//        new populateInteractionVec(_ci, i, j).doAll(_out);
-      }
-    }
     _out.delete_and_lock(_job);
+
     tryComplete();
 
     // Needs to create _domain and enum chunk values
   }
 
-  @Override public void onCompletion(CountedCompleter caller){
+  @Override
+  public void onCompletion(CountedCompleter caller) {
     _out.update(_job);
     _out.unlock(_job);
   }
 
-  // Create domain
-  private static class createInteractionDomain extends MRTask2<createInteractionDomain> {
-    // INPUT
-    final private Interaction _ci;
-    private final  HashMap<String, Integer> _hm = new HashMap<String, Integer>();
 
-//    private NonBlockingHashMap<Long,Long> _nbhm;
-    int _i;
-    int _j;
 
-    // OUTPUT
-    public String[] _domain;
 
-    public createInteractionDomain(Interaction ci, int i, int j){
-      _i = i;
-      _j = j;
-      _ci = ci;
-      _domain = new String[]{"yes","no"};
-      Log.info("Making interaction between " + _ci.source._names[_ci.factors[i]] + " and " + _ci.source._names[_ci.factors[j]]);
-    }
+// Create domain
+private static class createInteractionDomain extends MRTask2<createInteractionDomain> {
+  // INPUT
+  final private Interaction _ci;
+  private final Utils.IcedHashMap<Utils.IcedLong, Utils.IcedLong> _hm = new Utils.IcedHashMap<Utils.IcedLong, Utils.IcedLong>();
 
-    @Override
-    public void map (Chunk[]cs){
-      _hm.put("Hi", 1);
-//      _nbhm = new NonBlockingHashMap<Long, Long>();
-      final int newCol = cs.length-1;
-      for (int r = 0; r < cs[0]._len; r++) {
+  int _i;
+  int _j;
+
+  // OUTPUT
+  public String[] _domain;
+
+  public createInteractionDomain(Interaction ci, int i, int j) {
+    _i = i;
+    _j = j;
+    _ci = ci;
+    String[] A = _ci.source.domains()[_ci.factors[i]];
+    String[] B = _ci.source.domains()[_ci.factors[j]];
+
+    _domain = new String[]{ A[0]+"_"+B[0], A[1]+"_"+B[0], A[0]+"_"+B[1], A[1]+"_"+B[1]};
+
+    Log.info("Making interaction between " + _ci.source._names[_ci.factors[i]] + " and " + _ci.source._names[_ci.factors[j]]);
+  }
+
+  @Override
+  public void map(Chunk[] cs) {
+    final int newCol = cs.length - 1;
+    for (int r = 0; r < cs[0]._len; r++) {
 //        if (cs[_i].isNA0(r) && cs[_j].isNA0(r)) {
 //          cs[newCol].setNA0(r);
 //        }
@@ -112,16 +95,16 @@ public class createInteractions extends H2O.H2OCountedCompleter {
 //          Long val = _nbhm.get(key);
 //          _nbhm.put(key, val + 1);
 //        }
-        cs[newCol].set0(r, Math.abs(new Random().nextLong())%2);
-      }
-    }
-
-    @Override
-    public void reduce(createInteractionDomain mrt) {
-//      assert _nbhm != null;
-//      assert mrt._nbhm != null;
-      // TODO: merge the two hashmaps
+      cs[newCol].set0(r, Math.abs(new Random().nextLong()) % 4);
     }
   }
+
+  @Override
+  public void reduce(createInteractionDomain mrt) {
+//      assert _nbhm != null;
+//      assert mrt._nbhm != null;
+    // TODO: merge the two hashmaps
+  }
+}
 
 }
