951089810437a0cdab593aa0ad6fba315be5ecb0
Arno Candel <arno.candel@gmail.com>
2014-10-25 11:48:08 -0700

    PUB-1067: Working for higher-order features. TODO: add self-interaction (order 0) to limit the number of factors

--- src/main/java/water/fvec/createInteractions.java
+++ src/main/java/water/fvec/createInteractions.java
@@ -2,9 +2,11 @@ package water.fvec;
 
 import hex.Interaction;
 import jsr166y.CountedCompleter;
+import org.apache.commons.math3.stat.inference.TestUtils;
 import water.*;
 import water.util.Log;
 import water.util.Utils;
+import static water.util.Utils.IcedLong;
 
 import java.util.*;
 
@@ -18,95 +20,46 @@ public class createInteractions extends H2O.H2OCountedCompleter {
 
   final private Interaction _ci;
 
+  static final private int _missing = Integer.MIN_VALUE; //marker for missing factor level
+  static final private String _other = "other"; // name for lost factor levels
+
   private Frame _out;
   final private Key _job;
 
-  @Override
-  public void compute2() {
-    // base frame - same as source
-    _out = new Frame(Key.make(_ci.target), _ci.source.names(), _ci.source.vecs());
+  private Map<Long, Long> _sortedMap = null;
 
-//    for (int i=1; i<_ci.factors.length; ++i) {
-      int idx1 = _ci.factors[0];
-      int idx2 = _ci.factors[1];
-
-      // Pass 1: compute unique domains of all interaction features
-      createInteractionDomain pass1 = new createInteractionDomain(_ci, idx1, idx2).doAll(_out);
-      String[] domain = pass1._domain;
-
-      // Create a new Vec based on the domain
-      Vec interaction = _out.anyVec().makeZero(domain);
-      String name = _ci.source._names[idx1] + "_" + _ci.source._names[idx2];
-      _out.add(name, interaction);
-
-      // Create array of enum pairs, in the same (sorted) order as in the _domain map -> for linear lookup
-      long[] keys = new long[pass1._tm.size()];
-      int pos = 0;
-      for (long k : pass1._tm.keySet()) {
-        keys[pos++] = k;
+  private static Map<Long, Long> mySort(Map<IcedLong, IcedLong> unsortMap) {
+    List<Map.Entry<IcedLong, IcedLong>> list = new LinkedList<Map.Entry<IcedLong, IcedLong>>(unsortMap.entrySet());
+    // Sorting the list based on values
+    Collections.sort(list, new Comparator<Map.Entry<IcedLong, IcedLong>>() {
+      public int compare(Map.Entry<IcedLong, IcedLong> o1, Map.Entry<IcedLong, IcedLong> o2) {
+        return ((Long)o2.getValue()._val).compareTo(o1.getValue()._val);
       }
-
-      // Pass 2: fill Vec values
-      new fillInteractionEnums(idx1, idx2, keys).doAll(_out);
-//    }
-
-    _out.delete_and_lock(_job);
-    tryComplete();
-  }
-
-  @Override
-  public void onCompletion(CountedCompleter caller) {
-    _out.update(_job);
-    _out.unlock(_job);
-  }
-
-
-
-
-// Create interaction domain
-private static class createInteractionDomain extends MRTask2<createInteractionDomain> {
-  // INPUT
-  final private Interaction _ci;
-  final private int _i;
-  final private int _j;
-
-  // OUTPUT
-  public String[] _domain;
-
-  // Helper
-  private Utils.IcedHashMap<Utils.IcedLong, Utils.IcedLong> _hm = null;
-  transient private TreeMap<Long, Long> _tm = null;
-
-
-  public createInteractionDomain(Interaction ci, int i, int j) {
-    _ci = ci; _i = i; _j = j;
-    Log.info("Creating interaction features between " + _ci.source._names[_i] + " and " + _ci.source._names[_j]);
-  }
-
-  @Override
-  protected void postGlobal() {
-//    Log.info("Collected hash table");
-//    Log.info(java.util.Arrays.deepToString(_hm.entrySet().toArray()));
-
-    Log.info("Combining domains with " +
-            _fr.domains()[_i].length + " x " + _fr.domains()[_j].length + " = " +
-            (_fr.domains()[_i].length * _fr.domains()[_j].length) + " factors");
-
-    // sort hash map
-    _tm = new TreeMap(Collections.reverseOrder());
-    Iterator it = _hm.entrySet().iterator();
-    while (it.hasNext()) {
-      Map.Entry kv = (Map.Entry) it.next();
-      long ab = ((Utils.IcedLong) kv.getKey())._val;
-      long count = ((Utils.IcedLong) kv.getValue())._val;
-      _tm.put(ab, count);
+    });
+    // Maintaining insertion order with the help of LinkedList
+    Map sortedMap = new LinkedHashMap<Long, Long>();
+    for (Map.Entry<IcedLong, IcedLong> entry : list) {
+      sortedMap.put(entry.getKey()._val, entry.getValue()._val);
     }
+    return sortedMap;
+  }
+
+  protected String[] makeDomain(Map<IcedLong, IcedLong> unsortedMap, int _i, int _j) {
+    String[] _domain;
+    Frame _fr = _out;
+//    Log.info("Collected hash table");
+//    Log.info(java.util.Arrays.deepToString(unsortedMap.entrySet().toArray()));
+
+//    Log.info("Interaction between " + _fr.domains()[_i].length + " and " + _fr.domains()[_j].length + " factor levels => " +
+//            ((long)_fr.domains()[_i].length * (long)_fr.domains()[_j].length) + " possible factors.");
+
+    _sortedMap = mySort(unsortedMap);
 
     // create domain of the most frequent unique factors
     long factorCount = 0;
-    Log.info("Found " + _hm.size() + " unique interaction factors.");
-    _domain = new String[_hm.size()];
-    Iterator it2 = _tm.entrySet().iterator();
+//    Log.info("Found " + _sortedMap.size() + " unique interaction factors (out of " + ((long)_fr.domains()[_i].length * (long)_fr.domains()[_j].length) + ").");
+    _domain = new String[_sortedMap.size()]; //TODO: use ArrayList here, then convert to array
+    Iterator it2 = _sortedMap.entrySet().iterator();
     int d = 0;
     while (it2.hasNext()) {
       Map.Entry kv = (Map.Entry)it2.next();
@@ -117,62 +70,140 @@ private static class createInteractionDomain extends MRTask2<createInteractionDo
         // extract the two original factor enums
         int a = (int)(ab >> 32);
         int b = (int)ab;
-        String feature = _fr.domains()[_i][a] + "_" + _fr.domains()[_j][b];
+        assert ab == (((long)a << 32) | (b & 0xFFFFFFFFL));
+
+        final String fA = a != _missing ? _fr.domains()[_i][a] : "NA";
+        final String fB = b != _missing ? _fr.domains()[_j][b] : "NA";
+        final String feature = fA + "_" + fB;
 //        Log.info("Adding interaction feature " + feature + ", occurrence count: " + count);
 //        Log.info("Total number of interaction factors so far: " + factorCount);
         _domain[d++] = feature;
       } else break;
     }
-    if (d < _domain.length) {
-      String[] copy = new String[d];
+    if (d < _sortedMap.size()) {
+      Log.info("Truncated map to " + _sortedMap.size() + " elements.");
+      String[] copy = new String[d+1];
       System.arraycopy(_domain, 0, copy, 0, d);
+      copy[d] = _other;
       _domain = copy;
 
-      TreeMap tm = new TreeMap();
-      it2 = _tm.entrySet().iterator();
+      Map tm = new LinkedHashMap<Long, Long>();
+      it2 = _sortedMap.entrySet().iterator();
       while (--d >= 0) {
         Map.Entry kv = (Map.Entry) it2.next();
         tm.put(kv.getKey(), kv.getValue());
       }
-      _tm = tm;
-//      Log.info("Truncated tree map to " + _tm.size() + " elements.");
+      _sortedMap = tm;
     }
-//    Log.info("Final domain: " + Arrays.deepToString(_domain));
+//    Log.info("Created domain: " + Arrays.deepToString(_domain));
+    return _domain;
   }
 
 
   @Override
+  public void compute2() {
+    // base frame - same as source
+    DKV.remove(Key.make(_ci.target));//shouldn't be needed, but this avoids missing chunk issues
+    _out = new Frame(Key.make(_ci.target), _ci.source.names().clone(), _ci.source.vecs().clone());
+    _out.delete_and_lock(_job);
+
+    int idx1 = _ci.factors[0];
+    Vec tmp = null;
+    for (int i=1; i<_ci.factors.length; ++i) {
+      if (i>1) {
+        idx1 = _out.find(tmp);
+        assert idx1 >= 0;
+      }
+      int idx2 = _ci.factors[i];
+//      Log.info("Combining columns " + idx1 + " and " + idx2);
+
+      // Pass 1: compute unique domains of all interaction features
+      createInteractionDomain pass1 = new createInteractionDomain(idx1, idx2).doAll(_out);
+
+      // Create a new Vec based on the domain
+      _out.add(_out._names[idx1] + "_" + _out._names[idx2], _out.anyVec().makeZero(makeDomain(pass1._unsortedMap, idx1, idx2)));
+      _out.update(_job);
+
+      // Create array of enum pairs, in the same (sorted) order as in the _domain map -> for linear lookup
+      // Note: "other" is not mapped in keys, so keys.length can be 1 less than domain.length
+      long[] keys = new long[_sortedMap.size()];
+      int pos = 0;
+      for (long k : _sortedMap.keySet()) {
+        keys[pos++] = k;
+      }
+      assert(_out.lastVec().domain().length == keys.length || _out.lastVec().domain().length == keys.length + 1); // domain might contain _other
+
+      // Pass 2: fill Vec values
+      new fillInteractionEnums(idx1, idx2, keys).doAll(_out);
+      tmp = _out.lastVec();
+
+      // remove temporary vec
+      if (i>1) {
+        final int idx = _out.vecs().length-2; //second-last vec
+//        Log.info("Removing column " + _out._names[idx]);
+        _out.remove(idx);
+        _out.update(_job);
+      }
+    }
+    tryComplete();
+  }
+
+  @Override
+  public void onCompletion(CountedCompleter caller) {
+    _out.update(_job);
+    Log.info("Created interaction feature " + _out.names()[_out.names().length-1]
+            + " (order: " + _ci.factors.length + ") with " + _out.lastVec().domain().length + " factor levels.");
+    _out.unlock(_job);
+  }
+
+
+
+
+// Create interaction domain
+private static class createInteractionDomain extends MRTask2<createInteractionDomain> {
+  // INPUT
+  final private int _i;
+  final private int _j;
+
+  // OUTPUT
+  private Utils.IcedHashMap<IcedLong, IcedLong> _unsortedMap = null;
+
+  public createInteractionDomain(int i, int j) { _i = i; _j = j; }
+
+  @Override
   public void map(Chunk[] cs) {
-    _hm = new Utils.IcedHashMap<Utils.IcedLong, Utils.IcedLong>();
+    _unsortedMap = new Utils.IcedHashMap<IcedLong, IcedLong>();
     // find unique interaction domain
     for (int r = 0; r < cs[0]._len; r++) {
-      long a = cs[_i].isNA0(r) ? Integer.MIN_VALUE : cs[_i].at80(r);
-      long b = cs[_j].isNA0(r) ? Integer.MIN_VALUE : cs[_j].at80(r);
-
-      // enum levels must fit into int
-      assert((int)a == a);
-      assert((int)b == b);
+      int a = cs[_i].isNA0(r) ? _missing : (int)cs[_i].at80(r);
+      int b = cs[_j].isNA0(r) ? _missing : (int)cs[_j].at80(r);
 
       // key: combine both ints into a long
-      long ab = (a << 32) | (b & 0xFFFFFFFL);
+      long ab = ((long)a << 32) | (b & 0xFFFFFFFFL);
+      assert a == (int)(ab >> 32);
+      assert b == (int)ab;
 
       // add key to hash map, and count occurrences (for pruning)
-      Utils.IcedLong AB = new Utils.IcedLong(ab);
-      if (_hm.containsKey(AB)) {
-        _hm.put(AB, new Utils.IcedLong(_hm.get(AB)._val + 1));
+      IcedLong AB = new IcedLong(ab);
+      if (_unsortedMap.containsKey(AB)) {
+        _unsortedMap.put(AB, new IcedLong(_unsortedMap.get(AB)._val + 1));
       } else {
-        _hm.put(AB, new Utils.IcedLong(1));
+        _unsortedMap.put(AB, new IcedLong(1));
       }
     }
   }
 
   @Override
   public void reduce(createInteractionDomain mrt) {
-    // get all entries from mrt._hm, and put them into this._hm
-    mrt._hm.putAll(_hm);
-    mrt._hm.clear();
+    assert(mrt._unsortedMap != null);
+    assert(_unsortedMap != null);
+    Utils.IcedHashMap third = new Utils.IcedHashMap<IcedLong, IcedLong>();
+    third.putAll(mrt._unsortedMap);
+    third.putAll(_unsortedMap);
+    _unsortedMap = third;
+    mrt._unsortedMap = null;
 //    Log.info("Merged hash tables");
-//    Log.info(java.util.Arrays.deepToString(_hm.entrySet().toArray()));
+//    Log.info(java.util.Arrays.deepToString(_unsortedMap.entrySet().toArray()));
   }
 }
 
@@ -191,19 +222,13 @@ private static class createInteractionDomain extends MRTask2<createInteractionDo
     public void map(Chunk[] cs) {
       // find unique interaction domain
       for (int r = 0; r < cs[0]._len; r++) {
-        final long a = cs[_i].isNA0(r) ? Integer.MIN_VALUE : cs[_i].at80(r);
-        final long b = cs[_j].isNA0(r) ? Integer.MIN_VALUE : cs[_j].at80(r);
-
-        // enum levels must fit into int
-        assert((int)a == a);
-        assert((int)b == b);
+        final int a = cs[_i].isNA0(r) ? _missing : (int)cs[_i].at80(r);
+        final int b = cs[_j].isNA0(r) ? _missing : (int)cs[_j].at80(r);
 
         // key: combine both ints into a long
-        final long ab = (a << 32) | (b & 0xFFFFFFFL);
-
-//        Log.info("Looking up position of " + ab + " in domain");
-//        Log.info("This long should be in the keys:");
-//        Log.info(Arrays.toString(_keys));
+        final long ab = ((long)a << 32) | (b & 0xFFFFFFFFL);
+        assert a == (int)(ab >> 32);
+        assert b == (int)ab;
 
         // linear search in sorted array of factor levels (descending by occurrence), should be fastest for most small domains
         int level = -1;
@@ -213,15 +238,14 @@ private static class createInteractionDomain extends MRTask2<createInteractionDo
             break;
           }
         }
+        if (level == -1) {
+          level = _fr.lastVec().domain().length-1;
+          assert _fr.lastVec().domain()[level] == _other;
+        }
 
-        if (level >= 0)
-          cs[cs.length-1].set0(r, level);
-        else
-          cs[cs.length-1].setNA0(r);
+        cs[cs.length - 1].set0(r, level);
 
       }
-//    Log.info("Collected hash table");
-//    Log.info(java.util.Arrays.deepToString(_hm.entrySet().toArray()));
     }
 
   }
