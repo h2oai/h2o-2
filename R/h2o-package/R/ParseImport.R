b84f9924287c0cc2eacc32fa004e559948acc82c
Arno Candel <arno.candel@gmail.com>
2014-10-30 18:19:12 -0700

    PUB-1067: Use destination_key instead of target.

--- R/h2o-package/R/ParseImport.R
+++ R/h2o-package/R/ParseImport.R
@@ -145,14 +145,15 @@ h2o.interaction <- function(data, key=NULL, factors, pairwise, max_factors, min_
 
   if(!is.numeric(factors)) factors <- match(factors,colnames(data))
   if(!is.numeric(factors)) stop("factors must be a numeric value")
+  if(is.null(factors)) stop("factors not found")
   if(max_factors < 1) stop("max_factors cannot be < 1")
   if(!is.numeric(max_factors)) stop("max_factors must be a numeric value")
   if(min_occurrence < 1) stop("min_occurrence cannot be < 1")
   if(!is.numeric(min_occurrence)) stop("min_occurrence must be a numeric value")
 
   factors <- factors - 1 # make 0-based for Java
-  res <- .h2o.__remoteSend(data@h2o, .h2o.__PAGE_Interaction, source = data@key, target = key, factors = factors, pairwise = as.numeric(pairwise), max_factors = max_factors, min_occurrence = min_occurrence)
-  h2o.getFrame(data@h2o, res$target)
+  res <- .h2o.__remoteSend(data@h2o, .h2o.__PAGE_Interaction, source = data@key, destination_key = key, factors = factors, pairwise = as.numeric(pairwise), max_factors = max_factors, min_occurrence = min_occurrence)
+  h2o.getFrame(data@h2o, res$destination_key)
 }
 
 h2o.rebalance <- function(data, chunks, key) {
