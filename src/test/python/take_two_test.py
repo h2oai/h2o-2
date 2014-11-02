87e22f3d2b21cba3c84c5e2a1a48cc71b75e6f20
Radu Munteanu <radu@0xdata.com>
2014-11-01 21:52:34 -0700

    template python tests

---
+++ src/test/python/take_two_test.py
@@ -0,0 +1,42 @@
+import unittest
+from proboscis.asserts import assert_equal
+from proboscis.asserts import assert_false
+from proboscis.asserts import assert_raises
+from proboscis.asserts import assert_true
+from proboscis import after_class
+from proboscis import before_class
+from proboscis import SkipTest
+from proboscis import test
+
+import random
+import types
+import unittest
+import sys, pprint
+sys.path.extend(['.','..','py'])
+
+@test(groups=["acceptance"])
+class TestModelManagement(object):
+    @test(groups=["acceptance"])
+    def testD(self):
+        self.a = "a"
+        assert_equal(self.a, "a")
+
+    @test(groups=["acceptance"], depends_on=[testD])
+    def testE(self):
+        self.b = "b"
+        assert_equal ( self.b, "b" )
+
+    @test(groups=["acceptance"], depends_on=[testE])
+    def testF(self):
+        self.c = "c"
+        assert_equal ( self.c, "c" )
+
+
+## ----------------- proboscis boiler plate hook -------------------------
+# no reason to modify anything below
+def run_tests():
+    from proboscis import TestProgram
+    TestProgram().run_and_exit()
+
+if __name__ =='__main__':
+    run_tests()
