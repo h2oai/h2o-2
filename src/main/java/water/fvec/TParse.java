bfda58a29e982df49b1028e474dff44afa913518
SpencerA <spnrpa@gmail.com>
2014-11-07 02:08:56 -0800

    initial on Text Parser

---
+++ src/main/java/water/fvec/TParse.java
@@ -0,0 +1,7 @@
+package water.fvec;
+
+/**
+ * Created by spencer on 11/6/14.
+ */
+public class TParse {
+}
---
+++ src/main/java/water/parser/Doc.java
@@ -0,0 +1,7 @@
+package water.parser;
+
+/**
+ * Created by spencer on 11/6/14.
+ */
+public class Doc {
+}
