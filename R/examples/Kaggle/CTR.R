d5cb1982c56982d7c278e70b6825aeab011700d4
Arno Candel <arno.candel@gmail.com>
2014-11-23 16:41:10 -0800

    Update CTR starter script for new dataset.

--- R/examples/Kaggle/CTR.R
+++ R/examples/Kaggle/CTR.R
@@ -4,7 +4,7 @@ sink("CTR.log", split = T)
 # START
 #if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
 #if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
-#install.packages("h2o", repos=(c("http://s3.amazonaws.com/h2o-release/h2o/master/1574/R", getOption("repos")))) #choose a build here
+#install.packages("h2o", repos=(c("http://s3.amazonaws.com/h2o-release/h2o/master/1597/R", getOption("repos")))) #choose a build here
 # END
 
 # Fetch the latest nightly build using Jo-fai Chow's package
@@ -14,9 +14,10 @@ sink("CTR.log", split = T)
 library(h2o)
 library(stringr)
 
-## Connect to H2O server (On server(s), run 'java -Xmx32g -ea -jar h2o.jar -port 53322 -name CTR -data_max_factor_levels 100000000' first)
+## Connect to H2O server (On server(s), run 'java -jar h2o.jar -Xmx32G -port 53322 -name CTR -data_max_factor_levels 100000000' first)
 ## Go to http://server:53322/ to check Jobs/Data/Models etc.
-h2oServer <- h2o.init(ip="mr-0xd2", port = 53322)
+h2oServer <- h2o.init(ip="mr-0xd1", port = 53322)
+#h2oServer <- h2o.init(nthreads=-1, max_mem_size='32g')
 
 ## Helper function for feature engineering
 h2o.addNewFeatures <- function(frame, timecol, intcols, factorcols, key) {
@@ -66,8 +67,8 @@ myseed=5816985749037550201
 path <- "/home/arno/h2o-kaggle/ctr/"
 
 path_submission <- paste0(path,"./data/sampleSubmission.csv")
-path_train <- paste0(path,"./data/train_rev2.csv")
-path_test <- paste0(path,"./data/test_rev2.csv")
+path_train <- paste0(path,"./data/train")
+path_test <- paste0(path,"./data/test")
 
 cat("\nReading data.")
 train_hex <- h2o.importFile(h2oServer, path = path_train)
@@ -76,26 +77,28 @@ test_hex <- h2o.importFile(h2oServer, path = path_test)
 ## Feature engineering
 
 ## all columns
-#intcols <- c("C1","banner_pos","device_type","device_conn_type","C17","C18","C19","C20","C21","C22","C23","C24")
-#factorcols <- c("site_id","site_domain","site_category","app_id","app_domain","app_category","devide_id","device_ip","device_os","device_make","device_model")
+#intcols <- c("C1","banner_pos","site_category","device_type","device_conn_type","C14","C15","C16","C17","C18","C19","C20","C21")
+#factorcols <- c("site_id","site_domain","site_category","app_id","app_domain","app_category","device_id","device_ip","device_model")
 
-## pick a smaller subset for simplicity
-intcols <- c("C1","C17","C18","C19","C20","C21","C22","C23","C24")
-factorcols <- c("site_id","site_domain","site_category","app_id","app_domain","app_category","device_os","device_make","device_model")
+intcols <- c("C1","banner_pos","site_category","device_type","device_conn_type","C14","C15","C16","C17","C18","C19","C20","C21")
+factorcols <- c()
 
 cat("\nAdding features")
-train_hex <- h2o.addNewFeatures(train_hex, "hour", intcols, factorcols, "train_hex")
-test_hex <- h2o.addNewFeatures(test_hex, "hour", intcols, factorcols, "test_hex")
+train_hex <- h2o.addNewFeatures(train_hex, "hour", intcols, factorcols, "train.hex")
+test_hex <- h2o.addNewFeatures(test_hex, "hour", intcols, factorcols, "test.hex")
 
 ## Split into train/validation based on training days (first 9 days: train, last day: test)
 cat("\nSplitting into train/validation")
-train <- h2o.assign(train_hex[train_hex$day<10,], 'train')
-valid <- h2o.assign(train_hex[train_hex$day==10,], 'valid')
+train <- h2o.assign(train_hex[train_hex$day<30,], 'train')
+valid <- h2o.assign(train_hex[train_hex$day==30,], 'valid')
 h2o.rm(h2oServer, grep(pattern = "Last.value", x = h2o.ls(h2oServer)$Key, value = TRUE))
 
-cat("\nTraining H2O model on training/validation splits days 1-9/10")
-## Note: This could be grid search models, after which you would obtain the best model parameters via cvmodel@model[[1]]@model$params
-cvmodel <- h2o.randomForest(type="BigData", data=train, validation=valid, x=c(3:ncol(train)), y=2, ntree=10, depth=10, seed=myseed)
+cat("\nTraining H2O model on training/validation splits days <30/30")
+## Note: This could be grid search models, after which you would obtain the best model with model <- cvmodel@model[[1]]
+#cvmodel <- h2o.randomForest(data=train, validation=valid, x=c(3:ncol(train)), y=2,
+                            type="BigData", ntree=50, depth=20, seed=myseed)
+cvmodel <- h2o.gbm(data=train, validation=valid, x=c(3:ncol(train)), y=2,
+                            n.tree=100, interaction.depth=10)
 #cvmodel <- h2o.deeplearning(data=train, validation=valid, x=c(3:ncol(train)), y=2,
 #                            hidden=c(50,50), max_categorical_features=100000, train_samples_per_iteration=10000, score_validation_samples=10000)
 
@@ -110,11 +113,14 @@ cat("\nLogLoss on validation data:", h2o.logLoss(valid_preds, valid_resp))
 usefullmodel = T #Set to TRUE for higher accuracy 
 if (usefullmodel) {
   cat("\nTraining H2O model on all the training data.")
-  fullmodel <- h2o.randomForest(data=train_hex, x=c(3:ncol(train)), y=2, 
-                                seed=cvmodel@model$params$seed,
-                                type=cvmodel@model$params$type,
-                                ntree=cvmodel@model$params$ntree,
-                                depth=cvmodel@model$params$depth)
+#   fullmodel <- h2o.randomForest(data=train_hex, x=c(3:ncol(train)), y=2,
+#                                 type=cvmodel@model$params$type,
+#                                 ntree=cvmodel@model$params$ntree,
+#                                 depth=cvmodel@model$params$depth,
+#                                 seed=cvmodel@model$params$seed)
+  fullmodel <- h2o.gbm(data=train_hex, x=c(3:ncol(train)), y=2,
+                                n.tree=cvmodel@model$params$n.tree,
+                                interaction.depth=cvmodel@model$params$interaction.depth)
 #  fullmodel <- h2o.deeplearning(data=train_hex, x=c(3:ncol(train)), y=2,
 #                                hidden=cvmodel@model$params$hidden,
 #                                max_categorical_features=cvmodel@model$params$max_categorical_features,
