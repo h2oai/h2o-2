3b2c51c801e2683c21daae17d86da1d6257df8eb
Arno Candel <arno.candel@gmail.com>
2014-11-24 07:50:49 -0800

    Increase suggested max heap memory to 64G.

--- R/examples/Kaggle/CTR.R
+++ R/examples/Kaggle/CTR.R
@@ -14,10 +14,10 @@ sink("CTR.log", split = T)
 library(h2o)
 library(stringr)
 
-## Connect to H2O server (On server(s), run 'java -Xmx32G -jar h2o.jar -port 53322 -name CTR -data_max_factor_levels 100000000' first)
+## Connect to H2O server (On server(s), run 'java -Xmx64G -jar h2o.jar -port 53322 -name CTR -data_max_factor_levels 100000000' first)
 ## Go to http://server:53322/ to check Jobs/Data/Models etc.
 h2oServer <- h2o.init(ip="mr-0xd1", port = 53322)
-#h2oServer <- h2o.init(nthreads=-1, max_mem_size='32g')
+#h2oServer <- h2o.init(nthreads=-1, max_mem_size='64g')
 
 ## Helper function for feature engineering
 h2o.addNewFeatures <- function(frame, timecol, intcols, factorcols, key) {
