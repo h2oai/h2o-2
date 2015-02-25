a4fedfd0d61756b051afd3c5261322ff50ff0952
Amy Wang <amy@0xdata.com>
2015-02-25 13:31:21 -0800

    hex-2020 beta constraints for linear regression

--- R/tests/Utils/glmR.R
+++ R/tests/Utils/glmR.R
@@ -15,3 +15,40 @@ checkGLMModel <- function(myGLM.h2o, myGLM.r) {
   checkEqualsNumeric(sort(myGLM.h2o@model$coefficients), sort(coeff.R), tolerance = 3.8)
   checkEqualsNumeric(myGLM.h2o@model$null.deviance, myGLM.r$nulldev, tolerance = 1.5)
 }
+
+
+
+### Functions to compare model deviance and coefficients
+compare_deviance <- function(h2o_model, glmnet_model){
+  print(paste("Deviance in GLMnet Model : " , deviance(glmnet_model)))
+  print(paste("Deviance in H2O Model    : " , h2o_model@model$deviance))
+  diff = deviance(glmnet_model) - h2o_model@model$deviance
+  if(diff < 2E-2) {
+    return("PASS")
+  } else {
+    return ("Deviance in H2O model doesn't match up to GLMnet!")
+  }  
+}
+
+
+compare_coeff <- function(h2o_model, glmnet_model){
+  ncol = length(glmnet_model$a0)
+  h2o_coeff = h2o_model@model$coefficients
+  raw_glm_coeff = glmnet_model$beta[,ncol]
+  coeffNames = names(h2o_coeff)
+  fun <- function(coeffName) {
+    if(!coeffName=="Intercept"){
+      raw_glm_coeff[coeffName]
+    } else {
+      glmnet_model$a0[[ncol]]
+    }
+  }
+  glmnet_coeff = sapply(coeffNames, fun)
+  diff = abs(glmnet_coeff - h2o_coeff)
+  print(rbind(h2o_coeff, glmnet_coeff))
+  if(all(diff < 2E-2)) {
+    return("PASS")
+  } else {
+    return ("Coefficients in H2O model doesn't match up to GLMnet!")
+  }
+}
---
+++ R/tests/testdir_jira/runit_hex_2020_LR_beta_constraints.R
@@ -0,0 +1,65 @@
+## This test is to check the beta contraint argument for GLM
+## The test will import the prostate data set,
+## runs glm with and without beta contraints which will be checked
+## against glmnet's results.
+
+#setwd("/Users/amy/h2o/R/tests/testdir_jira")
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source('../findNSourceUtils.R')
+
+test.LR.betaConstraints <- function(conn) {
+  
+  Log.info("Importing prostate dataset...")
+  prostate.hex = h2o.importFile(
+    object = conn,system.file("extdata", "prostate.csv", package = "h2o"))
+  
+  Log.info("Create beta constraints frame...")
+  myX =  c("AGE","RACE", "DPROS", "DCAPS", "PSA", "VOL", "GLEASON")
+  lowerbound = rep(-1, times = length(myX))
+  upperbound = rep(1, times = length(myX))
+  starting = rep(-0.008235181, times = length(myX))
+  betaConstraints = data.frame(names = myX, lower_bounds = lowerbound, upper_bounds = upperbound, beta_given= starting)
+  betaConstraints.hex = as.h2o(conn, betaConstraints)
+  prostate.csv = as.data.frame(prostate.hex)
+  
+  ######## Single variable CAPSULE ~ AGE in H2O and then R
+  ## actual coeff for Age without constraints = -.00823
+  Log.info("Run a Linear Regression with CAPSULE ~ AGE with bound beta->[0,1] in H2O...")
+  beta_age = betaConstraints.hex[betaConstraints.hex$names == "AGE"]
+  beta_age$lower_bounds = 0
+  beta_age$upper_bounds = 1
+  lr.h2o = h2o.glm(x = "AGE", y = "CAPSULE", data = prostate.hex, family = "gaussian", alpha = 0, beta_constraints = beta_age, standardize = T)
+  lambda = lr.h2o@model$lambda
+  
+  Log.info("Run a Linear Regression with CAPSULE ~ AGE with bound beta->[0,1] in R...")
+  intercept = rep(0, times = nrow(prostate.hex))
+  xDataFrame = data.frame(AGE = prostate.csv[,"AGE"], Intercept = intercept)
+  xMatrix_age = as.matrix(xDataFrame)
+  lr.R = glmnet(x = xMatrix_age, alpha = 0., lambda = lr.h2o@model$lambda, standardize = T,
+                y = prostate.csv[,"CAPSULE"], family = "gaussian", lower.limits = 0., upper.limits = 1.)
+  
+  compare_deviance(lr.h2o, lr.R)
+  compare_coeff(lr.h2o, lr.R)
+
+  #### shift AGE coefficient by 0.002
+  Log.info("Test Beta Constraints with negative upper bound in H2O...")
+  lower_bound = -0.007
+  upper_bound = -0.002
+  beta_age$lower_bounds = lower_bound
+  beta_age$upper_bounds = upper_bound
+  lr_negativeUpper.h2o = h2o.glm(x = "AGE", y = "CAPSULE", data = prostate.hex, family = "gaussian", alpha = 0, beta_constraints = beta_age, standardize = T)
+  
+  Log.info("Shift AGE column to reflect negative upperbound...")
+  xDataFrame = data.frame(AGE = prostate.csv[,"AGE"]*(1+upper_bound), Intercept = intercept)
+  xMatrix_age = as.matrix(xDataFrame)
+  lr_negativeUpper.R = glmnet(x = xMatrix_age, alpha = 0., lambda = lr.h2o@model$lambda, standardize = T,
+                              y = prostate.csv[,"CAPSULE"], family = "gaussian", lower.limits = lower_bound-upper_bound, upper.limits = 0.)
+  
+  compare_deviance(lr_negativeUpper.h2o, lr_negativeUpper.R)
+  compare_coeff(lr_negativeUpper.h2o, lr_negativeUpper.R)
+  
+  testEnd()
+}
+
+doTest("GLM Test: LR w/ Beta Constraints", test.LR.betaConstraints)
+
