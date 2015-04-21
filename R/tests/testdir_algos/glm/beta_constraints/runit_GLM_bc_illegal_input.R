c7b46614835701a0bce9a21331d94db2d2a7525a
Amy Wang <amy@0xdata.com>
2015-04-20 20:21:40 -0700

    added illegal argument checks in r unit for beta constraints

--- R/tests/testdir_algos/glm/beta_constraints/runit_GLM_bc_illegal_input.R
+++ R/tests/testdir_algos/glm/beta_constraints/runit_GLM_bc_illegal_input.R
@@ -26,10 +26,9 @@ test.bc.illegal <- function(conn) {
   family_type = "binomial"
   
   ## Function to run GLM with specific beta_constraints
-  run_glm <- function(bc) {
+  run_glm <- function(bc, data = modelStack) {
     h2o.glm(x = indVars, y = depVars, data = modelStack, family = family_type, use_all_factor_levels = T, key = "a",
-            lambda = 0, higher_accuracy = T,
-            alpha = alpha, beta_constraints = bc)
+            lambda = 0, higher_accuracy = T, alpha = alpha, beta_constraints = bc)
   }
     
   Log.info("Use beta constraints with same feature listed twice: ")  
@@ -51,6 +50,21 @@ test.bc.illegal <- function(conn) {
 #   c <- bc
 #   names(c) <- gsub("lower_bounds", replacement = "lowerbounds", x = names(bc))
 #   checkException(run_glm(c), "Did not detect one of the columns had a typo, GLM ran without lower bounds.", silent = T)
+
+  Log.info("Change column to enum and try to input first level as beta constraints with use_all_factors = F.")
+  # Choose column to use as categorical and convert column to enum column.
+  cat_col = "C217"
+  a = modelStack[,c(indVars,depVars)]
+  a[,cat_col] = as.factor(a[,cat_col])
+
+  bc_cat <- data.frame( names =  c( "C217.0","C217.1", "C217.2", "C217.3", "C217.6"), 
+                      lower_bounds = rep(-10000,5), upper_bounds = rep(10000,5),
+                      beta_given = c(0.1, -1, .5, 2.4, 1.5), 
+                      rho = rep( 1, 5))
+  bc_cat <- rbind(bc_cat, bc[!(bc$names == cat_col),])
+  checkException(run_glm(bc_cat, data = a), "Did not block user from using first factor level in beta constraints.", silent = T)
+#   Log.info("Bound was not expanded out for enum column, should reject.")
+#   checkException(run_glm(bc, data = a), "Fail to reject name C217 even though column is enum.", silent = T) 
 
   testEnd()
 }
