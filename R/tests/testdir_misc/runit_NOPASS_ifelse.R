a7ad1a5a69eb5e12428adf9337a89b3641cf07e9
Amy Wang <amy@0xdata.com>
2015-05-04 16:31:26 -0700

    typo for test fixed

--- R/tests/testdir_misc/runit_NOPASS_ifelse.R
+++ R/tests/testdir_misc/runit_NOPASS_ifelse.R
@@ -9,13 +9,13 @@ source('../findNSourceUtils.R')
 test.ifelse <- function(conn) {
   
   Log.info("Importing heart dataset into H2O...")
-  heart.hex <- as.h2o(h, object = heart)  
+  heart.hex <- as.h2o(conn, object = heart, key = "heart.hex")  
   Log.info("Change Surgery Column in R using bases' ifelse...")
   heart$surgery <- ifelse(heart$surgery == 0, "N", "Y")
   Log.info("Change Surgery Column in H2O using H2O's ifelse...")
   heart.hex$surgery <- ifelse(heart.hex$surgery == 0, "N", "Y")
   
-  if(!all(heart$surgery == as.data.fram(heart.hex$surgery))) stop("Conversion of column different between h2o and base ifelse function!")
+  if(!all(heart$surgery == as.data.frame(heart.hex$surgery))) stop("Conversion of column different between h2o and base ifelse function!")
   
   testEnd()
 }
